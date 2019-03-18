f4de265c9b1b46d64c41c562c89b4b84d55af0ea
dpkn31 <deepak.kn1990@gmail.com>
2019-03-18 05:19:33 +0100

    Engine for openfoam coupling

---
+++ pkg/openfoam/FoamCoupling.cpp
@@ -0,0 +1,193 @@
+// *  Deepak kn : deepak.kunhappan@3sr-grenoble.fr; deepak.kn1990@gmail.com
+
+#ifdef YADE_FOAM 
+#include <mpi.h>
+#include "FoamCoupling.hpp" 
+#include <iostream> 
+
+CREATE_LOGGER(FoamCoupling); 
+YADE_PLUGIN((FoamCoupling)); 
+
+void FoamCoupling::initMPI() { //Maybe move the  MPI init function to utils or something? 
+
+  //modify for YADE-MPI, 
+  
+  int threads; 
+  MPI_Init_thread(0,0,MPI_THREAD_SINGLE,&threads); 
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  std::cout  << "myrank = " << rank << std::endl;  
+  MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+  std::cout << "commSize = " << commSize << endl;  
+  int color = 2; //Foam uses 1  
+  MPI_Comm yadeComm; // dummy communicator; 
+  MPI_Comm_split(MPI_COMM_WORLD,color,rank,&yadeComm);
+
+}
+
+
+void FoamCoupling::setNumParticles(int np){
+  
+  numParticles = np;
+  bodyList.assign(numParticles, -1); 
+  particleData.assign(numParticles*10, 1e-19); //10 doubles for each sphere; 
+  procList.assign(numParticles, -1); 
+  hydroForce.assign(numParticles*6, 1e-19); // 3 force, 3 torque 
+  castNumParticle(numParticles); 
+} 
+
+void FoamCoupling::setIdList(const boost::python::list& alist) {
+
+  for (unsigned int i=0; i != bodyList.capacity(); ++i){
+    bodyList[i] = boost::python::extract<int> (alist[i]); 
+  }   
+
+}
+
+
+void FoamCoupling::castParticle() {
+
+#pragma omp parallel  for collapse (1)
+
+for (unsigned int i=0; i <  bodyList.capacity(); ++i)
+  {
+    const Body* b = (*scene -> bodies)[bodyList[i]].get(); 
+    if ( scene-> isPeriodic){
+      Vector3r pos = scene->cell->wrapPt( b->state->pos); 
+      particleData[i*10] = pos[0]; 
+      particleData[i*10+1] = pos[1]; 
+      particleData[i*10+2] = pos[2]; 
+    } else {
+
+    particleData[i*10] = b->state->pos[0]; 
+    particleData[i*10+1] = b->state->pos[1]; 
+    particleData[i*10+2] = b->state->pos[2]; 
+  }
+    particleData[i*10+3] = b->state->vel[0]; 
+    particleData[i*10+4] = b->state->vel[1]; 
+    particleData[i*10+5] = b->state->vel[2]; 
+    particleData[i*10+6] = b->state->angVel[0]; 
+    particleData[i*10+7] = b->state->angVel[1]; 
+    particleData[i*10+8] = b->state->angVel[2]; 
+    shared_ptr<Sphere> s = YADE_PTR_DYN_CAST<Sphere>(b->shape); 
+    particleData[i*10+9] = s->radius; 
+  } 
+
+  MPI_Bcast(&particleData.front(), particleData.capacity(), MPI_DOUBLE, rank, MPI_COMM_WORLD); 
+
+}
+
+
+void FoamCoupling::castNumParticle(int value) {
+  MPI_Bcast(&value, 1, MPI_INT, rank, MPI_COMM_WORLD); 
+
+}
+
+
+void FoamCoupling::updateProcList()
+{
+  for (unsigned int i=0; i != bodyList.capacity(); ++i) 
+  {
+     int dummy_val = -5; 
+     MPI_Allreduce(&dummy_val,&procList[i],1,MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+     if (procList[i] < 0 )  std::cout << "Particle not found in FOAM " << std::endl; 
+   } 
+} 
+
+void FoamCoupling::recvHydroForce() {
+
+  for (unsigned int i=0; i!= procList.capacity(); ++i) {
+    int recvFrom = procList[i]; 
+    for (unsigned int j=0; j != 6; ++j) {
+     MPI_Recv(&hydroForce[6*i+j],1,MPI_DOUBLE,recvFrom,sendTag,MPI_COMM_WORLD,&status); }
+   }
+}
+
+void FoamCoupling::setHydroForce() {
+
+
+ #pragma omp parallel for collapse(1)
+    for (unsigned int i=0; i < bodyList.capacity(); ++i) {
+       const Vector3r& fx=Vector3r(hydroForce[6*i], hydroForce[6*i+1], hydroForce[6*i+2]); 
+       const Vector3r& tx=Vector3r(hydroForce[6*i+3], hydroForce[6*i+4], hydroForce[6*i+5]);
+       scene->forces.addForce(bodyList[i], fx); 
+       scene->forces.addTorque(bodyList[i], tx);
+  }
+
+}
+
+void FoamCoupling::sumHydroForce() {
+    
+  Real dummy_val = 0.0; 
+  for (unsigned int i=0; i != bodyList.capacity(); ++i) {   
+   for (unsigned int j=0; j != 6; ++j){
+     MPI_Allreduce(&dummy_val ,&hydroForce[6*i+j],1,MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+   }
+  } 
+}
+
+
+void FoamCoupling::resetProcList() { 
+   int capacity = procList.capacity(); 
+   procList.assign(capacity, -1); 
+}
+
+
+void FoamCoupling::action() { 
+
+  if ( exchangeData()) {
+    runCoupling();  
+    exchangeDeltaT();  
+  }
+  setHydroForce(); 
+}
+
+bool FoamCoupling::exchangeData(){  
+
+  return scene->iter%dataExchangeInterval==0; 
+
+}
+
+void FoamCoupling::exchangeDeltaT() {
+
+
+  // Recv foamdt  first and broadcast;  
+  MPI_Recv(&foamDeltaT,1,MPI_DOUBLE,1,sendTag,MPI_COMM_WORLD,&status);  
+  //bcast yadedt to others.
+  Real  yadeDt = scene-> dt;
+  MPI_Bcast(&yadeDt,1,MPI_DOUBLE, rank, MPI_COMM_WORLD); 
+  // calculate the interval . TODO: to include hydrodynamic time scale if inertial in openfoam 
+  // here -> hDeltaT = getViscousTimeScale(); 
+  dataExchangeInterval = (long int) ((yadeDt < foamDeltaT) ? foamDeltaT/yadeDt : 1);   
+
+}
+
+
+ 
+Real FoamCoupling::getViscousTimeScale() { 
+ 
+//  Real hDeltaT = 0.0; 
+//  Real dummy = 1e9; 
+//
+//  MPI_Allreduce(&dummy, &hDeltaT, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);  
+//
+  return 0; 
+  
+}
+
+void FoamCoupling::runCoupling() {
+
+    castParticle(); 
+    updateProcList(); 
+    if (isGaussianInterp){
+        sumHydroForce(); 
+    } else {
+    recvHydroForce();
+  }
+}
+
+void FoamCoupling::killMPI() {
+  MPI_Finalize(); 
+}
+
+#endif
+
---
+++ pkg/openfoam/FoamCoupling.hpp
@@ -0,0 +1,79 @@
+// YADE-OpenFOAM coupling module, Deepak kn  deepak.kunhappan@3sr-grenoble.fr/deepak.kn1990@gmail.com
+#ifdef YADE_FOAM
+
+#pragma once 
+
+#include <core/Scene.hpp> 
+#include <core/GlobalEngine.hpp> 
+#include <core/Body.hpp>
+#include <core/Omega.hpp>
+#include <pkg/common/Sphere.hpp> 
+#include <vector> 
+#include <core/InteractionContainer.hpp> // for pairwise hydro interaction (to be implemented) 
+#include <lib/serialization/Serializable.hpp>
+
+class Scene; 
+class FoamCoupling : public GlobalEngine {
+
+
+  private:
+    // some variables for MPI_Send/Recv 
+    const int sendTag=500;  
+    MPI_Status status; 
+    int rank, commSize; 
+
+
+  public: 
+    
+    void initMPI(); 
+    void setNumParticles(int); 
+    void setIdList(const boost::python::list& );  
+    void killMPI(); 
+    void updateProcList();
+    void castParticle();
+    void castNumParticle(int); 
+    void resetProcList(); 
+    void recvHydroForce(); 
+    void setHydroForce();
+    void sumHydroForce(); 
+    void exchangeDeltaT();  
+    void runCoupling(); 
+    bool exchangeData();
+    Real getViscousTimeScale();  // not fully implemented, piece of code left in foam.
+    virtual void action(); 
+    virtual ~FoamCoupling(){}; 
+    std::vector<int> bodyList; 
+    std::vector<double> hydroForce; 
+    std::vector<double> particleData;
+    std::vector<int>  procList; 
+    Real foamDeltaT; 
+    long int  dataExchangeInterval=1; 
+    bool recvdFoamDeltaT; 
+    bool isGaussianInterp; 
+    
+    YADE_CLASS_BASE_DOC_ATTRS_INIT_CTOR_PY(FoamCoupling,GlobalEngine, "An engine for coupling Yade with the finite volume fluid solver OpenFOAM in parallel." " \n Requirements : Yade compiled with MPI libs, OpenFOAM-6 (openfoam is not required for compilation)." "Yade is executed under MPI environment with OpenFOAM simultaneously, and using MPI communication  routines data is exchanged between the solvers \n."
+   "1. Yade broadcasts the particle data -> position, velocity, ang-velocity, radius to all the foam processes as in :yref:`castParticle <FoamCoupling::castParticle>` \n"
+   "2. In each foam process, particle is searched.Yade keeps a vector(cpp) of the rank of the openfoam process containing that particular particle (FoamCoupling::procList), using :yref:`updateProcList <FoamCoupling::updateProcList>`\n"
+  "3. In simple lagrangian point force coupling Yade recieves the particle hydrodynamic force and torque from the openfoam process, the sender is identified from  the vector :yref:`FoamCoupling::procList`.\n"
+  "In the case of Gaussian interpolation, contribution from every  process is summed using function :yref:`sumHydroForce <FoamCoupling::sumHydroForce>`. \n"
+  "4. The interval (substepping) is set automatically (:yref:`FoamCoupling::dataExchangeInterval`) based on dtfoam/dtYade, calculated in function :yref:`exchangeDeltaT<FoamCoupling::exchangeDeltaT>`.\n "
+  "The solver for OpenFOAM can be found at : https://github.com/dpkn31/Yade-OpenFOAM-coupling", 
+    ((int,numParticles,1, , "number of particles in coupling."))
+    ((double,particleDensity,1, , "particle Density")) //not needed  as this is set in foam  
+    ((double,fluidDensity,1, ,"fluidDensity")) //not needed  as this is set in foam  
+    ,
+    ,
+    ,
+    .def("setIdList", &FoamCoupling::setIdList,boost::python::arg("bodyIdlist"), "list of body ids in hydroForce coupling. (links to :yref: `FoamCoupling::bodyList` vector, used to build particle data :yref:`FoamCoupling::particleData`. :yref:`FoamCoupling::particleData` contains the particle pos, vel, angvel, radius and this is sent to foam. )")
+    .def("initMPI", &FoamCoupling::initMPI, "Initiallize MPI communicator for coupling. Should be called at the beginning of the script. :yref: `initMPI <FoamCoupling::initMPI>` Initializes  the MPI environment. " )
+    .def("killMPI", &FoamCoupling::killMPI, "Destroy MPI, to be called at the end of the simulation, from :yref:`killMPI<FoamCoupling::killMPI>`")
+    .def("setNumParticles",&FoamCoupling::setNumParticles,boost::python::arg("numparticles"),"number of particles in coupling")
+    .def_readonly("foamDeltaT", &FoamCoupling::foamDeltaT, "timestep in openfoam solver from  :yref:`exchangeDeltaT <FoamCoupling::exchangeDeltaT>` ") 
+    .def_readonly("dataExchangeInterval", &FoamCoupling::dataExchangeInterval, "Number of iterations/substepping : for stability and to be in sync with fluid solver calculated in :yref:`exchangeDeltaT <FoamCoupling::exchangeDeltaT>`")
+    .def_readwrite("isGaussianInterp", &FoamCoupling::isGaussianInterp, "switch for Gaussian interpolation of field varibles in openfoam. Uses  :yref:`sumHydroForce<FoamCoupling::sumHydroForce>` to obtain hydrodynamic force ") 
+    )
+    DECLARE_LOGGER; 
+}; 
+REGISTER_SERIALIZABLE(FoamCoupling); 
+
+#endif  
