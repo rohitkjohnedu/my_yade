1eb6f7a1c32a4393f50019ee1e37401cc43b4a73
Francois <francois.kneib@gmail.com>
2019-04-02 10:09:36 +0200

    Revert changes to reach the state before futurize --stage1.
    Reason: using --unicode-literals everywhere is not a good approach.
    
    This reverts commit 72e6b8b4feaa6a5f3aeaf8c2ea8bc6356b48dd15.

--- core/Material.cpp
+++ core/Material.cpp
@@ -9,18 +9,18 @@ const shared_ptr<Material> Material::byId(int id, Scene* w_){
 	return w->materials[id];
 }
 
-const shared_ptr<Material> Material::byLabel(const std::wstring& label, Scene* w_){
+const shared_ptr<Material> Material::byLabel(const std::string& label, Scene* w_){
 	Scene* w=w_?w_:Omega::instance().getScene().get();
 	FOREACH(const shared_ptr<Material>& m, w->materials){
 		if(m->label == label) return m;
 	}
-	throw std::runtime_error((L"No material labeled `"+label+L"'.").c_str());
+	throw std::runtime_error(("No material labeled `"+label+"'.").c_str());
 }
 
-const int Material::byLabelIndex(const std::wstring& label, Scene* w_){
+const int Material::byLabelIndex(const std::string& label, Scene* w_){
 	Scene* w=w_?w_:Omega::instance().getScene().get(); size_t iMax=w->materials.size();
 	for(size_t i=0; i<iMax; i++){
 		if(w->materials[i]->label==label) return i;
 	}
-	throw std::runtime_error((L"No material labeled `"+label+L"'.").c_str());
+	throw std::runtime_error(("No material labeled `"+label+"'.").c_str());
 }
