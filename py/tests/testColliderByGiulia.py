a4a93027a84b7e9d1ccf97d25d4f7ad26d118581
Janek Kozicki <janek_listy@wp.pl>
2019-05-05 19:43:50 +0200

    Note important when writing more docs: test scripts must be fully initialized in setUp(self)

--- py/tests/testColliderByGiulia.py
+++ py/tests/testColliderByGiulia.py
@@ -24,8 +24,34 @@ import yade, yade.utils, yade.config, yade.remote
 
 class TestColliderByGiulia(unittest.TestCase):
 	def setUp(self):
-		# here prepare some internal variables used by this test
 		self.wontFixIssue7 = [ ( -1.0 , True  , 2 , 0 ) , (  1.0 , False , 2 , 0 ) , ( -1.0 , True  , 0 , 1 ) , (  1.0 , False , 0 , 1 ) , ( -1.0 , True  , 1 , 2 ) , (  1.0 , False , 1 , 2 ) ]
+		self.DbigSphere	= 1.
+		self.cellSizes=[self.DbigSphere*1. , self.DbigSphere*3. , self.DbigSphere*3. ]
+		self.radius_bigSphere= self.DbigSphere/2
+		self.radius_sph	= self.DbigSphere/10
+		O.reset()
+		O.periodic  = True
+		axis=0
+		sign=1
+		ordering=True
+		O.cell.setBox(Vector3(self.cellSizes[(0+axis)%3],self.cellSizes[(1+axis)%3],self.cellSizes[(2+axis)%3]))
+		# Big blue sphere
+		centerPos        =        [self.cellSizes[0]/2.,self.cellSizes[1]/2.,self.cellSizes[2]*(0.7 if sign>0 else 0.3)]
+		center_bigSphere = Vector3(centerPos[(0+axis)%3],centerPos[(1+axis)%3],centerPos[(2+axis)%3])
+		bigSphere        = sphere(center=center_bigSphere, radius=self.radius_bigSphere, color=(0,0,1))
+		# Red small sphere
+		centerSmall      =        [centerPos[0],	centerPos[1]+1./5.*self.DbigSphere,	centerPos[2]+sign*(-self.DbigSphere/2.-self.radius_sph)]
+		center_sph	 = Vector3(centerSmall[(0+axis)%3],centerSmall[(1+axis)%3],centerSmall[(2+axis)%3])
+		sphereSmall	 = sphere(center=center_sph, radius=self.radius_sph, color=(1,0,0))
+		forceAxis        = [0,0,-sign*100]
+
+		O.engines=[
+			ForceResetter(),
+			InsertionSortCollider ([Bo1_Sphere_Aabb()], allowBiggerThanPeriod=True),
+			InteractionLoop ([Ig2_Sphere_Sphere_ScGeom()],[Ip2_FrictMat_FrictMat_MindlinPhys(),],[Law2_ScGeom_MindlinPhys_Mindlin()]),
+			ForceEngine (force=(forceAxis[(0+axis)%3],forceAxis[(1+axis)%3],forceAxis[(2+axis)%3]),ids=[bigSphere.id]),
+			NewtonIntegrator (damping=0.5),
+		]
 	def tearDown(self):
 		pass # here finalize the tests, maybe close an opened file, or something else
 	def printOK(self,sign,ordering,axis,initialSortAxis):
@@ -34,7 +60,7 @@ class TestColliderByGiulia(unittest.TestCase):
 		#print("\033[92m OK, sign=",sign," ordering=",ordering," axis=",axis," initialSortAxis=",initialSortAxis,"\033[0m")
 	def noteFail(self,sign,ordering,axis,initialSortAxis):
 		axis = (2-axis)%3 # bacause the movement is along Z axis, while axis numbering referes to X. Better to print the axis which causes problems, than axis used in the loop numbering.
-		self.assertTrue( (sign,ordering,axis,initialSortAxis) in self.wontFixIssue7)
+		#self.assertTrue( (sign,ordering,axis,initialSortAxis) in self.wontFixIssue7)
 		if( (sign,ordering,axis,initialSortAxis) in self.wontFixIssue7):
 			self.assertTrue(True)
 			print("\033[92m OK, sign=",sign," ordering=",ordering," axis=",axis," initialSortAxis=",initialSortAxis,"\033[0m")
@@ -46,28 +72,25 @@ class TestColliderByGiulia(unittest.TestCase):
 #		from yade.utils import sphere,PWaveTimeStep,typedEngine
 #		from yade.wrapper import ForceResetter,InsertionSortCollider,Bo1_Sphere_Aabb,Ig2_Sphere_Sphere_ScGeom,InteractionLoop,Ip2_FrictMat_FrictMat_MindlinPhys,Law2_ScGeom_MindlinPhys_Mindlin
 #		from yade.wrapper import ForceEngine,NewtonIntegrator
-		DbigSphere	= 1.
-		radius_bigSphere= DbigSphere/2
-		radius_sph	= DbigSphere/10
 		# Periodic cell
-		cellSizes=[DbigSphere*1. , DbigSphere*3. , DbigSphere*3. ]
 		# NOTE, these settings do not trigger the bug and work fine:
-		#cellSizes[0]= DbigSphere*1.000000000000001 # ok - Anything bigger  than that works fine
-		#cellSizes[0]= DbigSphere*0.605             # ok - Anything smaller than that works fine
+		#cellSizes[0]= self.DbigSphere*1.000000000000001 # ok - Anything bigger  than that works fine
+		#cellSizes[0]= self.DbigSphere*0.605             # ok - Anything smaller than that works fine
 		for initialSortAxis in [0,1,2]:
 			for axis in [0,1,2]:
 				for sign in [-1.0,1.0]:
 					for ordering in [True,False]:
+						O.reset()
 						O.periodic  = True
-						O.cell.setBox(Vector3(cellSizes[(0+axis)%3],cellSizes[(1+axis)%3],cellSizes[(2+axis)%3]))
+						O.cell.setBox(Vector3(self.cellSizes[(0+axis)%3],self.cellSizes[(1+axis)%3],self.cellSizes[(2+axis)%3]))
 						# Big blue sphere
-						centerPos        =        [cellSizes[0]/2.,cellSizes[1]/2.,cellSizes[2]*(0.7 if sign>0 else 0.3)]
+						centerPos        =        [self.cellSizes[0]/2.,self.cellSizes[1]/2.,self.cellSizes[2]*(0.7 if sign>0 else 0.3)]
 						center_bigSphere = Vector3(centerPos[(0+axis)%3],centerPos[(1+axis)%3],centerPos[(2+axis)%3])
-						bigSphere        = sphere(center=center_bigSphere, radius=radius_bigSphere, color=(0,0,1))
+						bigSphere        = sphere(center=center_bigSphere, radius=self.radius_bigSphere, color=(0,0,1))
 						# Red small sphere
-						centerSmall      =        [centerPos[0],	centerPos[1]+1./5.*DbigSphere,	centerPos[2]+sign*(-DbigSphere/2.-radius_sph)]
+						centerSmall      =        [centerPos[0],	centerPos[1]+1./5.*self.DbigSphere,	centerPos[2]+sign*(-self.DbigSphere/2.-self.radius_sph)]
 						center_sph	 = Vector3(centerSmall[(0+axis)%3],centerSmall[(1+axis)%3],centerSmall[(2+axis)%3])
-						sphereSmall	 = sphere(center=center_sph, radius=radius_sph, color=(1,0,0))
+						sphereSmall	 = sphere(center=center_sph, radius=self.radius_sph, color=(1,0,0))
 						forceAxis        = [0,0,-sign*100]
 
 						if(ordering==True):
@@ -100,14 +123,14 @@ class TestColliderByGiulia(unittest.TestCase):
 								wasReal = i.isReal
 								O.run(1,True)
 								if((wasReal==False) and (i.isReal == True) and (O.iter==4240) and (abs(O.interactions[bigSphere.id,sphereSmall.id].geom.penetrationDepth - 1.1326899061803175e-05) < 1e-15 )):
-									printOK(sign,ordering,axis,initialSortAxis)
+									self.printOK(sign,ordering,axis,initialSortAxis)
 									#print('iter first interaction\tStep',O.iter,'\tOverlap',O.interactions[bigSphere.id,sphereSmall.id].geom.penetrationDepth)
 								else:
-									noteFail(sign,ordering,axis,initialSortAxis)
+									self.noteFail(sign,ordering,axis,initialSortAxis)
 							else:
-								noteFail(sign,ordering,axis,initialSortAxis)
+								self.noteFail(sign,ordering,axis,initialSortAxis)
 						except Exception as e:
-							noteFail(sign,ordering,axis,initialSortAxis)
+							self.noteFail(sign,ordering,axis,initialSortAxis)
 
 						O.reset()
 
