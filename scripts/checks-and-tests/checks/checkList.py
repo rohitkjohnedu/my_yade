7fd85192ef3735e13d6139ea8c56813b0f9f8270
Janek Kozicki <janek_listy@wp.pl>
2019-05-09 17:07:58 +0200

    Remove printing output to stderr output.

--- scripts/checks-and-tests/checks/checkList.py
+++ scripts/checks-and-tests/checks/checkList.py
@@ -8,14 +8,11 @@ class YadeCheckError(Exception):
 	"""Use ``raise YadeCheckError("message") when check fails. Or throw any other python exception that seems suitable."""
 	pass
 
-def errprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
-
 scriptsToRun=os.listdir(checksPath)
 failedScripts=list()
 
 skipScripts = ['checkList.py']
-onlyOneScript = []
+onlyOneScript = [] # use this if you want to test only one script, it takes precedence over skipScripts.
 
 def mustCheck(sc):
 	if(len(onlyOneScript)==1): return sc in onlyOneScript
@@ -24,25 +21,25 @@ def mustCheck(sc):
 for script in scriptsToRun:
 	if (script[len(script)-3:]==".py" and mustCheck(script)):
 		try:
-			errprint("###################################")
-			errprint("running: ",script)
+			print("###################################")
+			print("running: ",script)
 			execfile(checksPath+"/"+script)
-			errprint("Status: success")
-			errprint("___________________________________")
+			print("Status: success")
+			print("___________________________________")
 		except Exception as e:
 			failedScripts.append(script)
-			errprint('\033[91m',script," failure, caught exception ",e.__class__.__name__,": ",e,'\033[0m')
+			print('\033[91m',script," failure, caught exception ",e.__class__.__name__,": ",e,'\033[0m')
 		O.reset()
 	elif (not mustCheck(script)):
-		errprint("###################################")
-		errprint("Skipping %s, because it is in SkipScripts"%script)
+		print("###################################")
+		print("Skipping %s, because it is in SkipScripts"%script)
 
 if (len(failedScripts)!=0):
-	errprint('\033[91m', len(failedScripts) , " tests are failed"+'\033[0m')
-	for s in failedScripts: errprint("  "+s)
+	print('\033[91m', len(failedScripts) , " tests are failed"+'\033[0m')
+	for s in failedScripts: print("  "+s)
 	sys.exit(1)
 else:
 	# https://misc.flogisoft.com/bash/tip_colors_and_formatting
-	errprint('\033[92m'+"*** ALL CHECKS PASSED ***"+'\033[0m')
+	print('\033[92m'+"*** ALL CHECKS PASSED ***"+'\033[0m')
 	sys.exit(0)
 
