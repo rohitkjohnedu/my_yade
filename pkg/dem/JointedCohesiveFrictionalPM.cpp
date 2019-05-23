38ae2736ae7c07fdedf0bf49af790d49ac998338
robcaulk <rob.caulk@gmail.com>
2019-05-23 09:16:05 +0200

    Fix radius correction error in weibulldistribution (thanks Klaus Theoni)

--- pkg/dem/JointedCohesiveFrictionalPM.cpp
+++ pkg/dem/JointedCohesiveFrictionalPM.cpp
@@ -604,8 +604,8 @@ void Ip2_JCFpmMat_JCFpmMat_JCFpmPhys::distributeCrossSectionsWeibull(shared_ptr<
 	Real correction = weibullDistribution(e2);
 	if (correction < weibullCutOffMin) correction = weibullCutOffMin;
 	else if (correction > weibullCutOffMax) correction = weibullCutOffMax;
-	Real interactingRadius = correction*min(R1, R2);  // correcting radius to account for grain interactions
-	contactPhysics->crossSection = Mathr::PI*pow(interactingRadius,2);
+	Real interactingRadius = min(R1, R2);  
+	contactPhysics->crossSection = correction*Mathr::PI*pow(interactingRadius,2);
 }
 
 JCFpmPhys::~JCFpmPhys(){}
