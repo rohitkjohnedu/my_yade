bca9d313b6ffb3a698dfea0c59799fcede83e6d1
Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
2019-06-07 19:11:18 +0100

    Stop writing the vertices of the Aabb in the PB shape class. Removal of default Aabb enlargement factors (1.05).

--- pkg/dem/PotentialBlock2AABB.cpp
+++ pkg/dem/PotentialBlock2AABB.cpp
@@ -7,47 +7,59 @@
 
 void PotentialBlock2AABB::go(const shared_ptr<Shape>& cm, shared_ptr<Bound>& bv, const Se3r& se3, const Body*){
 	PotentialBlock* pp = static_cast<PotentialBlock*>(cm.get());
-	if(!bv){ bv=shared_ptr<Bound>(new Aabb); }	
+	if(!bv){ bv=shared_ptr<Bound>(new Aabb); }
 	Aabb* aabb = static_cast<Aabb*>(bv.get());
 	//Matrix3r r = se3.orientation.toRotationMatrix();
 
 	if(pp->AabbMinMax == false){
-		Real distFromCentre = 1.05*pp->R; // std::max(maxD, pp->R);
+		Real distFromCentre = 1.0*pp->R; // std::max(maxD, pp->R);
 		halfSize = Vector3r(distFromCentre,distFromCentre,distFromCentre); 
 		aabb->min = se3.position-halfSize;
 		aabb->max = se3.position+halfSize;
 		return;
 	}else{
-		Matrix3r r=se3.orientation.toRotationMatrix();
-		Vector3r halfSizeMin(Vector3r::Zero());Vector3r halfSizeMax(Vector3r::Zero());
-		if(pp->vertices.size() ==0){
-			//pp->vertices.clear();
-			pp->vertices.push_back(Vector3r(pp->maxAabbRotated[0],pp->maxAabbRotated[1],pp->maxAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(pp->maxAabbRotated[0],pp->maxAabbRotated[1],-pp->minAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],-pp->minAabbRotated[1],pp->maxAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],-pp->minAabbRotated[1],-pp->minAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],pp->maxAabbRotated[1],pp->maxAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],pp->maxAabbRotated[1],-pp->minAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(pp->maxAabbRotated[0],-pp->minAabbRotated[1],pp->maxAabbRotated[2]));
-			pp->vertices.push_back(Vector3r(pp->maxAabbRotated[0],-pp->minAabbRotated[1],-pp->minAabbRotated[2]));
-		}
-		Vector3r aabbMin(0,0,0);
+//		Matrix3r r=se3.orientation.toRotationMatrix();
+//		Vector3r halfSizeMin(Vector3r::Zero());Vector3r halfSizeMax(Vector3r::Zero());
+//		if(pp->vertices.size() ==0){
+//			//pp->vertices.clear();
+//			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0], pp->maxAabbRotated[1], pp->maxAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0], pp->maxAabbRotated[1],-pp->minAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],-pp->minAabbRotated[1], pp->maxAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],-pp->minAabbRotated[1],-pp->minAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0], pp->maxAabbRotated[1], pp->maxAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0], pp->maxAabbRotated[1],-pp->minAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0],-pp->minAabbRotated[1], pp->maxAabbRotated[2] ));
+//			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0],-pp->minAabbRotated[1],-pp->minAabbRotated[2] ));
+//		}
+
+		Vector3r aabbMin(0,0,0); 
 		Vector3r aabbMax(0,0,0);
-		for (int i=0; i<8; i++){
-			Vector3r vertex = r*(pp->oriAabb.conjugate()*pp->vertices[i]);
-			if(vertex.x() < aabbMin.x()){ aabbMin.x() = vertex.x(); }
-			if(vertex.y() < aabbMin.y()){ aabbMin.y() = vertex.y(); }
-			if(vertex.z() < aabbMin.z()){ aabbMin.z() = vertex.z(); }
-			if(vertex.x() > aabbMax.x()){ aabbMax.x() = vertex.x(); }
-			if(vertex.y() > aabbMax.y()){ aabbMax.y() = vertex.y(); }
-			if(vertex.z() > aabbMax.z()){ aabbMax.z() = vertex.z(); }
+
+		for (unsigned int i=0; i<pp->vertices.size(); i++){
+//			Vector3r vertex = r*(pp->oriAabb.conjugate()*pp->vertices[i]);			
+			Vector3r vertex = se3.orientation*pp->vertices[i];
+
+			aabbMin=aabbMin.cwiseMin(vertex);
+			aabbMax=aabbMax.cwiseMax(vertex);
+
+//			aabbMin = Vector3r( min(aabbMin[0],vertex[0]), min(aabbMin[1],vertex[1]), min(aabbMin[2],vertex[2]) );
+//			aabbMax = Vector3r( max(aabbMax[0],vertex[0]), max(aabbMax[1],vertex[1]), max(aabbMax[2],vertex[2]) );
+
+//			if(vertex.x() < aabbMin.x()){ aabbMin.x() = vertex.x(); }
+//			if(vertex.y() < aabbMin.y()){ aabbMin.y() = vertex.y(); }
+//			if(vertex.z() < aabbMin.z()){ aabbMin.z() = vertex.z(); }
+
+//			if(vertex.x() > aabbMax.x()){ aabbMax.x() = vertex.x(); }
+//			if(vertex.y() > aabbMax.y()){ aabbMax.y() = vertex.y(); }
+//			if(vertex.z() > aabbMax.z()){ aabbMax.z() = vertex.z(); }
 		}
-		aabb->min=  se3.position + 1.05*aabbMin;
-		aabb->max = se3.position + 1.05*aabbMax;
-		halfSizeMin = -1.0*aabbMin;
-		halfSizeMax = 1.0*aabbMax;
+
+		aabb->min=  se3.position + 1.0*aabbMin;
+		aabb->max = se3.position + 1.0*aabbMax;
+//		halfSizeMin = -1.0*aabbMin;
+//		halfSizeMax =  1.0*aabbMax;
 	
-		pp->halfSize = 1.1*Vector3r(std::max(halfSizeMin.x(),halfSizeMax.x()),std::max(halfSizeMin.y(),halfSizeMax.y()),std::max(halfSizeMin.z(),halfSizeMax.z()));
+//		pp->halfSize = 1.0*Vector3r(std::max(halfSizeMin.x(),halfSizeMax.x()),std::max(halfSizeMin.y(),halfSizeMax.y()),std::max(halfSizeMin.z(),halfSizeMax.z()));
 		return;
 	}
 	
