77c61b15231e2630f2fa1e442522508c09515870
dpkn31 <deepak.kn1990@gmail.com>
2019-09-04 16:39:04 +0200

    add flag for  parallel fluid coupling, set communicator to fluid coupling engine, bound functor for fluid grid bounding boxes, introduce a global list of bodies in coupling

--- py/mpy.py
+++ py/mpy.py
@@ -749,6 +749,8 @@ def splitScene():
 			
 			#tell the collider how to handle this new thing
 			collider.boundDispatcher.functors=collider.boundDispatcher.functors+[Bo1_Subdomain_Aabb()]
+			if FLUID_COUPLING: 
+				collider.boundDispatcher.functors = collider.boundDispatcher.functors+[Bo1_FluidDomainBbox_Aabb()]
 			collider.targetInterv=0
 			collider.keepListsShort=True # probably not needed, O.bodies.insertAtId should turn it on automaticaly 
 			O.bodies.useRedirection=True # idem
@@ -775,11 +777,13 @@ def splitScene():
 				if domainBody==None: wprint("SUBDOMAIN NOT FOUND FOR RANK=",rank)
 				O.subD = domainBody.shape
 				O.subD.subdomains = subdomains
-		
+				
 		O._sceneObj.subdomain = rank
 		O.subD.comm=comm #make sure the c++ uses the merged intracommunicator
 		
 		O.subD.init() 
+		
+		wprint("LEN OF BODIES BEFORE FLUID COUPLING = ", len(O.bodies))
 		
 		if FLUID_COUPLING:
 			fluidCoupling = utils.typedEngine('FoamCoupling') 
