6c6653a445462177bc7fbf80d2563de103b7771d
dpkn31 <deepak.kn1990@gmail.com>
2019-09-14 19:12:09 +0200

    getters and setters for scene->subD

--- py/wrapper/yadeWrapper.cpp
+++ py/wrapper/yadeWrapper.cpp
@@ -29,6 +29,8 @@
 #include <boost/lambda/bind.hpp>
 #include <boost/math/special_functions/nonfinite_num_facets.hpp>
 #include <boost/python/raw_function.hpp>
+
+#include <locale>
 #include <boost/random/linear_congruential.hpp>
 #include <boost/random/uniform_real.hpp>
 #include <boost/random/variate_generator.hpp>
@@ -1007,6 +1009,31 @@ public:
 	void stopAtIter_set(long s) { OMEGA.getScene()->stopAtIter = s; }
 	Real stopAtTime_get() { return OMEGA.getScene()->stopAtTime; }
 	void stopAtTime_set(Real s) { OMEGA.getScene()->stopAtTime = s; }
+	
+#ifdef YADE_MPI 
+	Body::id_t getSubdomainId(){
+		return OMEGA.getScene()->thisSubdomainId; 
+	}
+	
+	void setSubdomainId(Body::id_t subdId){
+		OMEGA.getScene()->thisSubdomainId = subdId; 
+	}
+	
+	void setSubD(shared_ptr<Subdomain> subD){
+		OMEGA.getScene()->subD = subD; 
+	}
+	
+	shared_ptr<Subdomain> getSubD() {
+		return YADE_PTR_DYN_CAST<Subdomain>(OMEGA.getScene()->subD); 
+	}
+#endif 
+	bool dynDt_get(){return OMEGA.getScene()->timeStepperActive();}
+	bool dynDt_set(bool activate){if(not OMEGA.getScene()->timeStepperActivate(activate) and activate) /* not activated despite passed value */ throw runtime_error("No TimeStepper found in O.engines."); return true;}
+	bool dynDtAvailable_get(){ return OMEGA.getScene()->timeStepperPresent(); }
+	long stopAtIter_get(){return OMEGA.getScene()->stopAtIter; }
+	void stopAtIter_set(long s){OMEGA.getScene()->stopAtIter=s; }
+	Real stopAtTime_get(){return OMEGA.getScene()->stopAtTime; }
+	void stopAtTime_set(Real s){OMEGA.getScene()->stopAtTime=s; }
 
 
 	bool timingEnabled_get() { return TimingInfo::enabled; }
@@ -1449,7 +1476,6 @@ try {
 	        .value("noResize", yade::Attr::noResize);
 
 	py::class_<pyOmega>("Omega")
-<<<<<<< 18aa7511517a24e39441f58e39171b5da0709e98
 	        .add_property("iter", &pyOmega::iter, "Get current step number")
 	        .add_property(
 	                "subStep",
