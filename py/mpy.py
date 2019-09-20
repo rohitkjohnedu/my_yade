7d34ebfe0c76318f5ad273b0cbbd13af75088b76
dpkn31 <deepak.kn1990@gmail.com>
2019-09-20 14:25:10 +0200

    use setIDstoSubdomain function to set the bodies owned by the master into its subdomain

--- py/mpy.py
+++ py/mpy.py
@@ -747,6 +747,9 @@ def splitScene():
 			subD= O.subD #alias
 			subD.comm=comm #make sure the c++ uses the merged intracommunicator
 			
+			masterBodies = [b.id for b in O.bodies if b.subdomain==0] 
+			subD.setIDstoSubdomain(masterBodies)
+			
 			#tell the collider how to handle this new thing
 			collider.boundDispatcher.functors=collider.boundDispatcher.functors+[Bo1_Subdomain_Aabb()]
 			if FLUID_COUPLING: 
