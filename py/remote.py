7f9a9235e8e8949d115c26f91602bc65bbc48558
William Chèvremont <william.chevremont@univ-grenoble-alpes.fr>
2019-09-30 17:57:58 +0200

    Fix python2->python3 bug on TCP connections

--- py/remote.py
+++ py/remote.py
@@ -75,18 +75,18 @@ class PythonConsoleSocketEmulator(socketserver.BaseRequestHandler):
 			print("TCP Connection from non-127.0.0.* address %s rejected"%self.client_address[0])
 			return
 		print(self.client_address, 'connected!')
-		self.request.send('Enter auth cookie: ')
+		self.request.send(('Enter auth cookie: ').encode())
 	def tryLogin(self):
-		if self.request.recv(1024).rstrip()==self.server.cookie:
+		if self.request.recv(1024).rstrip().decode()==self.server.cookie:
 			self.server.authenticated+=[self.client_address]
-			self.request.send("""__   __    ____                 __  _____ ____ ____  
+			self.request.send(("""__   __    ____                 __  _____ ____ ____  
 \ \ / /_ _|  _ \  ___    ___   / / |_   _/ ___|  _ \ 
  \ V / _` | | | |/ _ \  / _ \ / /    | || |   | |_) |
   | | (_| | |_| |  __/ | (_) / /     | || |___|  __/ 
   |_|\__,_|____/ \___|  \___/_/      |_| \____|_|    
 
 (connected from %s:%d)
->>> """%(str(self.client_address[0]),self.client_address[1]))
+>>> """%(str(self.client_address[0]),self.client_address[1])).encode())
 			return True
 		else:
 			import time
@@ -95,16 +95,16 @@ class PythonConsoleSocketEmulator(socketserver.BaseRequestHandler):
 			return False
 	def displayhook(self,s):
 		import pprint
-		self.request.send(pprint.pformat(s))
+		self.request.send(pprint.pformat(s).encode())
 	def handle(self):
 		if self.client_address not in self.server.authenticated and not self.tryLogin(): return
 		import code,io,traceback
 		buf=[]
 		while True:
 			data = self.request.recv(1024).rstrip()
-			if data=='\x04' or data=='exit' or data=='quit': # \x04 == ^D 
+			if data==b'\x04' or data==b'exit' or data==b'quit': # \x04 == ^D 
 				return
-			buf.append(data)
+			buf.append(data.decode())
 			orig_displayhook,orig_stdout=sys.displayhook,sys.stdout
 			sio=io.StringIO()
 			continuation=False
@@ -115,19 +115,19 @@ class PythonConsoleSocketEmulator(socketserver.BaseRequestHandler):
 					sys.displayhook=self.displayhook
 					sys.stdout=sio
 					exec(comp)
-					self.request.send(sio.getvalue())
+					self.request.send(sio.getvalue().encode())
 					buf=[]
 				else:
-					self.request.send('... '); continuation=True
+					self.request.send(('... ').encode()); continuation=True
 			except:
-				self.request.send(traceback.format_exc())
+				self.request.send(traceback.format_exc().encode())
 				buf=[]
 			finally:
 				sys.displayhook,sys.stdout=orig_displayhook,orig_stdout
-				if not continuation: self.request.send('\n>>> ')
+				if not continuation: self.request.send(('\n>>> ').encode())
 	def finish(self):
 		print(self.client_address, 'disconnected!')
-		self.request.send('\nBye ' + str(self.client_address) + '\n')
+		self.request.send(('\nBye ' + str(self.client_address) + '\n').encode())
 
 
 def _runInBackground(func):
