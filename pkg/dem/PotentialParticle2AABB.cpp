46c385e8e95795f918d5518978f0c9c4626a20c1
Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
2019-10-14 01:03:18 +0100

    Minimise script. Introduce aabbEnlargeFactor

--- pkg/dem/PotentialParticle2AABB.cpp
+++ pkg/dem/PotentialParticle2AABB.cpp
@@ -10,23 +10,18 @@ namespace yade { // Cannot have #include directive inside.
 
 void PotentialParticle2AABB::go(const shared_ptr<Shape>& cm, shared_ptr<Bound>& bv, const Se3r& se3, const Body*) {
 	PotentialParticle* pp = static_cast<PotentialParticle*>(cm.get());
-	if(!bv) {
-		bv=shared_ptr<Bound>(new Aabb);
-	}
+	if(!bv) { bv=shared_ptr<Bound>(new Aabb); }
 	Aabb* aabb = static_cast<Aabb*>(bv.get());
 
 	if(pp->AabbMinMax == false) {
-		Real distFromCentre = 1.0*pp->R; // std::max(maxD, pp->R);
-		halfSize = Vector3r(distFromCentre,distFromCentre,distFromCentre);
+		Real distFromCentre = 1.0*pp->R;
+		Vector3r halfSize = (aabbEnlargeFactor>0?aabbEnlargeFactor:1.) * Vector3r(distFromCentre,distFromCentre,distFromCentre);
 		aabb->min = se3.position-halfSize;
 		aabb->max = se3.position+halfSize;
 		return;
 	} else {
-		Matrix3r r=se3.orientation.toRotationMatrix();
-//		Vector3r halfSizeMin(Vector3r::Zero());
-//		Vector3r halfSizeMax(Vector3r::Zero());
-		if(pp->vertices.size() ==0) {
-			//pp->vertices.clear();
+//		Matrix3r r=se3.orientation.toRotationMatrix();
+		if(pp->vertices.empty()) { // Until a calculation of the vertices of a PP is developed, write the initial Aabb corners into the vertices
 			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0], pp->maxAabbRotated[1], pp->maxAabbRotated[2]));
 			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0], pp->maxAabbRotated[1],-pp->minAabbRotated[2]));
 			pp->vertices.push_back(Vector3r(-pp->minAabbRotated[0],-pp->minAabbRotated[1], pp->maxAabbRotated[2]));
@@ -36,27 +31,20 @@ void PotentialParticle2AABB::go(const shared_ptr<Shape>& cm, shared_ptr<Bound>& 
 			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0],-pp->minAabbRotated[1], pp->maxAabbRotated[2]));
 			pp->vertices.push_back(Vector3r( pp->maxAabbRotated[0],-pp->minAabbRotated[1],-pp->minAabbRotated[2]));
 		}
-		Vector3r aabbMin(0,0,0);
-		Vector3r aabbMax(0,0,0);
-		for (int i=0; i<8; i++) {
-			Vector3r vertex = r*(pp->oriAabb.conjugate()*pp->vertices[i]);
-			if(vertex.x() < aabbMin.x()) { aabbMin.x() = vertex.x(); }
-			if(vertex.y() < aabbMin.y()) { aabbMin.y() = vertex.y(); }
-			if(vertex.z() < aabbMin.z()) { aabbMin.z() = vertex.z(); }
-
-			if(vertex.x() > aabbMax.x()) { aabbMax.x() = vertex.x(); }
-			if(vertex.y() > aabbMax.y()) { aabbMax.y() = vertex.y(); }
-			if(vertex.z() > aabbMax.z()) { aabbMax.z() = vertex.z(); }
+		Vector3r vertex, aabbMin(0,0,0), aabbMax(0,0,0);
+		for (unsigned int i=0; i<pp->vertices.size(); i++){
+			vertex = se3.orientation*pp->vertices[i]; // vertices in global coordinates
+			aabbMin = aabbMin.cwiseMin(vertex);
+			aabbMax = aabbMax.cwiseMax(vertex);
 		}
-		aabb->min=  se3.position + 1.0*aabbMin;
-		aabb->max = se3.position + 1.0*aabbMax;
-//		halfSizeMin = -1.0*aabbMin;
-//		halfSizeMax =  1.0*aabbMax;
-
-
+		if (aabbEnlargeFactor>0) {
+			aabbMin *= aabbEnlargeFactor;
+			aabbMax *= aabbEnlargeFactor;
+		}
+		aabb->min = se3.position + aabbMin;
+		aabb->max = se3.position + aabbMax;
 		return;
 	}
-
 }
 
 YADE_PLUGIN((PotentialParticle2AABB));
