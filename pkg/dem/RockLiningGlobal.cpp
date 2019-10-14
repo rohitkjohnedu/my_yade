51c87dbf54a38ca1a2b2c37f64f8dc204fe8a997
Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
2019-10-14 01:03:18 +0100

    Remove trailing whitespaces

--- pkg/dem/RockLiningGlobal.cpp
+++ pkg/dem/RockLiningGlobal.cpp
@@ -54,7 +54,7 @@ namespace yade { // Cannot have #include directive inside.
 void RockLiningGlobal::action(){
 	const double PI = 3.14159;
 	if (openingCreated == true && installed == false){
-		
+
 		double angleInterval = 2.0*PI/static_cast<double>(totalNodes);
 		for (int n=0; n<totalNodes;n++){
 			double currentAngle = 0.0 + n*angleInterval; /* from 0 degrees east */
@@ -67,10 +67,10 @@ void RockLiningGlobal::action(){
 			FOREACH(const shared_ptr<Body>& b, *scene->bodies){
 				if (!b) continue;
 				if (b->isClump() == true) continue;
-				PotentialBlock* pb=static_cast<PotentialBlock*>(b->shape.get()); 
+				PotentialBlock* pb=static_cast<PotentialBlock*>(b->shape.get());
 				if(!pb) continue;
-				if(pb->isBoundary == true || pb->erase== true || pb->isLining==true){continue;}	
-				State* state1 = b->state.get();				
+				if(pb->isBoundary == true || pb->erase== true || pb->isLining==true){continue;}
+				State* state1 = b->state.get();
 				Vector3r intersectionPt(0,0,0);
 				if ( installLining(pb,  state1, startingPoint, searchDir, outerRadius, intersectionPt )){
 					IDs.push_back(b->id);
@@ -80,21 +80,21 @@ void RockLiningGlobal::action(){
 			}
 
 			/* find closest block */
-			int totalBlocks = IDs.size(); 
-			double closestDistance = 100000.0; 
+			int totalBlocks = IDs.size();
+			double closestDistance = 100000.0;
 			int closestID=0;
 			for (int i=0; i<totalBlocks; i++){
 				if ( distanceFrOpening[i] < closestDistance){
 					closestID = IDs[i];
 					closestDistance = distanceFrOpening[i];
-				}			
+				}
 			}
 			stickIDs.push_back(closestID);
 			IDs.clear();distanceFrOpening.clear();
 //std::cout<<"closestID: "<<closestID<<endl;
-	
+
 			/* find intersection with edges of polygon */
-			Vector3r jointIntersection (0,0,0); 
+			Vector3r jointIntersection (0,0,0);
 			State* state1 = Body::byId(closestID,scene)->state.get();
 			Shape* shape1 = Body::byId(closestID,scene)->shape.get();
 			PotentialBlock *pb=static_cast<PotentialBlock*>(shape1);
@@ -103,7 +103,7 @@ void RockLiningGlobal::action(){
 			Vector3r nodeLocalPos(0,0,0); Vector3r nodeGlobalPos(0,0,0);
 //std::cout<<"totalPlanes: "<<totalPlanes<<endl;
 			double closestPlaneDist = 1000000;
-			for (int i=0; i<totalPlanes; i++){						
+			for (int i=0; i<totalPlanes; i++){
 					Vector3r plane = state1->ori*Vector3r(pb->a[i], pb->b[i], pb->c[i]); double planeD = plane.dot(state1->pos) + pb->d[i] +pb->r;
 					if ( intersectPlane(pb, state1,startingPoint,searchDir, outerRadius, jointIntersection, plane, planeD)){
 						double distance = jointIntersection.norm();
@@ -112,11 +112,11 @@ void RockLiningGlobal::action(){
 							nodeLocalPos = state1->ori.conjugate()*(jointIntersection-state1->pos);
 							nodeGlobalPos=jointIntersection;
 						}
-						
+
 					}
 			}
-			if(nodeGlobalPos.norm() > 1.03*openingRad){ nodeGlobalPos=1.03*openingRad*searchDir;} 
-			//if(nodeGlobalPos.norm() < 0.98*openingRad){ continue;} 
+			if(nodeGlobalPos.norm() > 1.03*openingRad){ nodeGlobalPos=1.03*openingRad*searchDir;}
+			//if(nodeGlobalPos.norm() < 0.98*openingRad){ continue;}
 			//initOverlap = interfaceTension/interfaceStiffness;
 			nodeGlobalPos = nodeGlobalPos + searchDir*initOverlap;
 			localCoordinates.push_back(nodeLocalPos);
@@ -125,7 +125,7 @@ void RockLiningGlobal::action(){
 			blockIDs.push_back(nodeID); //(nodeID); //(closestID);
 			refOri.push_back(Quaternionr::Identity()); //(state1->ori);
 			installed = true;
-			
+
 			axialForces.push_back(0.0);
 			shearForces.push_back(0.0);
 			moment.push_back(0.0);
@@ -136,31 +136,29 @@ void RockLiningGlobal::action(){
 		}
 		totalNodes=blockIDs.size();
 
-	
+
 		/* Assembling global stiffness matrix */
 		for (int n=0; n<totalNodes;n++){
 			int nextID = n+1;
 			if(nextID==totalNodes){nextID=0;}
 			double Length = (refPos[nextID]-refPos[n]).norm();
 			lengthNode.push_back(Length);
-			
+
 			Vector3r localDir = refPos[nextID]-refPos[n];
 			localDir.normalize(); refDir.push_back(localDir);
 			double angle = acos(localDir.dot(Vector3r(1,0,0)));
-			Vector3r signAngle =  Vector3r(1,0.0,0).cross(localDir); 
+			Vector3r signAngle =  Vector3r(1,0.0,0).cross(localDir);
 
-			if (signAngle.dot(Vector3r(0,-1.0,0)) < 0.0){angle =2.0*PI - angle;}			
+			if (signAngle.dot(Vector3r(0,-1.0,0)) < 0.0){angle =2.0*PI - angle;}
 			refAngle.push_back(angle);
 			std::cout<<"angle "<<n<<" : "<<angle/PI*180.0<<endl;
-
-			
 		}
 	}
 
 //std::cout<<"complete installation"<<endl;
 
 	if (installed == true && blockIDs.size()>=2){
-			
+
 			double displacementMatrix[totalNodes*3];
 			memset(displacementMatrix,0.0,sizeof(displacementMatrix));
 			//averageForce = 0.0; maxForce = 0.0;
@@ -170,33 +168,30 @@ void RockLiningGlobal::action(){
 				axialForces[j]=0.0;
 				shearForces[j]=0.0;
 				moment[j]=0.0;
-				
+
 			}
-				
+
 			for (int j=0; j<blockNo;j++){
-				
+
 				int nextNode = j + 1; if(nextNode == blockNo){nextNode=0;}
 				State* state1 = Body::byId(blockIDs[j],scene)->state.get();
 				State* state2 = Body::byId(blockIDs[nextNode],scene)->state.get();
 				Quaternionr qA = (state1->ori);
-				double thetaA = 2.0 *acos(qA.w()); 
-				if(qA.y() < 0.0){ 
+				double thetaA = 2.0 *acos(qA.w());
+				if(qA.y() < 0.0){
 					thetaA  =  -thetaA ;
 				}
 				Quaternionr qB = (state2->ori);
-				double thetaB = 2.0 *acos(qB.w()); 
-				if(qB.y() < 0.0){ 
+				double thetaB = 2.0 *acos(qB.w());
+				if(qB.y() < 0.0){
 					thetaB  =  -thetaB ;
 				}
 
-				
-				
 				double deformedAngle = refAngle[j];// - thetaA;
 
-				//double temperatureForce = expansionFactor*lengthNode[j]*EA; 
+				//double temperatureForce = expansionFactor*lengthNode[j]*EA;
 				//Vector3r tempForceGlobal = Vector3r(temperatureForce*cos(refAngle[j] ) , 0.0,-temperatureForce*sin(refAngle[j] ) ) ; //Ttranspose
-				
-				
+
 				Vector3r globalDispA = state1->pos - refPos[j];
 				double localXa = globalDispA.x()*cos(deformedAngle) + globalDispA.z()*sin(deformedAngle);
 				double localYa = -globalDispA.x()*sin(deformedAngle) + globalDispA.z()*cos(deformedAngle);
@@ -204,17 +199,13 @@ void RockLiningGlobal::action(){
 				double localXb = globalDispB.x()*cos(deformedAngle) + globalDispB.z()*sin(deformedAngle);
 				double localYb = -globalDispB.x()*sin(deformedAngle) + globalDispB.z()*cos(deformedAngle);
 
-				
-
-				
-
 				axialForces[j] = EA/lengthNode[j] * (localXa-localXb);
 				shearForces[j] = 12.0*EI/(pow(lengthNode[j],3))*(localYa-localYb) - 6.0*EI/(pow(lengthNode[j],2))*(thetaA+thetaB);
 				moment[j] = -6.0*EI/(pow(lengthNode[j],2))*(localYa-localYb) + 2.0*EI/lengthNode[j]*(2.0*thetaA+thetaB);
 
 				double globalForceX = axialForces[j]*cos(deformedAngle) - shearForces[j]*sin(deformedAngle);
 				double globalForceZ = axialForces[j]*sin(deformedAngle) + shearForces[j]*cos(deformedAngle);
-				
+
 				Vector3r totalForceA = Vector3r(globalForceX, 0.0, globalForceZ);
 				Vector3r torqueA = moment[j]*Vector3r(0,1,0);
 
@@ -226,8 +217,7 @@ void RockLiningGlobal::action(){
 				Vector3r totalForceB = Vector3r(globalForceXb, 0.0, globalForceZb);
 				Vector3r torqueB = momentB*Vector3r(0,1,0);
 
-				
-				double area = liningThickness*1.0;				
+				double area = liningThickness*1.0;
 				sigmaMax[j] = axialForces[j]/area + fabs(moment[j])*(2.0*liningThickness)/Inertia;
 				sigmaMin[j] = axialForces[j]/area - fabs(moment[j])*(2.0*liningThickness)/Inertia;
 				double displacementSign = (state1->pos - refPos[j]).dot(refPos[j]);
@@ -241,11 +231,7 @@ void RockLiningGlobal::action(){
 				scene->forces.addForce(blockIDs[nextNode],-totalForceB);
 			}
 
-
-		
 		//std::cout<<"end of rock llining global"<<endl;
-			
-		
 	}
 
 	#if 0
@@ -290,33 +276,30 @@ void RockLiningGlobal::action(){
 			plotDirection.normalize();
 			Vector3r nodalMoment = moment[i]*plotDirection;
 			float m[3]={nodalMoment[0],nodalMoment[1],nodalMoment[2]};
-			
 
 			Vector3r axialForce = axialForces[i]*plotDirection; //axialForce tension is negative  (plotdirection is pointing inwards), tension is pointing outwards
 			float fa[3]={axialForce[0],axialForce[1],axialForce[2]};
-			
 
 			Vector3r shearForce = shearForces[i]*plotDirection;
 			float fs[3]={shearForce[0],shearForce[1],shearForce[2]};
-			
 //#endif
 			if(blockIDs[i] == blockIDs[nextID]  ){
 				m={0,0,0};
 				fa = {0,0,0};
 				fs = {0,0,0};
-			}		
+			}
 			if (Body::byId(blockIDs[i],scene)->isClumpMember()==true && Body::byId(blockIDs[nextID],scene)->isClumpMember()==true ){
 				if (Body::byId(blockIDs[i],scene)->clumpId == Body::byId(blockIDs[nextID],scene)->clumpId){
 					m={0,0,0};
 					fa = {0,0,0};
 					fs = {0,0,0};
-				} 
+				}
 
 			}
-			liningNodalMoment->INSERT_NEXT_TUPLE(m);	
+			liningNodalMoment->INSERT_NEXT_TUPLE(m);
 			liningAxialForce->INSERT_NEXT_TUPLE(fa);
 			liningShearForce->INSERT_NEXT_TUPLE(fs);
-			//lineSource->Update();		
+			//lineSource->Update();
   		}
 		//#endif
 
@@ -378,7 +361,7 @@ void RockLiningGlobal::action(){
 			if(nextID == blockNo){nextID = 0;}
 			State* state1 = Body::byId(blockIDs[i],scene)->state.get();
 			State* state2 = Body::byId(blockIDs[nextID],scene)->state.get();
-			PotentialBlock* pb=static_cast<PotentialBlock*>(Body::byId(blockIDs[i],scene)->shape.get()); 
+			PotentialBlock* pb=static_cast<PotentialBlock*>(Body::byId(blockIDs[i],scene)->shape.get());
 
 		  	Vector3r globalPoint1 = state1->pos;
 			Vector3r globalPoint2 = state2->pos;
@@ -394,7 +377,7 @@ void RockLiningGlobal::action(){
 			Vector3r midPoint = globalPoint1; //  0.5*(globalPoint1+globalPoint2);
 			pid[0] = liningNode->InsertNextPoint( midPoint[0],  midPoint[1],  midPoint[2]);
 			liningNodeCells->InsertNextCell(1,pid);
-			Vector3r plotDirection = midPoint; 
+			Vector3r plotDirection = midPoint;
 			plotDirection.normalize();
 			Vector3r nodalMoment = moment[i]*plotDirection;
 			float m[3]={(float)nodalMoment[0], (float)nodalMoment[1], (float)nodalMoment[2]};
@@ -418,9 +401,8 @@ void RockLiningGlobal::action(){
 			float pNi[3] = {(float)normalPideal[0], (float)normalPideal[1], (float)normalPideal[2]};
 			liningNormalPressureIdeal->INSERT_NEXT_TUPLE(pNi);
 //#endif
-			
-			
-			//lineSource->Update();		
+
+			//lineSource->Update();
   		}
 		//#endif
 
@@ -459,7 +441,7 @@ int RockLiningGlobal::insertNode(Vector3r pos, double mass, double intervalLengt
 	shared_ptr<BodyContainer>& bodies = scene->bodies;
 
 //std::cout<<"pos: "<<pos<<", mass: "<<mass<<", intervalLength: "<<intervalLength<<endl;
-	
+
 	shared_ptr<Body> body(new Body()); /* new body */
 	shared_ptr<PotentialBlock> pBlock(new PotentialBlock);
 	shared_ptr<Aabb> aabb(new Aabb);
@@ -474,7 +456,7 @@ int RockLiningGlobal::insertNode(Vector3r pos, double mass, double intervalLengt
 	pBlock->cohesion.push_back(interfaceCohesion);
 	pBlock->tension.push_back(interfaceTension);
 	pBlock->liningTensionGap = interfaceTension/interfaceStiffness;
-	const int body_size= bodies->size(); 
+	const int body_size= bodies->size();
 	int newID = body_size;
 	pBlock->id = newID;
 	aabb->color = Vector3r(0,1,0);
@@ -490,13 +472,13 @@ int RockLiningGlobal::insertNode(Vector3r pos, double mass, double intervalLengt
 //std::cout<<"mass: "<<mass<<endl;
 //std::cout<<"inertia: "<<body->state->inertia<<endl;
 
-	body->shape	= pBlock; 
-	body->bound	= aabb; 
-	body->material	= mat; 
+	body->shape	= pBlock;
+	body->bound	= aabb;
+	body->material	= mat;
 
 //	std::cout<<"pos: "<<pos<<endl;
 //std::cout<<"before insert"<<endl;
-	return bodies->insert(body);	
+	return bodies->insert(body);
 //std::cout<<"after insert"<<endl;
 
 }
@@ -506,18 +488,15 @@ Vector3r RockLiningGlobal::getNodeDistance(const PotentialBlock* /*cm1*/,const S
 	//Vector3r nodeDist = Vector3r(0,0,0.0);
 	Vector3r global1 = state1->ori*localPt1 + state1->pos;
 	Vector3r global2 = state2->ori*localPt2 + state2->pos;
-	
 	return (global2-global1);
-	
 }
 
 
 double RockLiningGlobal::evaluateFNoSphereVol(const PotentialBlock* s1,const State* state1, const Vector3r newTrial){
-	
-	Vector3r tempP1 = newTrial - state1->pos;  
+	Vector3r tempP1 = newTrial - state1->pos;
 	/* Direction cosines */
 	//state1.ori.normalize();
-	Vector3r localP1 = state1->ori.conjugate()*tempP1; 
+	Vector3r localP1 = state1->ori.conjugate()*tempP1;
 	Real x = localP1.x();
 	Real y = localP1.y();
 	Real z = localP1.z();
@@ -525,18 +504,16 @@ double RockLiningGlobal::evaluateFNoSphereVol(const PotentialBlock* s1,const Sta
 
 	Real r = s1->r;  int insideCount = 0;
 	for (int i=0; i<planeNo; i++){
-		Real plane = s1->a[i]*x + s1->b[i]*y + s1->c[i]*z - s1->d[i]-1.0002*r; //-pow(10,-10); 
+		Real plane = s1->a[i]*x + s1->b[i]*y + s1->c[i]*z - s1->d[i]-1.0002*r; //-pow(10,-10);
 		if (Mathr::Sign(plane)*1.0<0.0){
 			insideCount++;
 		}
 	}
-	
+
 	/* Complete potential particle */
 	Real f = 1.0;
 	if (insideCount == planeNo){ f = -1.0;}
-
 	return f;
-
 }
 
 
@@ -544,7 +521,7 @@ double RockLiningGlobal::evaluateFNoSphereVol(const PotentialBlock* s1,const Sta
 bool RockLiningGlobal::installLining(const PotentialBlock* s1,const State* state1,const Vector3r startingPt,const Vector3r direction, const double length, Vector3r& intersectionPt){
 
     //Vector3r endPt = startingPt + length*direction;
-  
+
  // PotentialBlock *s1=static_cast<PotentialBlock*>(cm1.get());
   int planeNoA = s1->a.size();
 
@@ -558,20 +535,20 @@ bool RockLiningGlobal::installLining(const PotentialBlock* s1,const State* state
 // Ax - d < 0
 
 /* Variables to keep things neat */
-  int NUMCON = 3 /* equality */ + planeNoA /*block inequality */; 
-  int NUMVAR = 3/*3D */ + 1 /*t */+ 1 /* s */; 
+  int NUMCON = 3 /* equality */ + planeNoA /*block inequality */;
+  int NUMVAR = 3/*3D */ + 1 /*t */+ 1 /* s */;
   double s = 0.0;
   //bool converge = true;
 
-  Matrix3r Q1 = (state1->ori.conjugate()).toRotationMatrix(); 
+  Matrix3r Q1 = (state1->ori.conjugate()).toRotationMatrix();
   Eigen::MatrixXd A1 = Eigen::MatrixXd::Zero(planeNoA,3);
   for (int i=0; i < planeNoA; i++){
 	A1(i,0) = s1->a[i]; A1(i,1) = s1->b[i]; A1(i,2) = s1->c[i];
   }
   Eigen::MatrixXd AQ1 = A1*Q1;
-  Eigen::MatrixXd pos1(3,1); 
-  pos1(0,0) = state1->pos.x(); 
-  pos1(1,0) = state1->pos.y(); 
+  Eigen::MatrixXd pos1(3,1);
+  pos1(0,0) = state1->pos.x();
+  pos1(1,0) = state1->pos.y();
   pos1(2,0) = state1->pos.z();
   Eigen::MatrixXd Q1pos1 = AQ1*pos1;
 
@@ -582,7 +559,7 @@ ClpSimplex  model2;
                int numberRows = NUMCON;
                int numberColumns = NUMVAR;
                // This is fully dense - but would not normally be so
-            
+
                // Arrays will be set to default values
               model2.resize(0, numberColumns);
 	model2.setObjectiveCoefficient(0,0.0);
@@ -590,23 +567,18 @@ ClpSimplex  model2;
 	model2.setObjectiveCoefficient(2,0.0);
 	model2.setObjectiveCoefficient(3,0.0);
 	model2.setObjectiveCoefficient(4,1.0);
-              
+
 		for (int k = 0; k < 3; k++){
 		    model2.setColumnLower(k,-COIN_DBL_MAX);
-		    model2.setColumnUpper(k,COIN_DBL_MAX);                   
+		    model2.setColumnUpper(k,COIN_DBL_MAX);
 		}
                 model2.setColumnLower(3,openingRad);
-		   model2.setColumnUpper(3,length); 
+		   model2.setColumnUpper(3,length);
 		 model2.setColumnLower(4,-COIN_DBL_MAX);
-		    model2.setColumnUpper(4,COIN_DBL_MAX);  
-               // Rows
+		    model2.setColumnUpper(4,COIN_DBL_MAX);
+	// Rows
 	double rowLower[numberRows];
 	double rowUpper[numberRows];
-
-  
-
-
-  
 
 
 
@@ -619,8 +591,8 @@ rowUpper[1] = startingPt.y();
 rowUpper[2] = startingPt.z();
 
 for (int k = 0; k < planeNoA; k++){
-		    rowLower[3+k]= -COIN_DBL_MAX;                    
-		    rowUpper[3+k] = s1->d[k] + s1->r + Q1pos1(k,0); 
+	rowLower[3+k]= -COIN_DBL_MAX;
+	rowUpper[3+k] = s1->d[k] + s1->r + Q1pos1(k,0);
 }
 
 int row1Index[] = {0,3};
@@ -645,14 +617,13 @@ for (int i=0; i<planeNoA; i++){
 model2.scaling(0);
 model2.setLogLevel(0);
 model2.primal();
-          
 
           // Alternatively getColSolution()
           double * columnPrimal = model2.primalColumnSolution();
 
 
 	 Vector3r temp = Vector3r(columnPrimal[0],columnPrimal[1],columnPrimal[2]);
-	 intersectionPt = temp; //state1->ori.conjugate()*(temp-state1->pos); 
+	 intersectionPt = temp; //state1->ori.conjugate()*(temp-state1->pos);
 	 s = columnPrimal[4];
 
  int convergeSuccess = model2.status();
@@ -662,21 +633,19 @@ model2.primal();
 	 return true;
    }
 
-   
-
 }
 
 
 bool RockLiningGlobal::intersectPlane(const PotentialBlock* s1,const State* state1,const Vector3r startingPt,const Vector3r direction, const double length, Vector3r& intersectionPt, const Vector3r plane, const double planeD){
   //bool feasible = true;
   //Vector3r endPt = startingPt + length*direction;
-  
+
  // PotentialBlock *s1=static_cast<PotentialBlock*>(cm1.get());
   //int planeNoA = s1->a.size();
 
 /* Variables to keep things neat */
-  int NUMCON = 3 /* equality */ + 1 /*planeEquality */; 
-  int NUMVAR = 3/*3D */ + 1 /*t */; 
+  int NUMCON = 3 /* equality */ + 1 /*planeEquality */;
+  int NUMVAR = 3/*3D */ + 1 /*t */;
   double t = 0.0;
   //bool converge = true;
 
@@ -688,15 +657,14 @@ bool RockLiningGlobal::intersectPlane(const PotentialBlock* s1,const State* stat
 /* linear equality for blocks */
 // Ax - d = 0
 /* LINEAR CONSTRAINTS */
-  
-  
+
 ClpSimplex  model2;
        model2.setOptimizationDirection(1);
  // Create space for 3 columns and 10000 rows
                int numberRows = NUMCON;
                int numberColumns = NUMVAR;
                // This is fully dense - but would not normally be so
-            
+
                // Arrays will be set to default values
               model2.resize(0, numberColumns);
 	model2.setObjectiveCoefficient(0,0.0);
@@ -704,21 +672,14 @@ ClpSimplex  model2;
 	model2.setObjectiveCoefficient(2,0.0);
 	model2.setObjectiveCoefficient(3,1.0);
 
-              
 		for (int k = 0; k < 4; k++){
 		    model2.setColumnLower(k,-COIN_DBL_MAX);
-		    model2.setColumnUpper(k,COIN_DBL_MAX);                   
+		    model2.setColumnUpper(k,COIN_DBL_MAX);
 		}
-                
-               // Rows
+
+	// Rows
 	double rowLower[numberRows];
 	double rowUpper[numberRows];
-
-  
-
-  
-
-
 
 rowLower[0] = startingPt.x();
 rowLower[1] = startingPt.y();
@@ -751,11 +712,9 @@ model2.primal();
           double * columnPrimal = model2.primalColumnSolution();
 
 
-	 Vector3r temp = Vector3r(columnPrimal[0],columnPrimal[1],columnPrimal[2]);
-	 intersectionPt = temp; //state1->ori.conjugate()*(temp-state1->pos); 
-	 t = columnPrimal[3];
-
- 
+	Vector3r temp = Vector3r(columnPrimal[0],columnPrimal[1],columnPrimal[2]);
+	intersectionPt = temp; //state1->ori.conjugate()*(temp-state1->pos);
+	t = columnPrimal[3];
 
 	 int convergeSuccess = model2.status();
 double f = evaluateFNoSphereVol(s1,state1,intersectionPt);
