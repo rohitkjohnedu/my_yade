cb5f5f69f12baa2195d7c67c37a9a5b245435351
Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
2019-10-14 01:03:18 +0100

    Remove trailing whitespaces

--- pkg/dem/RockLiningGlobal.hpp
+++ pkg/dem/RockLiningGlobal.hpp
@@ -46,7 +46,7 @@
 
 namespace yade { // Cannot have #include directive inside.
 
-class RockLiningGlobal: public PeriodicEngine{	
+class RockLiningGlobal: public PeriodicEngine{
 	protected:
 		double stiffnessMatrix[36];
 		//double * globalStiffnessMatrix;
@@ -57,7 +57,7 @@ class RockLiningGlobal: public PeriodicEngine{
 			Bolts(Vector3r pt1, Vector3r  pt2){startingPoint = pt1; endPoint=pt2; }
 			Vector3r startingPoint;
 			Vector3r endPoint;
-			
+
 			/* variables stored in sequence starting from the block closest to the opening */
 			vector<int> blockIDs; /*blocks intersected */
 			vector<Vector3r> localCoordinates; /*local coordinates inside blocks */
@@ -73,14 +73,14 @@ class RockLiningGlobal: public PeriodicEngine{
 		bool intersectPlane(const PotentialBlock* s1,const State* state1,const Vector3r startingPt,const Vector3r direction, const double length, Vector3r& intersectionPt, const Vector3r plane, const double planeD);
   		virtual void action(void);
   	YADE_CLASS_BASE_DOC_ATTRS_CTOR_PY(RockLiningGlobal,PeriodicEngine,"Engine recording potential blocks as surfaces into files with given periodicity.",
-		((bool,assembledKglobal,false ,,"global stiffness matrix"))	
-		((double,density,0.0 ,,"density"))	
+		((bool,assembledKglobal,false ,,"global stiffness matrix"))
+		((double,density,0.0 ,,"density"))
 		((double,lumpedMass,0.0 ,,"lumpedMass"))
-		((double,EA,0.0 ,,"EA"))	
+		((double,EA,0.0 ,,"EA"))
 		((double,EI,0.0 ,,"EI"))
-		((double,initOverlap,pow(10,-5),,"initialOverlap"))	
+		((double,initOverlap,pow(10,-5),,"initialOverlap"))
 		((double,expansionFactor,pow(10,-5),,"alpha deltaT"))
-		((double,contactLength,1.0 ,,"contactLength"))	
+		((double,contactLength,1.0 ,,"contactLength"))
 		((vector<double>, sigmaMax, ,,"sigma max"))
 		((vector<double>, sigmaMin, ,,"sigma min"))
 		((double,ElasticModulus,0.0 ,,"E"))
@@ -97,8 +97,8 @@ class RockLiningGlobal: public PeriodicEngine{
 		((Vector3r,startingPoint,Vector3r(0,0,0) ,,"startingPt"))
 		((vector<int>,blockIDs, ,,"ids"))
 		((vector<Vector3r>,localCoordinates, ,,"local coordinates of intersection"))
-		((vector<Vector3r>,refPos, ,,"initial u"))	
-		((vector<Vector3r>,refDir, ,,"initial v"))	
+		((vector<Vector3r>,refPos, ,,"initial u"))
+		((vector<Vector3r>,refDir, ,,"initial v"))
 		((vector<Quaternionr>,refOri, ,,"initial theta"))
 		((vector<double>,refAngle, ,,"initial theta"))
 		((vector<double>,moment, ,,"moment"))
@@ -112,13 +112,12 @@ class RockLiningGlobal: public PeriodicEngine{
 		((vector<bool>,ruptured, ,,"ruptured"))
 		((Real,axialMax,1000000000 ,,"maximum axial force"))
 		((Real,shearMax,1000000000 ,,"maximum shear force"))
-		((int,vtkIteratorInterval,10000 ,,"how often to print vtk"))	
-		((int,vtkRefTimeStep,1 ,,"first timestep to print vtk"))	
-		((string,fileName,,,"File prefix to save to"))	
+		((int,vtkIteratorInterval,10000 ,,"how often to print vtk"))
+		((int,vtkRefTimeStep,1 ,,"first timestep to print vtk"))
+		((string,fileName,,,"File prefix to save to"))
 		((string,name,,,"File prefix to save to"))
 		,
 			//globalStiffnessMatrix = new double[totalNodes*3*totalNodes*3];
-			
 		,
   	);
 
@@ -132,13 +131,13 @@ REGISTER_SERIALIZABLE(RockLiningGlobal);
 extern "C" {
 #endif
 
-/* LAPACK LU */	
+/* LAPACK LU */
 	//int dgesv(int varNo, int varNo2, double *H, int varNo3, int *pivot, double* g, int varNo4, int info){
 	 extern void dgesv_(const int *N, const int *nrhs, double *Hessian, const int *lda, int *ipiv, double *gradient, const int *ldb, int *info);
 	// int ans;
 	// dgesv_(&varNo, &varNo2, H, &varNo3, pivot,g, &varNo4, &ans);
 	// return ans;
-	//}	
+	//}
 
 /* LAPACK Cholesky */
 	extern void dpbsv_(const char *uplo, const int *n, const int *kd, const int *nrhs, double *AB, const int *ldab, double *B, const int *ldb, int *info);
@@ -146,12 +145,12 @@ extern "C" {
 /* LAPACK QR */
 	extern void dgels_(const char *Trans, const int *m, const int *n, const int *nrhs, double *A, const int *lda, double *B, const int *ldb, const double *work, const int *lwork, int *info);
 
-	
+
 /*BLAS */
 	extern void dgemm_(const char *transA, const char *transB, const int *m, const int *n, const int *k, const double *alpha, double *A, const int *lda, double *B, const int *ldb, const double *beta, double *C, const int *ldc);
 
 	extern void dgemv_(const char *trans, const int *m, const int *n, const double *alpha, double *A, const int *lda, double *x, const int *incx, const double *beta, double *y, const int *incy);
-		
+
 	extern void dcopy_(const int *N, double *x, const int *incx, double *y, const int *incy);
 
 	extern double ddot_(const int *N, double *x, const int *incx, double *y, const int *incy);
