39fa66cece2866057dde0edfa38be266e7d7f95c
dpkn31 <deepak.kn1990@gmail.com>
2019-10-16 14:34:56 +0200

    forward declaration of class Shape, subD shared_ptr instantiated by boost::make_shared

--- core/Scene.hpp
+++ core/Scene.hpp
@@ -22,14 +22,14 @@
 #ifdef YADE_MPI 
 	#include<mpi.h>
 #endif
-#include <core/Shape.hpp>
+
 #ifdef YADE_OPENMP
 	#include<omp.h>
 #endif
 
 
 namespace yade { // Cannot have #include directive inside.
-
+class Shape; 
 class Bound;
 #ifdef YADE_OPENGL
 	class OpenGLRenderer;
@@ -80,7 +80,7 @@ class Scene: public Serializable{
 		#endif 
 		
 		void postLoad(Scene&);
-
+		
 		boost::posix_time::ptime prevTime; //Time value on the previous step
 
 	// clang-format off
@@ -100,7 +100,7 @@ class Scene: public Serializable{
 		((Body::id_t,selectedBody,-1,,"Id of body that is selected by the user"))
 	#ifdef YADE_MPI
 		((int,subdomain,0,,"the subdomain this scene is assigned in MPI/domain decomposition."))
-		((shared_ptr<Shape>,subD,new Shape,,"subdomain (shape) attached to this proc.")) // Subdomain is not directly used, as it leads to circular dependecy issues. 
+		((shared_ptr<Shape>,subD,boost::make_shared<Shape>(),,"subdomain (shape) attached to this proc."))
 	#endif
 		((vector<string>,tags,,,"Arbitrary key=value associations (tags like mp3 tags: author, date, version, description etc.)"))
 		((vector<shared_ptr<Engine> >,engines,,Attr::hidden,"Engines sequence in the simulation."))
