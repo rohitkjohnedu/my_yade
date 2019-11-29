e11a95074113752538ba96cd7ade5601ed20873a
dpkn31 <deepak.kn1990@gmail.com>
2019-11-29 04:12:11 +0100

    correct vtkrec example

--- examples/mpi/vtkRecorderExample.py
+++ examples/mpi/vtkRecorderExample.py
@@ -33,9 +33,7 @@ O.engines=[
 	ForceResetter(),
 	InsertionSortCollider([
 		Bo1_Sphere_Aabb(),
-		Bo1_Box_Aabb(), 
-		Bo1_Subdomain_Aabb()
-	], label = 'collider'), # always add labels. 
+		Bo1_Box_Aabb()], label = 'collider'), # always add labels. 
 	InteractionLoop(
 		[Ig2_Sphere_Sphere_ScGeom(),Ig2_Box_Sphere_ScGeom()],
 		[Ip2_FrictMat_FrictMat_FrictPhys()],
@@ -44,7 +42,7 @@ O.engines=[
 	),
 	GlobalStiffnessTimeStepper(timestepSafetyCoefficient=0.3,  timeStepUpdateInterval=100, parallelMode=True, label = 'timeStepper'), 
 	NewtonIntegrator(damping=0.1,gravity = (0, -0.1, 0), label='newton'), 
-	VTKRecorderParallel(fileName='spheres/3d-vtk-', recorders=['spheres', 'intr', 'boxes'], iterPeriod=100), #use .pvtu to open spheres, .pvtp for ints, and .vtu for boxes.
+	VTKRecorder(fileName='spheres/3d-vtk-', recorders=['spheres', 'intr', 'boxes'], parallelMode=True,iterPeriod=500), #use .pvtu to open spheres, .pvtp for ints, and .vtu for boxes.
 ]
 
 
@@ -52,6 +50,9 @@ collider.verletDist = 1.5
 
 #########  RUN  ##########
 # customize mpy
+mp.ERASE_REMOTE_MASTER = True
+mp.USE_CPP_REALLOC = True
+mp.USE_CPP_INTERS = True 
 mp.DOMAIN_DECOMPOSITION= True
 mp.mpirun(NSTEPS)
 mp.mergeScene() 
