bc0a81947f5ddf803f3c2ca26ca30e9c50f047b2
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-05 23:34:03 +0100

    Add test scripts.

---
+++ py/tests/testMath.py
@@ -0,0 +1,114 @@
+#!/bin/sh
+# autopkgtest check for minieigen
+# (C) 2015 Anton Gladky <gladk@debian.org>
+# (C) 2019 Janek Kozicki
+
+set -e
+
+LIBTOTEST=${1}
+DEC_DIGITS=${2}
+
+WORKDIR=$(mktemp -d)
+trap "rm -rf $WORKDIR" 0 INT QUIT ABRT PIPE TERM
+cp ${LIBTOTEST}.so ${WORKDIR}
+cd ${WORKDIR}
+
+cat <<EOF > esuptest.py
+import unittest, math, sys, mpmath
+import ${LIBTOTEST} as mne
+
+class SimpleTests(unittest.TestCase):
+	def setUp(self):
+		mpmath.mp.dps=${DEC_DIGITS}+1
+		# tolerance = 1.2×10⁻ᵈ⁺¹, where ᵈ==${DEC_DIGITS}
+		# so basically we store one more decimal digit, and expect one less decimal digit. That amounts to ignoring one (actually two) least significant digits.
+		self.tolerance=(mpmath.mpf(10)**(-${DEC_DIGITS}+1))*mpmath.mpf("1.2")
+		self.bits=mpmath.ceil(mpmath.mpf(${DEC_DIGITS})/(mpmath.log(2)/mpmath.log(10)))+1
+		# mpmath has 5 more internal bits
+		self.expectedEpsilon=(2**5)*mpmath.eps()
+		if(${DEC_DIGITS} == 6): # exception for float
+			self.bits=24
+			self.expectedEpsilon=1.1920928955078125e-07
+		if(${DEC_DIGITS} == 15): # exception for double
+			self.bits=53
+			self.expectedEpsilon=2.220446049250313e-16
+		if(${DEC_DIGITS} == 18): # exception for long double
+			self.bits=64
+			self.expectedEpsilon=mpmath.mpf('1.0842021724855044e-19')
+		if(${DEC_DIGITS} == 33): # exception for float128
+			self.bits=113
+			self.expectedEpsilon=mpmath.mpf('1.925929944387235853055977942584926994e-34')
+		self.maxval=(mpmath.mpf(1)-self.expectedEpsilon)*mpmath.power(2,mne.max_exp2)
+	def checkRelativeError(self,a,b,tol=None):
+		if(tol != None):
+			self.assertLessEqual(abs( (mpmath.mpf(a)-mpmath.mpf(b))/mpmath.mpf(b) ),tol)
+		else:
+			self.assertLessEqual(abs( (mpmath.mpf(a)-mpmath.mpf(b))/mpmath.mpf(b) ),self.tolerance)
+	def testSimple(self):
+		self.assertEqual(mne.defprec , self.bits )
+		zz=mpmath.acos(0)
+		print(zz.__repr__())
+		print("zz:",hex(id(zz)))
+		print("mpmath:",hex(id(mpmath)))
+		a=mne.Var()
+		a.val=zz
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		print("---- a.val=",a.val.__repr__())
+		print("---- zz   =",zz   .__repr__())
+		print("---- DPS  =",mpmath.mp.dps)
+		print("---- abs  =",abs(mpmath.mpf(a.val-zz)))
+		print("---- 10** =",self.tolerance)
+		self.checkRelativeError(a.val,zz)
+		self.assertEqual(mne.IsInteger, 0 )
+		self.assertEqual(mne.IsSigned, 1 )
+		self.assertEqual(mne.IsComplex,  0)
+		if(self.bits >= 100):
+			self.assertEqual(mne.RequireInitialization, 1 )
+		else:
+			self.assertEqual(mne.RequireInitialization, 0 )
+		self.assertGreaterEqual(mne.ReadCost, 1)
+		self.assertGreaterEqual(mne.AddCost, 1)
+		self.assertGreaterEqual(mne.MulCost, 1)
+		self.checkRelativeError(mne.highest(),self.maxval,2.1)
+		self.checkRelativeError(-mne.lowest(),self.maxval,2.1)
+		self.checkRelativeError(mne.Pi(),mpmath.pi)
+		self.checkRelativeError(mne.Euler(),mpmath.euler)
+		self.checkRelativeError(mne.Log2(),mpmath.log(2))
+		self.checkRelativeError(mne.Catalan(),mpmath.catalan)
+		self.checkRelativeError(mne.epsilon(),self.expectedEpsilon)
+		if(${DEC_DIGITS} == 6): # exception for float
+			self.assertLessEqual(mne.dummy_precision(),10e-6)
+		else:
+			self.checkRelativeError(mpmath.log(mne.dummy_precision()/mne.epsilon())/mpmath.log(10) , mpmath.mpf(${DEC_DIGITS})/10 , 1.5 )
+		for x in range(50):
+			if(str("${LIBTOTEST}")[-2:] == "nb" ): # this looks like a bug in /usr/include/eigen3/unsupported/Eigen/MPRealSupport !
+				self.assertLessEqual(abs(mne.random()-0.5),0.5)
+			else:
+				self.assertLessEqual(abs(mne.random()    ),1.0)
+		for a in range(5):
+			for bb in range(5):
+				b = bb*10
+				r = mne.random(a,a+b+1)
+				#print("random=",r)
+				self.assertLessEqual(r,a+b+1)
+				self.assertGreaterEqual(r,a)
+				self.assertFalse(mne.isMuchSmallerThan(r,1,mne.epsilon()))
+				self.assertTrue(mne.isMuchSmallerThan(self.expectedEpsilon,1+r,mne.epsilon()))
+				self.assertTrue(mne.isEqualFuzzy(r+self.expectedEpsilon*0.7,r,mne.epsilon()))
+				self.checkRelativeError(mne.toLongDouble(r),float(r), 1e-14) # FIXME - should be 1e-17, but python does not support that
+				self.checkRelativeError(mne.toDouble(r),float(r), 1e-14)
+				self.checkRelativeError(mne.toDouble(r),float(r), 1e-14)
+				self.assertEqual(mne.toLong(r),int(r))
+				self.assertEqual(mne.toInt(r),int(r))
+
+if __name__ == '__main__':
+		unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout, verbosity=2))
+EOF
+
+echo 'Test Python3'
+python3 esuptest.py
+echo "Python3 run: OK"
+
+cd
+rm -rf ${WORKDIR}
+
---
+++ py/tests/testMinieigenHP.py
@@ -0,0 +1,138 @@
+#!/bin/sh
+# autopkgtest check for minieigen
+# (C) 2015 Anton Gladky <gladk@debian.org>
+# (C) 2019 Janek Kozicki
+
+set -e
+
+LIBTOTEST=${1}
+DEC_DIGITS=${2}
+
+WORKDIR=$(mktemp -d)
+trap "rm -rf $WORKDIR" 0 INT QUIT ABRT PIPE TERM
+cp ${LIBTOTEST}.so ${WORKDIR}
+cd ${WORKDIR}
+
+cat <<EOF > minieigentest.py
+import unittest, math, sys, mpmath
+import ${LIBTOTEST} as mne
+
+class SimpleTests(unittest.TestCase):
+	def setUp(self):
+		mpmath.mp.dps=${DEC_DIGITS}+1
+		# tolerance = 1.001×10⁻ᵈ⁺¹, where ᵈ==${DEC_DIGITS}
+		# so basically we store one more decimal digit, and expect one less decimal digit. That amounts to ignoring one (actually two) least significant digits.
+		self.tolerance=(mpmath.mpf(10)**(-${DEC_DIGITS}+1))*mpmath.mpf("1.001")
+	def checkRelativeError(self,a,b):
+		self.assertLessEqual(abs( (mpmath.mpf(a)-mpmath.mpf(b))/mpmath.mpf(b) ),self.tolerance)
+	def testVector2i(self):
+		a2i = mne.Vector2i(2,1)
+		b2i = mne.Vector2i(3,5)
+		c2i = a2i + b2i
+
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( c2i[0] , mpmath.mpf("5") )
+		self.checkRelativeError( c2i[1] , mpmath.mpf("6") )
+
+		c2i *= 2
+
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( c2i[0] , mpmath.mpf("10") )
+		self.checkRelativeError( c2i[1] , mpmath.mpf("12") )
+
+	def testVector3(self):
+		a3r = mne.Vector3(2.1,1.1,4.3)
+		b3r = mne.Vector3(3.1,5.1,5.2)
+		c3r = a3r + b3r
+
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( c3r[0] , mpmath.mpf("5.2") )
+		self.checkRelativeError( c3r[1] , mpmath.mpf("6.2") )
+		self.checkRelativeError( c3r[2] , mpmath.mpf("9.5") )
+
+		c3r *= 3
+
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( c3r[0] , mpmath.mpf("15.6") )
+		self.checkRelativeError( c3r[1] , mpmath.mpf("18.6") )
+		self.checkRelativeError( c3r[2] , mpmath.mpf("28.5") )
+
+	def testMatrix3Test(self):
+		ll=1
+		print("------------------------------------------------------",ll);ll+=1;
+		a3m=mne.Matrix3(1,2,3,
+		                4,5,6,
+		                7,8,9)
+		print("------------------------------------------------------",ll);ll+=1;
+		b3m=a3m.transpose()
+		print("------------------------------------------------------",ll);ll+=1;
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		print("------------------------------------------------------",ll);ll+=1;
+		aaaaa=b3m[0][0]
+		print("------------------------------------------------------",ll,aaaaa);ll+=1;
+		print(b3m[0][0])
+		print("------------------------------------------------------",ll);ll+=1;
+		self.checkRelativeError( b3m[0][0] , mpmath.mpf("1") )
+		print("------------------------------------------------------",ll);ll+=1;
+		self.checkRelativeError( b3m[0][1] , mpmath.mpf("4") )
+		self.checkRelativeError( b3m[0][2] , mpmath.mpf("7") )
+		print("------------------------------------------------------",ll);ll+=1;
+		self.checkRelativeError( b3m[1][0] , mpmath.mpf("2") )
+		self.checkRelativeError( b3m[1][1] , mpmath.mpf("5") )
+		self.checkRelativeError( b3m[1][2] , mpmath.mpf("8") )
+		print("------------------------------------------------------",ll);ll+=1;
+		self.checkRelativeError( b3m[2][0] , mpmath.mpf("3") )
+		self.checkRelativeError( b3m[2][1] , mpmath.mpf("6") )
+		print("------------------------------------------------------",ll);ll+=1;
+		self.checkRelativeError( b3m[2][2] , mpmath.mpf("9") )
+
+		c3m=a3m.diagonal()
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( c3m[0] , mpmath.mpf("1") )
+		self.checkRelativeError( c3m[1] , mpmath.mpf("5") )
+		self.checkRelativeError( c3m[2] , mpmath.mpf("9") )
+
+		self.checkRelativeError( a3m.maxAbsCoeff() , mpmath.mpf("9") )
+
+
+	def testQuaternion(self):
+		ll=1
+		print("------------------------------------------------------",ll);ll+=1;
+		q1 = mne.Quaternion.Identity
+		print("------------------------------------------------------",ll,q1);ll+=1;
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( q1[3] , mpmath.mpf("1") )
+		print("------------------------------------------------------",ll);ll+=1;
+
+		q2 = q1.inverse()
+		self.checkRelativeError( q2[3] , mpmath.mpf("1") )
+		print("------------------------------------------------------",ll);ll+=1;
+		q3=mne.Quaternion(axis=mne.Vector3(1,0,0),angle=mpmath.pi/2.0)
+		m3q=q3.toRotationMatrix()
+		self.checkRelativeError( m3q[0][0] , mpmath.mpf("1") )
+		print(m3q[1][2].__repr__())
+		print("------------------------------------------------------",ll);ll+=1;
+		self.checkRelativeError( m3q[1][2] , mpmath.mpf("-1") )
+		print(q3)
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+
+		print("------------------------------------------------------",ll);ll+=1;
+		q4 = mne.Quaternion.Identity
+		q4.setFromTwoVectors(mne.Vector3(1,2,3),mne.Vector3(2,3,4))
+		print(q4.norm().__repr__())
+		print("------------------------------------------------------",ll);ll+=1;
+		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.checkRelativeError( q4.norm() , mpmath.mpf("1") )
+		print("------------------------------------------------------",ll);ll+=1;
+
+if __name__ == '__main__':
+		unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout, verbosity=2))
+EOF
+
+echo 'Test Python3'
+python3 minieigentest.py
+echo "Python3 run: OK"
+
+cd
+rm -rf ${WORKDIR}
+
