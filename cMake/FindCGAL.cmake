6d5e1cbc63f356cb8bd9a5cfb3c1b427a19c3100
Anton Gladky <gladk@debian.org>
2019-12-12 16:39:54 +0100

    Revert "Simplify FindCGAL"
    This reverts commit a8e74eb9c6cd554740b56b22b09458c07c9d7875.

--- cMake/FindCGAL.cmake
+++ cMake/FindCGAL.cmake
@@ -7,26 +7,38 @@
 # This module defines
 #  CGAL_DEFINITIONS: compiler flags for compiling with CGAL
 #  CGAL_INCLUDE_DIR: where to find CGAL.h
+#  CGAL_LIBRARIES: the libraries needed to use CGAL
 #  CGAL_FOUND: if false, do not try to use CGAL
 
-IF(CGAL_INCLUDE_DIR)
+IF(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
     SET(CGAL_FOUND TRUE)
-ELSE(CGAL_INCLUDE_DIR)
+ELSE(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
 	FIND_PATH(CGAL_INCLUDE_DIR CGAL/basic.h
         /usr/include/
         /usr/local/include/
         $ENV{ProgramFiles}/CGAL/*/include/
         $ENV{SystemDrive}/CGAL/*/include/
     )
+    FIND_LIBRARY(CGAL_LIBRARIES NAMES CGAL libCGAL
+        PATHS
+        /usr/lib
+        /usr/local/lib
+        /usr/lib/CGAL
+        /usr/lib64
+        /usr/local/lib64
+        /usr/lib64/CGAL
+        $ENV{ProgramFiles}/CGAL/*/lib/ms
+        $ENV{SystemDrive}/CGAL/*/lib/ms
+    )
     
-    IF(CGAL_INCLUDE_DIR)
+    IF(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
         SET(CGAL_FOUND TRUE)
-        MESSAGE(STATUS "Found CGAL: ${CGAL_INCLUDE_DIR}")
+        MESSAGE(STATUS "Found CGAL: ${CGAL_INCLUDE_DIR}, ${CGAL_LIBRARIES}")
 	INCLUDE_DIRECTORIES(${CGAL_INCLUDE_DIR})
-    ELSE(CGAL_INCLUDE_DIR)
+    ELSE(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
         SET(CGAL_FOUND FALSE)
         MESSAGE(STATUS "CGAL not found.")
-    ENDIF(CGAL_INCLUDE_DIR)
+    ENDIF(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
     
-    MARK_AS_ADVANCED(CGAL_INCLUDE_DIR)
-ENDIF(CGAL_INCLUDE_DIR)
+    MARK_AS_ADVANCED(CGAL_INCLUDE_DIR CGAL_LIBRARIES)
+ENDIF(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
