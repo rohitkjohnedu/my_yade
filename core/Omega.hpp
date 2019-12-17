e7c6cd4bb5411c07f1b0befb1199ffa15560e073
Anton Gladky <gladk@debian.org>
2019-12-17 20:43:57 +0100

    Remove unneeded include

--- core/Omega.hpp
+++ core/Omega.hpp
@@ -21,7 +21,6 @@
 #include <lib/factory/ClassFactory.hpp>
 
 #include <lib/base/Singleton.hpp>
-#include <boost/thread/mutex.hpp>
 #include "SimulationFlow.hpp"
 
 namespace yade { // Cannot have #include directive inside.
