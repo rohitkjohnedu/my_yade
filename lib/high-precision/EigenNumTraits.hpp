dc38b8e62e3af810bac9144e035e473e78e4f108
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-21 12:55:25 +0100

    Add EigenNumTraits.hpp for Complex type.

--- lib/high-precision/EigenNumTraits.hpp
+++ lib/high-precision/EigenNumTraits.hpp
@@ -49,44 +49,92 @@ template <> struct NumTraits<EigenTraitsReal> : GenericNumTraits<EigenTraitsReal
 	static inline Real epsilon(long = get_default_prec) { return std::numeric_limits<UR>::epsilon(); }
 	static inline Real epsilon(const Real&) { return std::numeric_limits<UR>::epsilon(); }
 	static inline Real smallest_positive() { return std::numeric_limits<UR>::min(); }
-
-	//#ifdef MPREAL_HAVE_DYNAMIC_STD_NUMERIC_LIMITS
-	static inline int digits10(long = get_default_prec) { return std::numeric_limits<UR>::digits10; }
-	static inline int digits10(const Real&) { return std::numeric_limits<UR>::digits10; }
-	//#endif
-
+	static inline int  digits10(long = get_default_prec) { return std::numeric_limits<UR>::digits10; }
+	static inline int  digits10(const Real&) { return std::numeric_limits<UR>::digits10; }
 	static inline Real dummy_precision() { return epsilon() * ::yade::pow(Real(10), digits10() / Real(10)); }
 };
 
 namespace internal {
 	template <> inline EigenTraitsReal random<EigenTraitsReal>() { return ::yade::random(); }
-
 	template <> inline EigenTraitsReal random<EigenTraitsReal>(const EigenTraitsReal& a, const EigenTraitsReal& b)
 	{
 		return a + (b - a) * ::yade::random01();
 	}
-
 	inline bool isMuchSmallerThan(const EigenTraitsReal& a, const EigenTraitsReal& b, const EigenTraitsReal& eps)
 	{
 		return ::yade::abs(a) <= ::yade::abs(b) * eps;
 	}
-
 	inline bool isEqualFuzzy(const EigenTraitsReal& a, const EigenTraitsReal& b, const EigenTraitsReal& eps) { return ::yade::abs(a - b) <= eps; }
 	inline bool isApprox(const EigenTraitsReal& a, const EigenTraitsReal& b, const EigenTraitsReal& eps) { return isEqualFuzzy(a, b, eps); }
-
 	inline bool isApproxOrLessThan(const EigenTraitsReal& a, const EigenTraitsReal& b, const EigenTraitsReal& eps)
 	{
 		return a <= b || isEqualFuzzy(a, b, eps);
 	}
-
-	template <> inline long double cast<EigenTraitsReal, long double>(const EigenTraitsReal& x) { return (long double)(x); /* x.toLDouble(); */ }
-
-	template <> inline double cast<EigenTraitsReal, double>(const EigenTraitsReal& x) { return double(x); /* x.toDouble(); */ }
-
-	template <> inline long cast<EigenTraitsReal, long>(const EigenTraitsReal& x) { return long(x); /* x.toLong(); */ }
-
-	template <> inline int cast<EigenTraitsReal, int>(const EigenTraitsReal& x) { return int(x); /* int(x.toLong()); */ }
+	template <> inline long double cast<EigenTraitsReal, long double>(const EigenTraitsReal& x) { return (long double)(x); }
+	template <> inline double      cast<EigenTraitsReal, double>(const EigenTraitsReal& x) { return double(x); }
+	template <> inline long        cast<EigenTraitsReal, long>(const EigenTraitsReal& x) { return long(x); }
+	template <> inline int         cast<EigenTraitsReal, int>(const EigenTraitsReal& x) { return int(x); }
 } // end namespace internal
+
+/*************************************************************************/
+/*************************       Complex        **************************/
+/*************************************************************************/
+
+template <> struct NumTraits<EigenTraitsComplex> : GenericNumTraits<EigenTraitsComplex> { // NOTE: Don't include this file for float, double, long double.
+	enum { IsInteger             = 0,
+	       IsSigned              = 1,
+	       IsComplex             = 1,
+	       RequireInitialization = 1,
+	       ReadCost              = 2 * ::EigenCostReal::ReadCost,
+	       AddCost               = 2 * ::EigenCostReal::AddCost,
+	       MulCost               = 4 * ::EigenCostReal::MulCost + 2 * ::EigenCostReal::AddCost };
+
+	typedef EigenTraitsComplex::value_type Real;
+	typedef EigenTraitsComplex             NonInteger;
+
+	static constexpr long get_default_prec = std::numeric_limits<Real>::digits;
+
+	static inline EigenTraitsComplex highest(long = get_default_prec) { return std::numeric_limits<Real>::max(); }
+	static inline EigenTraitsComplex lowest(long = get_default_prec) { return std::numeric_limits<Real>::lowest(); }
+
+	// Constants
+	static inline EigenTraitsComplex Pi(long = get_default_prec) { return boost::math::constants::pi<Real>(); }
+	static inline EigenTraitsComplex Euler(long = get_default_prec) { return boost::math::constants::euler<Real>(); }
+	static inline EigenTraitsComplex Log2(long = get_default_prec) { return ::yade::log(Real(2)); }
+	static inline EigenTraitsComplex Catalan(long = get_default_prec) { return boost::math::constants::catalan<Real>(); }
+
+	static inline EigenTraitsComplex epsilon(long = get_default_prec) { return std::numeric_limits<Real>::epsilon(); }
+	static inline EigenTraitsComplex epsilon(const EigenTraitsComplex&) { return std::numeric_limits<Real>::epsilon(); }
+	static inline EigenTraitsComplex smallest_positive() { return std::numeric_limits<Real>::min(); }
+	static inline int                digits10(long = get_default_prec) { return std::numeric_limits<Real>::digits10; }
+	static inline int                digits10(const EigenTraitsComplex&) { return std::numeric_limits<Real>::digits10; }
+	static inline EigenTraitsComplex dummy_precision() { return epsilon() * ::yade::pow(Real(10), digits10() / Real(10)); }
+};
+
+/*
+namespace internal {
+	template <> inline EigenTraitsComplex random<EigenTraitsComplex>() { return ::yade::random(); }
+	template <> inline EigenTraitsComplex random<EigenTraitsComplex>(const EigenTraitsComplex& a, const EigenTraitsComplex& b)
+	{
+		return a + (b - a) * ::yade::random01();
+	}
+	inline bool isMuchSmallerThan(const EigenTraitsComplex& a, const EigenTraitsComplex& b, const EigenTraitsComplex& eps)
+	{
+		return ::yade::abs(a) <= ::yade::abs(b) * eps;
+	}
+	inline bool isEqualFuzzy(const EigenTraitsComplex& a, const EigenTraitsComplex& b, const EigenTraitsComplex& eps) { return ::yade::abs(a - b) <= eps; }
+	inline bool isApprox(const EigenTraitsComplex& a, const EigenTraitsComplex& b, const EigenTraitsComplex& eps) { return isEqualFuzzy(a, b, eps); }
+	inline bool isApproxOrLessThan(const EigenTraitsComplex& a, const EigenTraitsComplex& b, const EigenTraitsComplex& eps)
+	{
+		return a <= b || isEqualFuzzy(a, b, eps);
+	}
+	template <> inline long double cast<EigenTraitsComplex, long double>(const EigenTraitsComplex& x) { return (long double)(x); }
+	template <> inline double      cast<EigenTraitsComplex, double>(const EigenTraitsComplex& x) { return double(x); }
+	template <> inline long        cast<EigenTraitsComplex, long>(const EigenTraitsComplex& x) { return long(x); }
+	template <> inline int         cast<EigenTraitsComplex, int>(const EigenTraitsComplex& x) { return int(x); }
+} // end namespace internal
+*/
+
 }
 
 /*
