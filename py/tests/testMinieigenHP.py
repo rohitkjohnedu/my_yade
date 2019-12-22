c31c72209700ac3bda5512821d4a1011ade257ca
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-22 19:44:39 +0100

    Set py/tests/testMinieigenHP.py to use yade.config.

--- py/tests/testMinieigenHP.py
+++ py/tests/testMinieigenHP.py
@@ -1,29 +1,20 @@
-#!/bin/bash
-# autopkgtest check for minieigen
+# -*- coding: utf-8 -*-
+# an extended version of autopkgtest check for minieigen
 # (C) 2015 Anton Gladky <gladk@debian.org>
 # (C) 2019 Janek Kozicki
 
-set -e
-
-LIBTOTEST=${1}
-DEC_DIGITS=${2}
-
-WORKDIR=$(mktemp -d)
-trap "rm -rf $WORKDIR" 0 INT QUIT ABRT PIPE TERM
-cp ${LIBTOTEST}.so ${WORKDIR}
-cd ${WORKDIR}
-
-cat <<EOF > minieigentest.py
 import unittest, math, sys, mpmath
 from mpmath import mpc
-import ${LIBTOTEST} as mne
+import minieigen as mne
+import yade
 
-class SimpleTests(unittest.TestCase):
+class ExtendedMinieigenTests(unittest.TestCase):
 	def setUp(self):
-		mpmath.mp.dps=${DEC_DIGITS}+1
-		# tolerance = 1.001×10⁻ᵈ⁺¹, where ᵈ==${DEC_DIGITS}
+		self.digs1=yade.config.highPrecisionDecimalPlaces+1
+		mpmath.mp.dps=self.digs1
+		# tolerance = 1.001×10⁻ᵈ⁺¹, where ᵈ==yade.config.highPrecisionDecimalPlaces
 		# so basically we store one more decimal digit, and expect one less decimal digit. That amounts to ignoring one (actually two) least significant digits.
-		self.tolerance=(mpmath.mpf(10)**(-${DEC_DIGITS}+1))*mpmath.mpf("1.001")
+		self.tolerance=(mpmath.mpf(10)**(-yade.config.highPrecisionDecimalPlaces+1))*mpmath.mpf("1.001")
 	def checkRelativeError(self,a,b):
 		if b!=0:
 			self.assertLessEqual(abs( (mpmath.mpf(a)-mpmath.mpf(b))/mpmath.mpf(b) ),self.tolerance)
@@ -38,17 +29,17 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c2i.eigenFlags(),352)
 		#self.assertEqual(c2i.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c2i[0] , mpmath.mpf("5") )
 		self.checkRelativeError( c2i[1] , mpmath.mpf("6") )
 
 		c2i *= 2
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c2i[0] , mpmath.mpf("10") )
 		self.checkRelativeError( c2i[1] , mpmath.mpf("12") )
 
-		from ${LIBTOTEST} import Vector2i
+		from minieigen import Vector2i
 		self.assertEqual( c2i , eval(c2i.__str__()) )
 
 	def testVector2(self):
@@ -58,17 +49,17 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c2.eigenFlags(),352)
 		#self.assertEqual(c2.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c2[0] , mpmath.mpf("5") )
 		self.checkRelativeError( c2[1] , mpmath.mpf("6") )
 
 		c2 *= 2
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c2[0] , mpmath.mpf("10") )
 		self.checkRelativeError( c2[1] , mpmath.mpf("12") )
 
-		from ${LIBTOTEST} import Vector2
+		from minieigen import Vector2
 		self.assertEqual( c2 , eval(c2.__str__()) )
 
 	def testVector2c(self):
@@ -78,17 +69,17 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c2c.eigenFlags(),352)
 		#self.assertEqual(c2c.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeComplexError( c2c[0] , mpmath.mpc("5","-10") )
 		self.checkRelativeComplexError( c2c[1] , mpmath.mpf("6") )
 
 		c2c *= 2
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeComplexError( c2c[0] , mpmath.mpc("10","-20") )
 		self.checkRelativeComplexError( c2c[1] , mpmath.mpf("12") )
 
-		from ${LIBTOTEST} import Vector2c
+		from minieigen import Vector2c
 		self.assertEqual( c2c , eval(c2c.__str__()) )
 
 	def testVector3i(self):
@@ -98,19 +89,19 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c3i.eigenFlags(),352)
 		#self.assertEqual(c3i.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3i[0] , mpmath.mpf("5") )
 		self.checkRelativeError( c3i[1] , mpmath.mpf("6") )
 		self.checkRelativeError( c3i[2] , mpmath.mpf("9") )
 
 		c3i *= 3
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3i[0] , mpmath.mpf("15") )
 		self.checkRelativeError( c3i[1] , mpmath.mpf("18") )
 		self.checkRelativeError( c3i[2] , mpmath.mpf("27") )
 
-		from ${LIBTOTEST} import Vector3i
+		from minieigen import Vector3i
 		self.assertEqual( c3i , eval(c3i.__str__()) )
 
 	def testVector3(self):
@@ -120,19 +111,19 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c3r.eigenFlags(),352)
 		#self.assertEqual(c3r.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3r[0] , mpmath.mpf("5.2") )
 		self.checkRelativeError( c3r[1] , mpmath.mpf("6.2") )
 		self.checkRelativeError( c3r[2] , mpmath.mpf("9.5") )
 
 		c3r *= 3
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3r[0] , mpmath.mpf("15.6") )
 		self.checkRelativeError( c3r[1] , mpmath.mpf("18.6") )
 		self.checkRelativeError( c3r[2] , mpmath.mpf("28.5") )
 
-		from ${LIBTOTEST} import Vector3
+		from minieigen import Vector3
 		self.checkRelativeError( c3r[0] , eval(c3r.__str__())[0] )
 		self.checkRelativeError( c3r[1] , eval(c3r.__str__())[1] )
 		self.checkRelativeError( c3r[2] , eval(c3r.__str__())[2] )
@@ -144,19 +135,19 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c3c.eigenFlags(),352)
 		#self.assertEqual(c3c.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeComplexError( c3c[0] , mpmath.mpc("5.2","1") )
 		self.checkRelativeComplexError( c3c[1] , mpmath.mpc("6.2","2.5") )
 		self.checkRelativeComplexError( c3c[2] , mpmath.mpc("9.5","-1") )
 
 		c3c *= 3
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeComplexError( c3c[0] , mpmath.mpc("15.6","3") )
 		self.checkRelativeComplexError( c3c[1] , mpmath.mpc("18.6","7.5") )
 		self.checkRelativeComplexError( c3c[2] , mpmath.mpc("28.5","-3") )
 
-		from ${LIBTOTEST} import Vector3c
+		from minieigen import Vector3c
 		self.checkRelativeComplexError( c3c[0] , eval(c3c.__str__())[0] )
 		self.checkRelativeComplexError( c3c[1] , eval(c3c.__str__())[1] )
 		self.checkRelativeComplexError( c3c[2] , eval(c3c.__str__())[2] )
@@ -169,19 +160,19 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c3a.eigenFlags(),352)
 		#self.assertEqual(c3a.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3a[0] , mpmath.mpf("5.2") )
 		self.checkRelativeError( c3a[1] , mpmath.mpf("6.2") )
 		self.checkRelativeError( c3a[2] , mpmath.mpf("9.5") )
 
 		c3a *= 3
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3a[0] , mpmath.mpf("15.6") )
 		self.checkRelativeError( c3a[1] , mpmath.mpf("18.6") )
 		self.checkRelativeError( c3a[2] , mpmath.mpf("28.5") )
 
-		from ${LIBTOTEST} import Vector3a
+		from minieigen import Vector3a
 		self.checkRelativeError( c3a[0] , eval(c3a.__str__())[0] )
 		self.checkRelativeError( c3a[1] , eval(c3a.__str__())[1] )
 		self.checkRelativeError( c3a[2] , eval(c3a.__str__())[2] )
@@ -193,7 +184,7 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(c4r.eigenFlags(),352)
 		#self.assertEqual(c4r.eigenStorageOrder(),0)
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c4r[0] , mpmath.mpf("5.2") )
 		self.checkRelativeError( c4r[1] , mpmath.mpf("6.2") )
 		self.checkRelativeError( c4r[2] , mpmath.mpf("9.5") )
@@ -201,13 +192,13 @@ class SimpleTests(unittest.TestCase):
 
 		c4r *= 3
 
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c4r[0] , mpmath.mpf("15.6") )
 		self.checkRelativeError( c4r[1] , mpmath.mpf("18.6") )
 		self.checkRelativeError( c4r[2] , mpmath.mpf("28.5") )
 		self.checkRelativeError( c4r[3] , mpmath.mpf("0.3") )
 
-		from ${LIBTOTEST} import Vector4
+		from minieigen import Vector4
 		self.checkRelativeError( c4r[0] , eval(c4r.__str__())[0] )
 		self.checkRelativeError( c4r[1] , eval(c4r.__str__())[1] )
 		self.checkRelativeError( c4r[2] , eval(c4r.__str__())[2] )
@@ -220,7 +211,7 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(a3m.eigenFlags(),352)
 		#self.assertEqual(a3m.eigenStorageOrder(),0)
 		b3m=a3m.transpose()
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( b3m[0][0] , mpmath.mpf("1") )
 		self.checkRelativeError( b3m[0][1] , mpmath.mpf("4") )
 		self.checkRelativeError( b3m[0][2] , mpmath.mpf("7") )
@@ -232,14 +223,14 @@ class SimpleTests(unittest.TestCase):
 		self.checkRelativeError( b3m[2][2] , mpmath.mpf("9") )
 
 		c3m=a3m.diagonal()
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( c3m[0] , mpmath.mpf("1") )
 		self.checkRelativeError( c3m[1] , mpmath.mpf("5") )
 		self.checkRelativeError( c3m[2] , mpmath.mpf("9") )
 
 		self.checkRelativeError( a3m.maxAbsCoeff() , mpmath.mpf("9") )
 
-		from ${LIBTOTEST} import Matrix3
+		from minieigen import Matrix3
 		for i in range(3):
 			for j in range(3):
 				self.checkRelativeError( b3m[i][j] , eval(b3m.__str__())[i][j] )
@@ -252,7 +243,7 @@ class SimpleTests(unittest.TestCase):
 		#self.assertEqual(a3m.eigenFlags(),352)
 		#self.assertEqual(a3m.eigenStorageOrder(),0)
 		b3m=a3m.transpose()
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeComplexError( b3m[0][0] , mpmath.mpc("1","1") )
 		self.checkRelativeComplexError( b3m[0][1] , mpmath.mpc("4","0") )
 		self.checkRelativeComplexError( b3m[0][2] , mpmath.mpc("7","0") )
@@ -264,14 +255,14 @@ class SimpleTests(unittest.TestCase):
 		self.checkRelativeComplexError( b3m[2][2] , mpmath.mpc("9","-9") )
 
 		c3m=a3m.diagonal()
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeComplexError( c3m[0] , mpmath.mpc("1","1") )
 		self.checkRelativeComplexError( c3m[1] , mpmath.mpc("5","0") )
 		self.checkRelativeComplexError( c3m[2] , mpmath.mpc("9","-9") )
 
 		self.checkRelativeComplexError( a3m.maxAbsCoeff() , abs(mpmath.mpc("9","-9")) )
 
-		from ${LIBTOTEST} import Matrix3c
+		from minieigen import Matrix3c
 		for i in range(3):
 			for j in range(3):
 				self.checkRelativeComplexError( b3m[i][j] , eval(b3m.__str__())[i][j] )
@@ -280,49 +271,34 @@ class SimpleTests(unittest.TestCase):
 
 	def testQuaternion(self):
 		q1 = mne.Quaternion.Identity
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( q1[3] , mpmath.mpf("1") )
 		#self.assertEqual(q1.eigenFlags(),32)
 		#self.assertEqual(q1.eigenStorageOrder(),0)
 
 		q2 = q1.inverse()
 		self.checkRelativeError( q2[3] , mpmath.mpf("1") )
-		q3=mne.Quaternion(axis=mne.Vector3(1,0,0),angle=mpmath.pi/2.0)
+		if(yade.config.highPrecisionMpmath):
+			q3=mne.Quaternion(axis=mne.Vector3(1,0,0),angle=mpmath.pi/2.0)
+		else:
+			q3=mne.Quaternion(axis=mne.Vector3(1,0,0),angle=1.570796326794896619231321691639)
 		m3q=q3.toRotationMatrix()
 		self.checkRelativeError( m3q[0][0] , mpmath.mpf("1") )
 		#print(m3q[1][2].__repr__())
 		self.checkRelativeError( m3q[1][2] , mpmath.mpf("-1") )
 		#print(q3)
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 
 		q4 = mne.Quaternion.Identity
 		q4.setFromTwoVectors(mne.Vector3(1,2,3),mne.Vector3(2,3,4))
 		#print(q4.norm().__repr__())
-		self.assertEqual(mpmath.mp.dps , ${DEC_DIGITS}+1 )
+		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( q4.norm() , mpmath.mpf("1") )
 
-		from ${LIBTOTEST} import Quaternion
+		from minieigen import Quaternion
 		self.checkRelativeError( q3[0] , eval(q3.__str__())[0] )
 		self.checkRelativeError( q3[1] , eval(q3.__str__())[1] )
 		self.checkRelativeError( q3[2] , eval(q3.__str__())[2] )
 		self.checkRelativeError( q3[3] , eval(q3.__str__())[3] )
 		#print(q3.__str__())
-
-if __name__ == '__main__':
-		unittest.main(testRunner=unittest.TextTestRunner(stream=sys.stdout, verbosity=2))
-EOF
-
-function handle_error() {
-	ls -la
-	gdb --batch -ex "bt full" `which python3` ./core
-	exit 1
-}
-
-echo 'Test Python3'
-ulimit -c unlimited
-python3 minieigentest.py || handle_error
-echo "Python3 run: OK"
-
-cd
-rm -rf ${WORKDIR}
 
