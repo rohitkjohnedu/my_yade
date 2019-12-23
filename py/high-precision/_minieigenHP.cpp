63ef2e1f9261b33985970a5c11e6e9d3ec80ab8a
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-23 18:57:03 +0100

    Add explaining comments in py/high-precision/_minieigenHP.cpp

--- py/high-precision/_minieigenHP.cpp
+++ py/high-precision/_minieigenHP.cpp
@@ -6,6 +6,12 @@
 *  GNU General Public License v2 or later. See file LICENSE for details. *
 *************************************************************************/
 
+// When yade uses high-precision number as Real type the usual (old) 'import minieigen'
+// has to be replaced with a minieigenHP library which uses exacly the same number of decimal places
+// as yade is using everywhere else. Please note that such precision can be very arbitrary, because cpp_bin_float
+// or mpfr take it as a compile-time argument. Hence such minieigenHP cannot be separately precompiled as a package.
+// Though it could be precompiled for some special types such as boost::multiprecision::float128
+
 #ifdef MINIEIGEN_OVERRIDE
 
 #include <lib/high-precision/Real.hpp>
