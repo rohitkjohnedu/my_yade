32c77beb35b6e0e8a16e66049449d75528be8020
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-24 16:53:15 +0100

    Improve comments in py/high-precision/math.py

--- py/high-precision/math.py
+++ py/high-precision/math.py
@@ -6,17 +6,17 @@
 #  GNU General Public License v2 or later. See file LICENSE for details. #
 ##########################################################################
 """
-This module exports all C++ math functions available in yade.
+This python module exposes all C++ math functions for Real and Complex type to python.
+In fact it sort of duplicates 'import math', 'import cmath' or 'import mpmath'.
+This module has three purposes:
+
+1. to reliably test all C++ math functions of arbitrary precision Real and Complex types against mpmath.
+2. to test Eigen NumTraits
+3. To allow writing python math code in a way that mirrors C++ math code in yade. As a bonus it
+   will be faster than mpmath because mpmath is a purely python library (which was one of the main
+   difficulties when writing lib/high-precision/ToFromPythonConverter.hpp)
+
 """
 # all C++ functions are accessible now:
 from yade._math import *
-
-def testPiPython():
-	"""
-	This function returns a string and python numerical representation of pi. They can be compared afterwards to see if precision conversion is handled correctly.
-
-	:return: ``(string(pi),pi)`` from python math.pi
-	"""
-	import math
-	return ( "%.16f" % math.pi , math.pi )
 
