d834794edd300132e022fe48ae50427e4c588f5c
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2019-12-25 16:38:02 +0100

    Vectorization: don't register AlignedVector3 twice in python. (1) as Vector3r (2) as Vector3a

--- py/high-precision/_ExposeVectors.cpp
+++ py/high-precision/_ExposeVectors.cpp
@@ -3,7 +3,20 @@
 #include <lib/high-precision/Real.hpp>
 #include <lib/high-precision/ToFromPythonConverter.hpp>
 using namespace ::yade::MathEigenTypes;
+
+#ifndef EIGEN_DONT_ALIGN
+// this is very funny. When compiling vecorized code the Vector3r is based on AlignedVector3. And then the line 'py::class_<Vector3ra>(…)' is trying to register
+// AlignedVector3 for the second time, because the Vector3r==AlignedVector3 is already registered. Which results in a runtime error.
+// So this define is only to avoid duplicate registration.
+#define EIGEN_DONT_ALIGN
+#define UNDEF_EIGEN_DONT_ALIGN
+#endif
+
 #include <minieigen/expose-vectors.cpp>
+
+#ifdef UNDEF_EIGEN_DONT_ALIGN
+#undef EIGEN_DONT_ALIGN
+#endif
 
 #endif
 
