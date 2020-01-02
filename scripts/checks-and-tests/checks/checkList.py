146d6de767da07baaddaff199f7f2e8ea2e3ebc2
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-01-02 20:36:11 +0100

    Print time spent on each of the --checks

--- scripts/checks-and-tests/checks/checkList.py
+++ scripts/checks-and-tests/checks/checkList.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 from past.builtins import execfile
 import yade,math,os,sys
+import time, datetime
 
 class YadeCheckError(Exception):
 	"""Use ``raise YadeCheckError("message") when check fails. Or throw any other python exception that seems suitable."""
@@ -10,6 +11,7 @@ class YadeCheckError(Exception):
 
 scriptsToRun=os.listdir(checksPath)
 failedScripts=list()
+maxElapsedTime=0
 
 #checkSpawn.py fails always for now, needs investigations
 skipScripts = ['checkList.py','checkSpawn.py']
@@ -26,8 +28,12 @@ for script in scriptsToRun:
 		print("###################################")
 		print("running: ",script)
 		try:
+			t0 = time.time()
 			execfile(checksPath+"/"+script)
-			print("Status: success")
+			t1 = time.time()
+			elapsedTime = t1-t0
+			maxElapsedTime = max(elapsedTime,maxElapsedTime)
+			print("Status:\033[92m success\033[0m, time spent on this check:"+("\033[92m " if elapsedTime<30 else "\033[91m ")+str(datetime.timedelta(seconds=elapsedTime))+"\033[0m")
 			print("___________________________________")
 		except Exception as e:
 			failedScripts.append(script)
@@ -36,6 +42,10 @@ for script in scriptsToRun:
 	elif (not mustCheck(script)):
 		print("###################################")
 		print("Skipping %s, because it is in SkipScripts"%script)
+
+if(maxElapsedTime > 30):
+	print("\033[95mWARNING: some checks took longer than 30 seconds.\033[0m")
+print("Most time spend on a single check: "+str(datetime.timedelta(seconds=maxElapsedTime)))
 
 if (len(failedScripts)!=0):
 	print('\033[91m', len(failedScripts) , " tests are failed"+'\033[0m')
