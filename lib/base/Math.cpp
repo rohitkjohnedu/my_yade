a6eeb1779e77c0fb8d042e4d178be078ecd2e9f8
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-01-10 20:38:50 +0100

    Don't calculate pi, use boost::math::constants::pi

--- lib/base/Math.cpp
+++ lib/base/Math.cpp
@@ -1,15 +1,18 @@
-#include<lib/base/Math.hpp>
+#include <lib/base/Math.hpp>
+#include <boost/math/constants/constants.hpp>
 
 namespace yade { // Cannot have #include directive inside.
 
-template<> const Real Math<Real>::EPSILON = DBL_EPSILON;
-template<> const Real Math<Real>::ZERO_TOLERANCE = 1e-20;
-template<> const Real Math<Real>::MAX_REAL = DBL_MAX;
-template<> const Real Math<Real>::PI = 4.0*atan(1.0);
-template<> const Real Math<Real>::TWO_PI = 2.0*Math<Real>::PI;
-template<> const Real Math<Real>::HALF_PI = 0.5*Math<Real>::PI;
-template<> const Real Math<Real>::DEG_TO_RAD = Math<Real>::PI/180.0;
-template<> const Real Math<Real>::RAD_TO_DEG = 180.0/Math<Real>::PI;
+// TODO: replace with numeric_limits.
+template <> const Real Math<Real>::EPSILON        = DBL_EPSILON;
+template <> const Real Math<Real>::ZERO_TOLERANCE = 1e-20;
+template <> const Real Math<Real>::MAX_REAL       = DBL_MAX;
+
+template <> const Real Math<Real>::PI             = boost::math::constants::pi<Real>();
+template <> const Real Math<Real>::TWO_PI         = boost::math::constants::two_pi<Real>();
+template <> const Real Math<Real>::HALF_PI        = boost::math::constants::half_pi<Real>();
+template <> const Real Math<Real>::DEG_TO_RAD     = Math<Real>::PI / Real(180);
+template <> const Real Math<Real>::RAD_TO_DEG     = Real(180) / Math<Real>::PI;
 
 template<> int ZeroInitializer<int>(){ return (int)0; }
 template<> Real ZeroInitializer<Real>(){ return (Real)0; }
