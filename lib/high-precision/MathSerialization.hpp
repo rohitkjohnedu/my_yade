40686cb758bb36944211223918941dc2a4284765
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-01-18 19:26:15 +0100

    Use ::yade::Real in MathSerialization.hpp

--- lib/high-precision/MathSerialization.hpp
+++ lib/high-precision/MathSerialization.hpp
@@ -67,7 +67,7 @@ namespace serialization {
 
 template<class Archive>
 void serialize(Archive & ar, yade::Vector2r & g, const unsigned int /*version*/){
-	Real &x=g[0], &y=g[1];
+	::yade::Real &x=g[0], &y=g[1];
 	ar & BOOST_SERIALIZATION_NVP(x) & BOOST_SERIALIZATION_NVP(y);
 }
 
@@ -80,7 +80,7 @@ void serialize(Archive & ar, yade::Vector2i & g, const unsigned int /*version*/)
 template<class Archive>
 void serialize(Archive & ar, yade::Vector3r & g, const unsigned int /*version*/)
 {
-	Real &x=g[0], &y=g[1], &z=g[2];
+	::yade::Real &x=g[0], &y=g[1], &z=g[2];
 	ar & BOOST_SERIALIZATION_NVP(x) & BOOST_SERIALIZATION_NVP(y) & BOOST_SERIALIZATION_NVP(z);
 }
 
@@ -92,7 +92,7 @@ void serialize(Archive & ar, yade::Vector3i & g, const unsigned int /*version*/)
 
 template<class Archive>
 void serialize(Archive & ar, yade::Vector6r & g, const unsigned int /*version*/){
-	Real &v0=g[0], &v1=g[1], &v2=g[2], &v3=g[3], &v4=g[4], &v5=g[5];
+	::yade::Real &v0=g[0], &v1=g[1], &v2=g[2], &v3=g[3], &v4=g[4], &v5=g[5];
 	ar & BOOST_SERIALIZATION_NVP(v0) & BOOST_SERIALIZATION_NVP(v1) & BOOST_SERIALIZATION_NVP(v2) & BOOST_SERIALIZATION_NVP(v3) & BOOST_SERIALIZATION_NVP(v4) & BOOST_SERIALIZATION_NVP(v5);
 }
 
@@ -105,7 +105,7 @@ void serialize(Archive & ar, yade::Vector6i & g, const unsigned int /*version*/)
 template<class Archive>
 void serialize(Archive & ar, yade::Quaternionr & g, const unsigned int /*version*/)
 {
-	Real &w=g.w(), &x=g.x(), &y=g.y(), &z=g.z();
+	::yade::Real &w=g.w(), &x=g.x(), &y=g.y(), &z=g.z();
 	ar & BOOST_SERIALIZATION_NVP(w) & BOOST_SERIALIZATION_NVP(x) & BOOST_SERIALIZATION_NVP(y) & BOOST_SERIALIZATION_NVP(z);
 }
 
@@ -117,7 +117,7 @@ void serialize(Archive & ar, yade::Se3r & g, const unsigned int /*version*/){
 
 template<class Archive>
 void serialize(Archive & ar, yade::Matrix3r & m, const unsigned int /*version*/){
-	Real &m00=m(0,0), &m01=m(0,1), &m02=m(0,2), &m10=m(1,0), &m11=m(1,1), &m12=m(1,2), &m20=m(2,0), &m21=m(2,1), &m22=m(2,2);
+	::yade::Real &m00=m(0,0), &m01=m(0,1), &m02=m(0,2), &m10=m(1,0), &m11=m(1,1), &m12=m(1,2), &m20=m(2,0), &m21=m(2,1), &m22=m(2,2);
 	ar & BOOST_SERIALIZATION_NVP(m00) & BOOST_SERIALIZATION_NVP(m01) & BOOST_SERIALIZATION_NVP(m02) &
 		BOOST_SERIALIZATION_NVP(m10) & BOOST_SERIALIZATION_NVP(m11) & BOOST_SERIALIZATION_NVP(m12) &
 		BOOST_SERIALIZATION_NVP(m20) & BOOST_SERIALIZATION_NVP(m21) & BOOST_SERIALIZATION_NVP(m22);
@@ -125,12 +125,12 @@ void serialize(Archive & ar, yade::Matrix3r & m, const unsigned int /*version*/)
 
 template<class Archive>
 void serialize(Archive & ar, yade::Matrix6r & m, const unsigned int /*version*/){
-	Real &m00=m(0,0), &m01=m(0,1), &m02=m(0,2), &m03=m(0,3), &m04=m(0,4), &m05=m(0,5);
-	Real &m10=m(1,0), &m11=m(1,1), &m12=m(1,2), &m13=m(1,3), &m14=m(1,4), &m15=m(1,5);
-	Real &m20=m(2,0), &m21=m(2,1), &m22=m(2,2), &m23=m(2,3), &m24=m(2,4), &m25=m(2,5);
-	Real &m30=m(3,0), &m31=m(3,1), &m32=m(3,2), &m33=m(3,3), &m34=m(3,4), &m35=m(3,5);
-	Real &m40=m(4,0), &m41=m(4,1), &m42=m(4,2), &m43=m(4,3), &m44=m(4,4), &m45=m(4,5);
-	Real &m50=m(5,0), &m51=m(5,1), &m52=m(5,2), &m53=m(5,3), &m54=m(5,4), &m55=m(5,5);
+	::yade::Real &m00=m(0,0), &m01=m(0,1), &m02=m(0,2), &m03=m(0,3), &m04=m(0,4), &m05=m(0,5);
+	::yade::Real &m10=m(1,0), &m11=m(1,1), &m12=m(1,2), &m13=m(1,3), &m14=m(1,4), &m15=m(1,5);
+	::yade::Real &m20=m(2,0), &m21=m(2,1), &m22=m(2,2), &m23=m(2,3), &m24=m(2,4), &m25=m(2,5);
+	::yade::Real &m30=m(3,0), &m31=m(3,1), &m32=m(3,2), &m33=m(3,3), &m34=m(3,4), &m35=m(3,5);
+	::yade::Real &m40=m(4,0), &m41=m(4,1), &m42=m(4,2), &m43=m(4,3), &m44=m(4,4), &m45=m(4,5);
+	::yade::Real &m50=m(5,0), &m51=m(5,1), &m52=m(5,2), &m53=m(5,3), &m54=m(5,4), &m55=m(5,5);
 	ar & BOOST_SERIALIZATION_NVP(m00) & BOOST_SERIALIZATION_NVP(m01) & BOOST_SERIALIZATION_NVP(m02) & BOOST_SERIALIZATION_NVP(m03) & BOOST_SERIALIZATION_NVP(m04) & BOOST_SERIALIZATION_NVP(m05) &
 	   BOOST_SERIALIZATION_NVP(m10) & BOOST_SERIALIZATION_NVP(m11) & BOOST_SERIALIZATION_NVP(m12) & BOOST_SERIALIZATION_NVP(m13) & BOOST_SERIALIZATION_NVP(m14) & BOOST_SERIALIZATION_NVP(m15) &
 	   BOOST_SERIALIZATION_NVP(m20) & BOOST_SERIALIZATION_NVP(m21) & BOOST_SERIALIZATION_NVP(m22) & BOOST_SERIALIZATION_NVP(m23) & BOOST_SERIALIZATION_NVP(m24) & BOOST_SERIALIZATION_NVP(m25) &
