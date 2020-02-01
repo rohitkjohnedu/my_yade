9f6b5836efec12f1ef24cc6bee222defa2ab3d75
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-02-01 20:39:20 +0000

    make_SSE is not using AlignedVector3 yet, so adjust tests appropriately.

--- py/tests/testMinieigenHP.py
+++ py/tests/testMinieigenHP.py
@@ -175,7 +175,7 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeComplexError( c3c[2] , eval(c3c.__str__())[2] )
 
 	def testVector3na(self):
-		if(mne.vectorize == False): return
+		if((mne.vectorize == False) or (not hasattr(mne, 'Vector3na'))): return
 		a3a = mne.Vector3na(2.1,1.1,4.3)
 		b3a = mne.Vector3na(3.1,5.1,5.2)
 		c3a = a3a + b3a
@@ -194,7 +194,7 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c3a[1] , mpmath.mpf("18.6") )
 		self.checkRelativeError( c3a[2] , mpmath.mpf("28.5") )
 
-		from yade.minieigenHP import Vector3na
+		from yade.minieigenHP import Vector3na as Vector3
 		self.checkRelativeError( c3a[0] , eval(c3a.__str__())[0] )
 		self.checkRelativeError( c3a[1] , eval(c3a.__str__())[1] )
 		self.checkRelativeError( c3a[2] , eval(c3a.__str__())[2] )
