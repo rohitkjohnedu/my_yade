bbaea183828cb04d07c056d270126650d64fe4ea
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-02-14 17:32:31 +0100

    Add yade.math.linspace(…) to facilitate transition of old python scripts to high-precision. More such functions are expected.

--- py/high-precision/math.py
+++ py/high-precision/math.py
@@ -6,25 +6,45 @@
 #  GNU General Public License v2 or later. See file LICENSE for details. #
 ##########################################################################
 """
-This python module exposes all C++ math functions for Real and Complex type to python.
+This python module exposes all C++ math functions for Real and Complex types to python.
 In fact it sort of duplicates ``import math``, ``import cmath`` or ``import mpmath``.
-This module has four purposes:
+Also it facilitates migration of old python scripts to high precision calculations.
 
-1. to reliably test all C++ math functions of arbitrary precision Real and Complex types against mpmath.
-2. to test Eigen NumTraits
-3. to test CGAL NumTraits
-4. To allow writing python math code in a way that mirrors C++ math code in yade. As a bonus it
+This module has following purposes:
+
+1. To reliably :ysrc:`test<py/tests/testMath.py>` all C++ math functions of arbitrary precision Real and Complex types against mpmath.
+2. To act as a "migration helper" for python scripts which call python mathematical functions that do not work
+   well with mpmath. As an example see :yref:`yade.math.linspace` below.
+3. To allow writing python math code in a way that mirrors C++ math code in yade. As a bonus it
    will be faster than mpmath because mpmath is a purely python library (which was one of the main
    difficulties when writing :ysrc:`lib/high-precision/ToFromPythonConverter.hpp`)
+4. To test Eigen NumTraits
+5. To test CGAL NumTraits
 
-If another math function is needed it should be added to following files:
+If another ``C++`` :ref:`math function<cpp-hp-compatibility>` is needed it should be added to following files:
 
 1. :ysrc:`lib/high-precision/MathFunctions.hpp`
 2. :ysrc:`py/high-precision/_math.cpp`
 3. :ysrc:`py/tests/testMath.py`
 4. :ysrc:`py/tests/testMathHelper.py`
 
+If another ``python`` math function does not work well with ``mpmath`` it should be added below, and original
+calls to this function should call this function instead, e.g. ``numpy.linspace(…)`` is replaced with ``yade.math.linspace(…)``.
+
 """
 # all C++ functions are accessible now:
 from yade._math import *
+
+import yade.config
+
+def linspace(a,b,num):
+	"""
+	This function calls ``numpy.linspace(…)`` or ``mpmath.linspace(…)``, because ``numpy.linspace`` function does not work with mpmath.
+	"""
+	if(yade.config.highPrecisionMpmath):
+		import mpmath
+		return mpmath.linspace(a,b,num)
+	else:
+		import numpy
+		return numpy.linspace(a,b,num=num)
 
