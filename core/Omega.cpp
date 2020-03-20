e721fa0750357c3ca89fb318ecee3495f43bccd9
clang-formatter <clang-formatter>
2020-03-20 20:08:36 +0100

    scripts/clang-formatter.sh ./core (3/7)

--- core/Omega.cpp
+++ core/Omega.cpp
@@ -10,147 +10,171 @@
 
 #include "Omega.hpp"
 #include "Scene.hpp"
-#include "TimeStepper.hpp"
 #include "ThreadRunner.hpp"
+#include "TimeStepper.hpp"
 #include <lib/base/Math.hpp>
 #include <lib/multimethods/FunctorWrapper.hpp>
 #include <lib/multimethods/Indexable.hpp>
 #include <lib/serialization/ObjectIO.hpp>
 
 #include <boost/algorithm/string.hpp>
-#include <boost/thread/mutex.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/thread/mutex.hpp>
 #include <cxxabi.h>
 
 SINGLETON_SELF(yade::Omega);
 
 namespace yade { // Cannot have #include directive inside.
 
-class RenderMutexLock: public std::lock_guard<std::mutex>{
-	public:
-	RenderMutexLock(): std::lock_guard<std::mutex>(Omega::instance().renderMutex){}
-	~RenderMutexLock(){}
+class RenderMutexLock : public std::lock_guard<std::mutex> {
+public:
+	RenderMutexLock()
+	        : std::lock_guard<std::mutex>(Omega::instance().renderMutex)
+	{
+	}
+	~RenderMutexLock() {}
 };
 
 CREATE_LOGGER(Omega);
 
-const std::map<string,DynlibDescriptor>& Omega::getDynlibsDescriptor(){return dynlibs;}
+const std::map<string, DynlibDescriptor>& Omega::getDynlibsDescriptor() { return dynlibs; }
 
-const shared_ptr<Scene>& Omega::getScene(){return scenes.at(currentSceneNb);}
-void Omega::resetCurrentScene(){ RenderMutexLock lock; scenes.at(currentSceneNb) = shared_ptr<Scene>(new Scene);}
-void Omega::resetScene(){ resetCurrentScene(); }
-void Omega::resetAllScenes(){
+const shared_ptr<Scene>& Omega::getScene() { return scenes.at(currentSceneNb); }
+void                     Omega::resetCurrentScene()
+{
+	RenderMutexLock lock;
+	scenes.at(currentSceneNb) = shared_ptr<Scene>(new Scene);
+}
+void Omega::resetScene() { resetCurrentScene(); }
+void Omega::resetAllScenes()
+{
 	RenderMutexLock lock;
 	scenes.resize(1);
-	scenes[0] = shared_ptr<Scene>(new Scene);
-	currentSceneNb=0;
+	scenes[0]      = shared_ptr<Scene>(new Scene);
+	currentSceneNb = 0;
 }
 
-int Omega::addScene(){
+int Omega::addScene()
+{
 	scenes.push_back(shared_ptr<Scene>(new Scene));
-	return scenes.size()-1;
+	return scenes.size() - 1;
 }
 
-void Omega::switchToScene(int i) {
-	if (i<0 || i>=int(scenes.size())) {
-		LOG_ERROR("Scene "<<i<<" has not been created yet, no switch.");
+void Omega::switchToScene(int i)
+{
+	if (i < 0 || i >= int(scenes.size())) {
+		LOG_ERROR("Scene " << i << " has not been created yet, no switch.");
 		return;
 	}
-	currentSceneNb=i;
+	currentSceneNb = i;
 }
 
-Real Omega::getRealTime(){
-	return (boost::posix_time::microsec_clock::local_time()-startupLocalTime).total_milliseconds()/1e3;
+Real Omega::getRealTime() { return (boost::posix_time::microsec_clock::local_time() - startupLocalTime).total_milliseconds() / 1e3; }
+
+boost::posix_time::time_duration Omega::getRealTime_duration() { return boost::posix_time::microsec_clock::local_time() - startupLocalTime; }
+
+void Omega::initTemps()
+{
+	char dirTemplate[] = "/tmp/yade-XXXXXX";
+	tmpFileDir         = mkdtemp(dirTemplate);
+	tmpFileCounter     = 0;
 }
 
-boost::posix_time::time_duration Omega::getRealTime_duration(){
-	return boost::posix_time::microsec_clock::local_time()-startupLocalTime;
-}
-
-void Omega::initTemps(){
-	char dirTemplate[]="/tmp/yade-XXXXXX";
-	tmpFileDir=mkdtemp(dirTemplate);
-	tmpFileCounter=0;
-}
-
-void Omega::cleanupTemps(){
+void Omega::cleanupTemps()
+{
 	stop();
 	boost::filesystem::path tmpPath(tmpFileDir);
 	boost::filesystem::remove_all(tmpPath);
 }
 
-std::string Omega::tmpFilename(){
-	if(tmpFileDir.empty()) throw runtime_error("tmpFileDir empty; Omega::initTemps not yet called()?");
+std::string Omega::tmpFilename()
+{
+	if (tmpFileDir.empty())
+		throw runtime_error("tmpFileDir empty; Omega::initTemps not yet called()?");
 	const std::lock_guard<std::mutex> lock(tmpFileCounterMutex);
-	return tmpFileDir+"/tmp-"+boost::lexical_cast<string>(tmpFileCounter++);
+	return tmpFileDir + "/tmp-" + boost::lexical_cast<string>(tmpFileCounter++);
 }
 
-void Omega::reset(){
+void Omega::reset()
+{
 	stop();
 	init();
 }
 
-void Omega::init(){
-	sceneFile="";
+void Omega::init()
+{
+	sceneFile = "";
 	resetAllScenes();
-	sceneAnother=shared_ptr<Scene>(new Scene);
+	sceneAnother = shared_ptr<Scene>(new Scene);
 	timeInit();
 	createSimulationLoop();
 }
 
-void Omega::timeInit(){
-	startupLocalTime=boost::posix_time::microsec_clock::local_time();
-}
+void Omega::timeInit() { startupLocalTime = boost::posix_time::microsec_clock::local_time(); }
 
-void Omega::createSimulationLoop(){
-	simulationLoop=shared_ptr<ThreadRunner>(new ThreadRunner(&simulationFlow_));
-}
-void Omega::stop(){
+void Omega::createSimulationLoop() { simulationLoop = shared_ptr<ThreadRunner>(new ThreadRunner(&simulationFlow_)); }
+void Omega::stop()
+{
 	LOG_DEBUG("");
-	if (simulationLoop&&simulationLoop->looping())simulationLoop->stop();
-	if (simulationLoop) simulationLoop=shared_ptr<ThreadRunner>();
+	if (simulationLoop && simulationLoop->looping())
+		simulationLoop->stop();
+	if (simulationLoop)
+		simulationLoop = shared_ptr<ThreadRunner>();
 }
 
 /* WARNING: even a single simulation step is run asynchronously; the call will return before the iteration is finished. */
-void Omega::step(){
-	if (simulationLoop){
+void Omega::step()
+{
+	if (simulationLoop) {
 		simulationLoop->spawnSingleAction();
 	}
 }
 
-void Omega::run(){
-	if(!simulationLoop){LOG_ERROR("No Omega::simulationLoop? Creating one (please report bug)."); createSimulationLoop(); }
-	if (simulationLoop && !simulationLoop->looping()){
+void Omega::run()
+{
+	if (!simulationLoop) {
+		LOG_ERROR("No Omega::simulationLoop? Creating one (please report bug).");
+		createSimulationLoop();
+	}
+	if (simulationLoop && !simulationLoop->looping()) {
 		simulationLoop->start();
 	}
 }
 
-void Omega::pause(){
-	if (simulationLoop && simulationLoop->looping()){
+void Omega::pause()
+{
+	if (simulationLoop && simulationLoop->looping()) {
 		simulationLoop->stop();
 	}
 }
 
-bool Omega::isRunning(){ if(simulationLoop) return simulationLoop->looping(); else return false; }
+bool Omega::isRunning()
+{
+	if (simulationLoop)
+		return simulationLoop->looping();
+	else
+		return false;
+}
 
-void Omega::buildDynlibDatabase(const vector<string>& dynlibsList){	
-	LOG_DEBUG("called with "<<dynlibsList.size()<<" plugins.");
-	boost::python::object wrapperScope=boost::python::import("yade.wrapper");
-	std::list<string> pythonables;
-	for(const auto & name : dynlibsList){
+void Omega::buildDynlibDatabase(const vector<string>& dynlibsList)
+{
+	LOG_DEBUG("called with " << dynlibsList.size() << " plugins.");
+	boost::python::object wrapperScope = boost::python::import("yade.wrapper");
+	std::list<string>     pythonables;
+	for (const auto& name : dynlibsList) {
 		shared_ptr<Factorable> f;
 		try {
-			LOG_DEBUG("Factoring plugin "<<name);
-			f = ClassFactory::instance().createShared(name);
-			dynlibs[name].isSerializable = ((YADE_PTR_DYN_CAST<Serializable>(f)).get()!=0);
-			for(int i=0;i<f->getBaseClassNumber();i++){
+			LOG_DEBUG("Factoring plugin " << name);
+			f                            = ClassFactory::instance().createShared(name);
+			dynlibs[name].isSerializable = ((YADE_PTR_DYN_CAST<Serializable>(f)).get() != 0);
+			for (int i = 0; i < f->getBaseClassNumber(); i++) {
 				dynlibs[name].baseClasses.insert(f->getBaseClassName(i));
 			}
-			if(dynlibs[name].isSerializable) pythonables.push_back(name);
-		}
-		catch (std::runtime_error& e){
+			if (dynlibs[name].isSerializable)
+				pythonables.push_back(name);
+		} catch (std::runtime_error& e) {
 			/* FIXME: this catches all errors! Some of them are not harmful, however:
-			 * when a class is not factorable, it is OK to skip it; */	
+			 * when a class is not factorable, it is OK to skip it; */
 		}
 	}
 	// handle Serializable specially
@@ -158,18 +182,21 @@ void Omega::buildDynlibDatabase(const vector<string>& dynlibsList){
 	/* python classes must be registered such that base classes come before derived ones;
 	for now, just loop until we succeed; proper solution will be to build graphs of classes
 	and traverse it from the top. It will be done once all classes are pythonable. */
-	for(int i=0; i<100 && pythonables.size()>0; i++){
-		if(getenv("YADE_DEBUG")) cerr<<endl<<"[[[ Round "<<i<<" ]]]: ";
+	for (int i = 0; i < 100 && pythonables.size() > 0; i++) {
+		if (getenv("YADE_DEBUG"))
+			cerr << endl << "[[[ Round " << i << " ]]]: ";
 		std::list<string> done;
-		for(std::list<string>::iterator I=pythonables.begin(); I!=pythonables.end(); ){
-			shared_ptr<Serializable> s=boost::static_pointer_cast<Serializable>(ClassFactory::instance().createShared(*I));
-			try{
-				if(getenv("YADE_DEBUG")) cerr<<"{{"<<*I<<"}}";
+		for (std::list<string>::iterator I = pythonables.begin(); I != pythonables.end();) {
+			shared_ptr<Serializable> s = boost::static_pointer_cast<Serializable>(ClassFactory::instance().createShared(*I));
+			try {
+				if (getenv("YADE_DEBUG"))
+					cerr << "{{" << *I << "}}";
 				s->pyRegisterClass(wrapperScope);
-				std::list<string>::iterator prev=I++;
+				std::list<string>::iterator prev = I++;
 				pythonables.erase(prev);
-			} catch (...){
-				if(getenv("YADE_DEBUG")) cerr<<"["<<*I<<"]";
+			} catch (...) {
+				if (getenv("YADE_DEBUG"))
+					cerr << "[" << *I << "]";
 				//PyErr_Print();
 				boost::python::handle_exception();
 				PyErr_Clear();
@@ -178,103 +205,124 @@ void Omega::buildDynlibDatabase(const vector<string>& dynlibsList){
 		}
 	}
 
-	std::map<string,DynlibDescriptor>::iterator dli    = dynlibs.begin();
-	std::map<string,DynlibDescriptor>::iterator dliEnd = dynlibs.end();
-	for( ; dli!=dliEnd ; ++dli){
+	std::map<string, DynlibDescriptor>::iterator dli    = dynlibs.begin();
+	std::map<string, DynlibDescriptor>::iterator dliEnd = dynlibs.end();
+	for (; dli != dliEnd; ++dli) {
 		std::set<string>::iterator bci    = (*dli).second.baseClasses.begin();
 		std::set<string>::iterator bciEnd = (*dli).second.baseClasses.end();
-		for( ; bci!=bciEnd ; ++bci){
+		for (; bci != bciEnd; ++bci) {
 			string name = *bci;
-			if (name=="Dispatcher1D" || name=="Dispatcher2D") (*dli).second.baseClasses.insert("Dispatcher");
-			else if (name=="Functor1D" || name=="Functor2D") (*dli).second.baseClasses.insert("Functor");
-			else if (name=="Serializable") (*dli).second.baseClasses.insert("Factorable");
-			else if (name!="Factorable" && name!="Indexable") {
+			if (name == "Dispatcher1D" || name == "Dispatcher2D")
+				(*dli).second.baseClasses.insert("Dispatcher");
+			else if (name == "Functor1D" || name == "Functor2D")
+				(*dli).second.baseClasses.insert("Functor");
+			else if (name == "Serializable")
+				(*dli).second.baseClasses.insert("Factorable");
+			else if (name != "Factorable" && name != "Indexable") {
 				shared_ptr<Factorable> f = ClassFactory::instance().createShared(name);
-				for(int i=0;i<f->getBaseClassNumber();i++)
+				for (int i = 0; i < f->getBaseClassNumber(); i++)
 					dynlibs[name].baseClasses.insert(f->getBaseClassName(i));
 			}
 		}
 	}
 }
 
-bool Omega::isInheritingFrom(const string& className, const string& baseClassName){
-	return (dynlibs[className].baseClasses.find(baseClassName)!=dynlibs[className].baseClasses.end());
+bool Omega::isInheritingFrom(const string& className, const string& baseClassName)
+{
+	return (dynlibs[className].baseClasses.find(baseClassName) != dynlibs[className].baseClasses.end());
 }
 
-bool Omega::isInheritingFrom_recursive(const string& className, const string& baseClassName){
-	if (dynlibs[className].baseClasses.find(baseClassName)!=dynlibs[className].baseClasses.end()) return true;
-	for (const auto & parent : dynlibs[className].baseClasses){
-		if(isInheritingFrom_recursive(parent,baseClassName)) return true;
+bool Omega::isInheritingFrom_recursive(const string& className, const string& baseClassName)
+{
+	if (dynlibs[className].baseClasses.find(baseClassName) != dynlibs[className].baseClasses.end())
+		return true;
+	for (const auto& parent : dynlibs[className].baseClasses) {
+		if (isInheritingFrom_recursive(parent, baseClassName))
+			return true;
 	}
 	return false;
 }
 
-void Omega::loadPlugins(vector<string> pluginFiles){
-	for (const auto & plugin : pluginFiles){
-		LOG_DEBUG("Loading plugin "<<plugin);
-		if(!ClassFactory::instance().load(plugin)){
-			string err=ClassFactory::instance().lastError();
-			if(err.find(": undefined symbol: ")!=std::string::npos){
-				size_t pos=err.rfind(":");	assert(pos!=std::string::npos);
-				std::string sym(err,pos+2); //2 removes ": " from the beginning
-				int status=0; char* demangled_sym=abi::__cxa_demangle(sym.c_str(),0,0,&status);
-				LOG_FATAL(plugin<<": undefined symbol `"<<demangled_sym<<"'"); LOG_FATAL(plugin<<": "<<err); LOG_FATAL("Bailing out.");
-			}
-			else {
-				LOG_FATAL(plugin<<": "<<err<<" ."); /* leave space to not to confuse c++filt */ LOG_FATAL("Bailing out.");
+void Omega::loadPlugins(vector<string> pluginFiles)
+{
+	for (const auto& plugin : pluginFiles) {
+		LOG_DEBUG("Loading plugin " << plugin);
+		if (!ClassFactory::instance().load(plugin)) {
+			string err = ClassFactory::instance().lastError();
+			if (err.find(": undefined symbol: ") != std::string::npos) {
+				size_t pos = err.rfind(":");
+				assert(pos != std::string::npos);
+				std::string sym(err, pos + 2); //2 removes ": " from the beginning
+				int         status        = 0;
+				char*       demangled_sym = abi::__cxa_demangle(sym.c_str(), 0, 0, &status);
+				LOG_FATAL(plugin << ": undefined symbol `" << demangled_sym << "'");
+				LOG_FATAL(plugin << ": " << err);
+				LOG_FATAL("Bailing out.");
+			} else {
+				LOG_FATAL(plugin << ": " << err << " ."); /* leave space to not to confuse c++filt */
+				LOG_FATAL("Bailing out.");
 			}
 			abort();
 		}
 	}
 	std::list<string>& plugins(ClassFactory::instance().pluginClasses);
-	plugins.sort(); plugins.unique();
-	buildDynlibDatabase(vector<string>(plugins.begin(),plugins.end()));
+	plugins.sort();
+	plugins.unique();
+	buildDynlibDatabase(vector<string>(plugins.begin(), plugins.end()));
 }
 
-void Omega::loadSimulation(const string& f, bool quiet){
-	bool isMem=boost::algorithm::starts_with(f,":memory:");
-	if(!isMem && !boost::filesystem::exists(f)) throw runtime_error("Simulation file to load doesn't exist: "+f);
-	if(isMem && memSavedSimulations.count(f)==0) throw runtime_error("Cannot load nonexistent memory-saved simulation "+f);
-	
-	if(!quiet) LOG_INFO("Loading file "+f);
+void Omega::loadSimulation(const string& f, bool quiet)
+{
+	bool isMem = boost::algorithm::starts_with(f, ":memory:");
+	if (!isMem && !boost::filesystem::exists(f))
+		throw runtime_error("Simulation file to load doesn't exist: " + f);
+	if (isMem && memSavedSimulations.count(f) == 0)
+		throw runtime_error("Cannot load nonexistent memory-saved simulation " + f);
+
+	if (!quiet)
+		LOG_INFO("Loading file " + f);
 	shared_ptr<Scene>& scene = scenes[currentSceneNb];
 	{
 		stop(); // stop current simulation if running
 		resetScene();
 		RenderMutexLock lock;
-		if(isMem){
+		if (isMem) {
 			std::istringstream iss(memSavedSimulations[f]);
-			yade::ObjectIO::load<decltype(scene),boost::archive::binary_iarchive>(iss,"scene",scene);
+			yade::ObjectIO::load<decltype(scene), boost::archive::binary_iarchive>(iss, "scene", scene);
 		} else {
-			yade::ObjectIO::load(f,"scene",scene);
+			yade::ObjectIO::load(f, "scene", scene);
 		}
 	}
-	if(scene->getClassName()!="Scene") throw logic_error("Wrong file format (scene is not a Scene!?) in "+f);
-	sceneFile=f;
+	if (scene->getClassName() != "Scene")
+		throw logic_error("Wrong file format (scene is not a Scene!?) in " + f);
+	sceneFile = f;
 	timeInit();
-	
+
 	//ForceContainer is not serialized, better prepare it with correct size here
 	//Assumption: maxId is size-1
-	scene->forces.addMaxId(scene->bodies->size()-1);
-	
-	if(!quiet) LOG_DEBUG("Simulation loaded");
+	scene->forces.addMaxId(scene->bodies->size() - 1);
+
+	if (!quiet)
+		LOG_DEBUG("Simulation loaded");
 }
 
-void Omega::saveSimulation(const string& f, bool quiet){
-	if(f.size()==0) throw runtime_error("f of file to save has zero length.");
-	if(!quiet) LOG_INFO("Saving file " << f);
+void Omega::saveSimulation(const string& f, bool quiet)
+{
+	if (f.size() == 0)
+		throw runtime_error("f of file to save has zero length.");
+	if (!quiet)
+		LOG_INFO("Saving file " << f);
 	shared_ptr<Scene>& scene = scenes[currentSceneNb];
-	if(boost::algorithm::starts_with(f,":memory:")){
-		if(memSavedSimulations.count(f)>0 && !quiet) LOG_INFO("Overwriting in-memory saved simulation "<<f);
+	if (boost::algorithm::starts_with(f, ":memory:")) {
+		if (memSavedSimulations.count(f) > 0 && !quiet)
+			LOG_INFO("Overwriting in-memory saved simulation " << f);
 		std::ostringstream oss;
-		yade::ObjectIO::save<decltype(scene),boost::archive::binary_oarchive>(oss,"scene",scene);
-		memSavedSimulations[f]=oss.str();
+		yade::ObjectIO::save<decltype(scene), boost::archive::binary_oarchive>(oss, "scene", scene);
+		memSavedSimulations[f] = oss.str();
+	} else {
+		yade::ObjectIO::save(f, "scene", scene);
 	}
-	else {
-		yade::ObjectIO::save(f,"scene",scene); 
-	}
-	sceneFile=f;
+	sceneFile = f;
 }
 
 } // namespace yade
-
--- core/Omega.hpp
+++ core/Omega.hpp
@@ -13,102 +13,102 @@
 // XXX never do #include<Python.h>, see https://www.boost.org/doc/libs/1_71_0/libs/python/doc/html/building/include_issues.html
 #include <boost/python/detail/wrap_python.hpp>
 #include <fstream>
-#include <time.h>
 #include <iostream>
 #include <mutex>
+#include <time.h>
 
 #include <lib/base/Math.hpp>
 #include <lib/factory/ClassFactory.hpp>
 
+#include "SimulationFlow.hpp"
 #include <lib/base/Singleton.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
-#include "SimulationFlow.hpp"
-#include <set>
 #include <map>
+#include <set>
 
 namespace yade { // Cannot have #include directive inside.
 
 class Scene;
 class ThreadRunner;
 
-struct DynlibDescriptor{
+struct DynlibDescriptor {
 	std::set<string> baseClasses;
-	bool isSerializable;
+	bool             isSerializable;
 };
 
-class Omega: public Singleton<Omega>{
-	shared_ptr<ThreadRunner> simulationLoop;
-	SimulationFlow simulationFlow_;
-	std::map<string,DynlibDescriptor> dynlibs; // FIXME : should store that in ClassFactory ?
-	void buildDynlibDatabase(const vector<string>& dynlibsList); // FIXME - maybe in ClassFactory ?
+class Omega : public Singleton<Omega> {
+	shared_ptr<ThreadRunner>           simulationLoop;
+	SimulationFlow                     simulationFlow_;
+	std::map<string, DynlibDescriptor> dynlibs;                                                // FIXME : should store that in ClassFactory ?
+	void                               buildDynlibDatabase(const vector<string>& dynlibsList); // FIXME - maybe in ClassFactory ?
 
-	vector<shared_ptr<Scene> > scenes;
-	int currentSceneNb;
-	shared_ptr<Scene> sceneAnother; // used for temporarily running different simulation, in Omega().switchscene()
+	vector<shared_ptr<Scene>> scenes;
+	int                       currentSceneNb;
+	shared_ptr<Scene>         sceneAnother; // used for temporarily running different simulation, in Omega().switchscene()
 
-  boost::posix_time::ptime startupLocalTime;
+	boost::posix_time::ptime startupLocalTime;
 
-	std::map<string,string> memSavedSimulations;
+	std::map<string, string> memSavedSimulations;
 
 	// to avoid accessing simulation when it is being loaded (should avoid crashes with the UI)
-	std::mutex tmpFileCounterMutex;
-	long tmpFileCounter;
+	std::mutex  tmpFileCounterMutex;
+	long        tmpFileCounter;
 	std::string tmpFileDir;
 
-	public:
-		// management, not generally useful
-		void init();
-		void reset();
-		void timeInit();
-		void initTemps();
-		void cleanupTemps();
-		const std::map<string,DynlibDescriptor>& getDynlibsDescriptor();
-		void loadPlugins(vector<string> pluginFiles);
-		bool isInheritingFrom(const string& className, const string& baseClassName );
-		bool isInheritingFrom_recursive(const string& className, const string& baseClassName );
-		void createSimulationLoop();
-		bool hasSimulationLoop(){return (bool)(simulationLoop);}
-		string gdbCrashBatch;
-		char** origArgv; int origArgc;
-		// do not change by hand
-		/* Mutex for:
+public:
+	// management, not generally useful
+	void                                      init();
+	void                                      reset();
+	void                                      timeInit();
+	void                                      initTemps();
+	void                                      cleanupTemps();
+	const std::map<string, DynlibDescriptor>& getDynlibsDescriptor();
+	void                                      loadPlugins(vector<string> pluginFiles);
+	bool                                      isInheritingFrom(const string& className, const string& baseClassName);
+	bool                                      isInheritingFrom_recursive(const string& className, const string& baseClassName);
+	void                                      createSimulationLoop();
+	bool                                      hasSimulationLoop() { return (bool)(simulationLoop); }
+	string                                    gdbCrashBatch;
+	char**                                    origArgv;
+	int                                       origArgc;
+	// do not change by hand
+	/* Mutex for:
 		* 1. GLViewer::paintGL (deffered lock: if fails, no GL painting is done)
 		* 2. other threads that wish to manipulate GL
 		* 3. Omega when substantial changes to the scene are being made (bodies being deleted, simulation loaded etc) so that GL doesn't access those and crash */
-		std::mutex renderMutex;
+	std::mutex renderMutex;
 
-		void run();
-		void pause();
-		void step();
-		void stop(); // resets the simulationLoop
-		bool isRunning();
-		std::string sceneFile; // updated at load/save automatically
-		void loadSimulation(const string& name, bool quiet=false);
-		void saveSimulation(const string& name, bool quiet=false);
+	void        run();
+	void        pause();
+	void        step();
+	void        stop(); // resets the simulationLoop
+	bool        isRunning();
+	std::string sceneFile; // updated at load/save automatically
+	void        loadSimulation(const string& name, bool quiet = false);
+	void        saveSimulation(const string& name, bool quiet = false);
 
-		void resetScene();
-		void resetCurrentScene();
-		void resetAllScenes();
-		const shared_ptr<Scene>& getScene();
-		void setScene(const shared_ptr<Scene>& source) {scenes[currentSceneNb]=source;}
-		int addScene();
-		void switchToScene(int i);
-		//! Return unique temporary filename. May be deleted by the user; if not, will be deleted at shutdown.
-		string tmpFilename();
-		Real getRealTime();
-    boost::posix_time::time_duration getRealTime_duration();
+	void                     resetScene();
+	void                     resetCurrentScene();
+	void                     resetAllScenes();
+	const shared_ptr<Scene>& getScene();
+	void                     setScene(const shared_ptr<Scene>& source) { scenes[currentSceneNb] = source; }
+	int                      addScene();
+	void                     switchToScene(int i);
+	//! Return unique temporary filename. May be deleted by the user; if not, will be deleted at shutdown.
+	string                           tmpFilename();
+	Real                             getRealTime();
+	boost::posix_time::time_duration getRealTime_duration();
 
-		// configuration directory used for logging config and possibly other things
-		std::string confDir;
+	// configuration directory used for logging config and possibly other things
+	std::string confDir;
 
 	DECLARE_LOGGER;
 
-	Omega(){ LOG_DEBUG("Constructing Omega."); }
-	~Omega(){}
+	Omega() { LOG_DEBUG("Constructing Omega."); }
+	~Omega() {}
 
 	FRIEND_SINGLETON(Omega);
 	friend class pyOmega;
 };
 
 } // namespace yade
-
