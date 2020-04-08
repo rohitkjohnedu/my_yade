e08730811a1d90be4958274497dcc35dbf028c7f
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-04-08 21:14:44 +0200

    fix typo

--- lib/high-precision/MathEigenTypes.hpp
+++ lib/high-precision/MathEigenTypes.hpp
@@ -23,7 +23,7 @@ template <typename Scalar> using Vector3 = Eigen::Matrix<Scalar, 3, 1>;
 template <typename Scalar> using Vector4 = Eigen::Matrix<Scalar, 4, 1>;
 template <typename Scalar> using Vector6 = Eigen::Matrix<Scalar, 6, 1>;
 
-template <typename Scalar> using Matrix2 = Eigen::Matrix<Scalar, 3, 3>;
+template <typename Scalar> using Matrix2 = Eigen::Matrix<Scalar, 2, 2>;
 template <typename Scalar> using Matrix3 = Eigen::Matrix<Scalar, 3, 3>;
 template <typename Scalar> using Matrix4 = Eigen::Matrix<Scalar, 4, 4>;
 template <typename Scalar> using Matrix6 = Eigen::Matrix<Scalar, 6, 6>;
