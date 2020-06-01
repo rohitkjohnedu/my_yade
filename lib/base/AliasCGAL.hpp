55f92e0d15bb9032c77a688a4685693998785cf5
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-06-01 19:09:18 +0200

    AliasCGAL.hpp starts using ExplicitRealHP.hpp to have shorter code.

--- lib/base/AliasCGAL.hpp
+++ lib/base/AliasCGAL.hpp
@@ -49,166 +49,45 @@ namespace CGAL {
 // The Exact_predicates_inexact_constructions_kernel used `double`. Make corresponding typedef for Real type.
 template <int L> class ERealHP;
 
-template<>
-class ERealHP<1> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<1>>::Base<ERealHP<1>>::Type, ERealHP<1>>,
+// There are two ways to avoid this macro (hint: the best is to use C++20). See file lib/high-precision/ExplicitRealHP.hpp for details.
 #ifdef CGAL_NO_STATIC_FILTERS
-                      false>
+#define YADE_REAL_HP_CGAL_KERNEL_ADAPTOR(levelHP)                                                                                                              \
+	template <>                                                                                                                                            \
+	class ERealHP<levelHP> : public Filtered_kernel_adaptor<                                                                                               \
+	                                 Type_equality_wrapper<Simple_cartesian<::yade::RealHP<levelHP>>::Base<ERealHP<levelHP>>::Type, ERealHP<levelHP>>,     \
+	                                 false> {                                                                                                              \
+	};
 #else
-                      true>
+#define YADE_REAL_HP_CGAL_KERNEL_ADAPTOR(levelHP)                                                                                                              \
+	template <>                                                                                                                                            \
+	class ERealHP<levelHP> : public Filtered_kernel_adaptor<                                                                                               \
+	                                 Type_equality_wrapper<Simple_cartesian<::yade::RealHP<levelHP>>::Base<ERealHP<levelHP>>::Type, ERealHP<levelHP>>,     \
+	                                 true> {                                                                                                               \
+	};
 #endif
-{
-};
 
-template<>
-class ERealHP<2> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<2>>::Base<ERealHP<2>>::Type, ERealHP<2>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
+// There are two ways to avoid this macro (hint: the best is to use C++20). See file lib/high-precision/ExplicitRealHP.hpp for details.
+YADE_HP_RUN_MACRO(YADE_REAL_HP_CGAL_KERNEL_ADAPTOR)
+#undef YADE_REAL_HP_CGAL_KERNEL_ADAPTOR
 
-template<>
-class ERealHP<3> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<3>>::Base<ERealHP<3>>::Type, ERealHP<3>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
+using Exact_Real_predicates_inexact_constructions_kernel                          = ERealHP<1>;
+template <int levelHP> using Exact_RealHP_predicates_inexact_constructions_kernel = ERealHP<levelHP>;
 
-template<>
-class ERealHP<4> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<4>>::Base<ERealHP<4>>::Type, ERealHP<4>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
+#define YADE_REAL_HP_TRIANGULATION(levelHP)                                                                                                                    \
+	template <> struct Triangulation_structural_filtering_traits<ERealHP<levelHP>> {                                                                       \
+		typedef Tag_true Use_structural_filtering_tag;                                                                                                 \
+	};
 
-template<>
-class ERealHP<5> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<5>>::Base<ERealHP<5>>::Type, ERealHP<5>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
-
-template<>
-class ERealHP<6> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<6>>::Base<ERealHP<6>>::Type, ERealHP<6>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
-
-template<>
-class ERealHP<7> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<7>>::Base<ERealHP<7>>::Type, ERealHP<7>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
-
-template<>
-class ERealHP<8> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<8>>::Base<ERealHP<8>>::Type, ERealHP<8>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
-
-template<>
-class ERealHP<9> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<9>>::Base<ERealHP<9>>::Type, ERealHP<9>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
-
-template<>
-class ERealHP<10> : public Filtered_kernel_adaptor<
-                      Type_equality_wrapper<Simple_cartesian<::yade::RealHP<10>>::Base<ERealHP<10>>::Type, ERealHP<10>>,
-#ifdef CGAL_NO_STATIC_FILTERS
-                      false>
-#else
-                      true>
-#endif
-{
-};
-
-
-using Exact_Real_predicates_inexact_constructions_kernel = ERealHP<1>;
-template <int N> using Exact_RealHP_predicates_inexact_constructions_kernel = ERealHP<N>;
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<1>> { // FIXME ←←← tu jest wymuszone 1
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<2>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<3>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<4>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<5>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<6>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<7>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<8>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<9>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
-
-template <> struct Triangulation_structural_filtering_traits<ERealHP<10>> { // FIXME ←←← tu jest wymuszone 2
-	typedef Tag_true Use_structural_filtering_tag;
-};
+// There are two ways to avoid this macro (hint: the best is to use C++20). See file lib/high-precision/ExplicitRealHP.hpp for details.
+YADE_HP_RUN_MACRO(YADE_REAL_HP_TRIANGULATION)
+#undef YADE_REAL_HP_TRIANGULATION
 
 } // namespace CGAL
 
 namespace yade {
 
 // CGAL definitions - does not work with another kernel!! Why???
-//   / Answer: because to use exact kernal yade must link with mpfr / Janek.
+//   / Answer: because to use exact kernel yade must link with mpfr / Janek.
 //   / to do this have a look at CMakeLists 'Real precision' section
 
 // These are taken from files pkg/dem/Polyhedra.hpp and /pkg/dem/Gl1_PotentialBlock.hpp
@@ -218,27 +97,28 @@ namespace yade {
 //	namespace CGP {
 
 // FIXME : template <int N> struct CgalHP { using ……… };
+// FIXME : use better names
 
-using K              = CGAL::Exact_Real_predicates_inexact_constructions_kernel;
-template <int N> using K_HP              = CGAL::Exact_RealHP_predicates_inexact_constructions_kernel<N>; // FIXME - this line and all the others.
-using Polyhedron     = CGAL::Polyhedron_3<K>;
-using Mesh           = CGAL::Surface_mesh<K::Point_3>;
-using Triangulation  = CGAL::Delaunay_triangulation_3<K>;
-using CGALpoint      = K::Point_3;
-template <int N> using CGALpoint_HP      = typename K_HP<N>::Point_3;
-using CGALtriangle   = K::Triangle_3;
-using CGALvector     = K::Vector_3;
-template<int N> using CGALvector_HP     = typename K_HP<N>::Vector_3;
-using Transformation = CGAL::Aff_transformation_3<K>;
-using Segment        = K::Segment_3;
-using Triangle       = CGAL::Triangle_3<K>;
-using Plane          = CGAL::Plane_3<K>;
-template <int N> using Plane_HP         = typename CGAL::Plane_3<K_HP<N>>;
-using Line           = CGAL::Line_3<K>;
-using CGAL_ORIGIN    = CGAL::Origin;
-using CGAL_AABB_tree = CGAL::AABB_tree<CGAL::AABB_traits<K, CGAL::AABB_triangle_primitive<K, std::vector<Triangle>::iterator>>>;
+using K                              = CGAL::Exact_Real_predicates_inexact_constructions_kernel;
+template <int N> using K_HP          = CGAL::Exact_RealHP_predicates_inexact_constructions_kernel<N>; // FIXME - this line and all the others.
+using Polyhedron                     = CGAL::Polyhedron_3<K>;
+using Mesh                           = CGAL::Surface_mesh<K::Point_3>;
+using Triangulation                  = CGAL::Delaunay_triangulation_3<K>;
+using CGALpoint                      = K::Point_3;
+template <int N> using CGALpoint_HP  = typename K_HP<N>::Point_3;
+using CGALtriangle                   = K::Triangle_3;
+using CGALvector                     = K::Vector_3;
+template <int N> using CGALvector_HP = typename K_HP<N>::Vector_3;
+using Transformation                 = CGAL::Aff_transformation_3<K>;
+using Segment                        = K::Segment_3;
+using Triangle                       = CGAL::Triangle_3<K>;
+using Plane                          = CGAL::Plane_3<K>;
+template <int N> using Plane_HP      = typename CGAL::Plane_3<K_HP<N>>;
+using Line                           = CGAL::Line_3<K>;
+using CGAL_ORIGIN                    = CGAL::Origin;
+using CGAL_AABB_tree                 = CGAL::AABB_tree<CGAL::AABB_traits<K, CGAL::AABB_triangle_primitive<K, std::vector<Triangle>::iterator>>>;
 //	}
-// It would be perhaps useful to collect here similar stuff frmo other files.
+// It would be perhaps useful to collect here similar stuff from other files.
 } // namespace yade
 
 #endif
--- lib/high-precision/ExplicitRealHP.hpp
+++ lib/high-precision/ExplicitRealHP.hpp
@@ -5,41 +5,70 @@
 *  GNU General Public License v2 or later. See file LICENSE for details. *
 *************************************************************************/
 
+#ifndef YADE_EXPLICIT_REAL_HP_HPP
+#define YADE_EXPLICIT_REAL_HP_HPP
+
+#include <boost/preprocessor.hpp>
+
+// RealHP<…> can't work with older gcc, cmake will detect this.
+#ifndef YADE_DISABLE_REAL_MULTI_HP
+
 /*
 
-This file provides explicit instantinations for RealHP<N> types. Before we switch to C++20 they are unfortunately necessary.
+This file provides explicit instantinations for RealHP<N>, Vector3rHP<N> etc. types. Before we switch to C++20 this file is unfortunately necessary.
 
-There are two ways to avoid these macors:
+One cannot "just use Vector3rHP<123>", and be happy, because Eigen and CGAL are not flexible enough in template specialization mechanisms.
+The RealHP<123> just works, but for Vector3rHP<123> this file is necessary.
+
+The current solution to this problem is to set the list of supported numbers below (separately for C++ and Python):
+	C++	: YADE_EXPLICIT_REAL_HP  ↔ The numbers listed here will work in C++ for RealHP<N> in CGAL and Eigen. Rather cheap in compilation time.
+	Python	: YADE_PYTHON_REAL_HP    ↔ These are exported to python. Expensive: each one makes compilation longer by 1 minute.
+
+Caution: trying to use an unregistered for python Vector3rHP<N> type in YADE_CLASS_BASE_DOC_ATTRS_* to export value to Python will cause problems.
+         however it is safe (and intended) to use them in the C++ calculations in critical sections of code, without exporting them to python.
+*/
+
+#define YADE_EXPLICIT_REAL_HP (1)(2)(3)(4)(5)(6)(7)(8)(9)(10)
+//#define YADE_PYTHON_REAL_HP (1)(2)(3)(4)
+
+// Python: if you are doing some debugging, and need to access from python all the precisions that are used in C++, then instead of above, use this:
+#define YADE_PYTHON_REAL_HP YADE_EXPLICIT_REAL_HP
+
+#else // on older compilers or with older libraries, this can't work, cmake will detect such problems. In this case only RealHP<1> is instantinated.
+#define YADE_EXPLICIT_REAL_HP (1)
+#endif
+
+/*
+There are two ways to avoid these macros:
 
 1) modify Eigen library to change the main template declaration from
 
 	template<typename>
 	struct NumTraits;
 
-into
+   into
 
 	template<typename, typename=void>
 	struct NumTraits;
 
-then we could use the second typename to enable/disable our template specialization, by writing:
+   then we could use the second typename to enable/disable our template specialization, by writing:
 
 	template <typename Rr>
 	struct NumTraits <Rr, typename std::enable_if<::yade::math::IsHP<Rr>>::type> : public NumTraitsHP<::yade::math::levelOfRealHP<Rr>> {};
 
-I checked locally that modification of Eigen library fixes the template specializations and removes macros.
+   Do the same with CGAL.
+   I checked locally that modification of Eigen library fixes the template specializations and removes all these macros.
 
 2) wait until we move yade to C++20 and then use the https://en.cppreference.com/w/cpp/language/constraints to enable/disable our
    template specializations of NumTraitsRealHP (which can then be renamed to Eigen::NumTraits) using the `requires` and `using`
    C++20 keywords, explained in section "Type requirements". The classes can then be renamed back to original names:
+
 	NumTraitsRealHP				→ Eigen::NumTraits
 	RealHP_Is_valid				→ CGAL::Is_valid
 	RealHP_Algebraic_structure_traits	→ CGAL::Algebraic_structure_traits 
 	RealHP_embeddable_traits		→ CGAL::Real_embeddable_traits
 
 */
-
-#ifndef YADE_EXPLICIT_REAL_HP_HPP
-#define YADE_EXPLICIT_REAL_HP_HPP
 
 #if ((not defined(YADE_REAL_MATH_NAMESPACE)) or (not defined(YADE_EIGEN_SUPPORT_REAL_HP)) or (defined(YADE_CGAL) and (not defined(YADE_CGAL_SUPPORT_REAL_HP))))
 #error "This file cannot be included alone, include Real.hpp instead"
@@ -50,19 +79,35 @@ struct ExplicitRealHP {
 }; // for ctags
 }
 
+#define YADE_HP_PARSE_ONE(r, name, levelHP) name(levelHP)
+// This macro ↓ is used in AliasCGAL.hpp, that code could be put here, but this would make compilation unnecessarily longer.
+#define YADE_HP_RUN_MACRO(name) BOOST_PP_SEQ_FOR_EACH(YADE_HP_PARSE_ONE, name, YADE_EXPLICIT_REAL_HP) // it just creates: name(1) name(2) name(3) ....
+
+#define YADE_IGNORE_ARG(arg)
+
+#define YADE_HP_PARSE_SEQUENCE(r, data, levelHP)                                                                                                               \
+	BOOST_PP_IF(                                                                                                                                           \
+	        BOOST_PP_GREATER_EQUAL(levelHP, /* skip 'below' this number */ BOOST_PP_SEQ_ELEM(1, data)),                                                    \
+	        /* execute macro with this 'name'   */ BOOST_PP_SEQ_ELEM(0, data),                                                                             \
+	        /* if it is skipped, then ignore it */ YADE_IGNORE_ARG)                                                                                        \
+	(levelHP)
+
+#define YADE_HP_RUN_MACRO_FROM_LEVEL(name, below) BOOST_PP_SEQ_FOR_EACH(YADE_HP_PARSE_SEQUENCE, (name)(below), YADE_EXPLICIT_REAL_HP)
+
+
 // The float and double are supported by default in Eigen and CGAL, so they have to be skipped.
-#define YADE_REAL_HP_ALL_ADD_EXPLICIT_SUPPORT_FOR(name) name(1) name(2) name(3) name(4) name(5) name(6) name(7) name(8) name(9) name(10)
 #if (YADE_REAL_BIT >= 80)
-#define YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR(name) YADE_REAL_HP_ALL_ADD_EXPLICIT_SUPPORT_FOR(name)
+#define YADE_HP_RUN_EXPLICIT_MACRO(name) YADE_HP_RUN_MACRO(name) //               it just creates: name(1) name(2) name(3) .... using YADE_EXPLICIT_REAL_HP
 #elif (YADE_REAL_BIT >= 64)
-#define YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR(name) name(2) name(3) name(4) name(5) name(6) name(7) name(8) name(9) name(10)
+#define YADE_HP_RUN_EXPLICIT_MACRO(name) YADE_HP_RUN_MACRO_FROM_LEVEL(name, 2) // it just creates:         name(2) name(3) ....
 #elif (YADE_REAL_BIT >= 32)
-#define YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR(name) name(3) name(4) name(5) name(6) name(7) name(8) name(9) name(10)
+#define YADE_HP_RUN_EXPLICIT_MACRO(name) YADE_HP_RUN_MACRO_FROM_LEVEL(name, 3) // it just creates:                 name(3) ....
 #endif
 
 namespace Eigen {
 
-YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR(YADE_EIGEN_SUPPORT_REAL_HP)
+// The YADE_EIGEN_SUPPORT_REAL_HP is provided from file lib/high-precision/EigenNumTraits.hpp
+YADE_HP_RUN_EXPLICIT_MACRO(YADE_EIGEN_SUPPORT_REAL_HP)
 #undef YADE_EIGEN_SUPPORT_REAL_HP
 
 } // namespace Eigen
@@ -70,13 +115,14 @@ YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR(YADE_EIGEN_SUPPORT_REAL_HP)
 #ifdef YADE_CGAL
 namespace CGAL {
 
-YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR(YADE_CGAL_SUPPORT_REAL_HP)
+// The YADE_CGAL_SUPPORT_REAL_HP is provided from file lib/high-precision/CgalNumTraits.hpp
+YADE_HP_RUN_EXPLICIT_MACRO(YADE_CGAL_SUPPORT_REAL_HP)
 #undef YADE_CGAL_SUPPORT_REAL_HP
 
 } // namespace CGAL
 #endif // YADE_CGAL
 
-#undef YADE_REAL_HP_ADD_EXPLICIT_SUPPORT_FOR
+#undef YADE_HP_RUN_EXPLICIT_MACRO
 
 #endif
 
