0b57bc58773b40e4781b3a92f2d8a0ed21751fa3
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-06-05 00:29:32 +0200

    py/tests/testMinieigenHP.py : first version which passes all tests in Eigen HP, Vector3rHP<1...10> and all others.

--- py/tests/testMinieigenHP.py
+++ py/tests/testMinieigenHP.py
@@ -16,9 +16,41 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		#FIXME: self.digs1=mne        .highPrecisionDecimalPlaces+mne      .extraDigits10NecessaryForStringRepresentation
 		#FIXME: self.digs1=yade.config.highPrecisionDecimalPlaces+yade.math.extraDigits10NecessaryForStringRepresentation
 		mpmath.mp.dps=self.digs1
-		# tolerance = 1.001×10⁻ᵈ⁺¹, where ᵈ==yade.config.highPrecisionDecimalPlaces
-		# so basically we store one more decimal digit, and expect one less decimal digit. That amounts to ignoring one (actually two) least significant digits.
-		self.tolerance=(mpmath.mpf(10)**(-yade.config.highPrecisionDecimalPlaces+1))*mpmath.mpf("1.001")
+		self.extraStrDigits = mne.extraStringDigits
+		self.maxTestLevelHP = mne.maxRealLevelHP
+		self.baseDigits     = mne.digits10RealHP(1)
+		self.builtinHP      = { 6 : [6,15,18,24,33] , 15 : [15,33] } # higher precisions are multiplies of baseDigits, see NthLevelRealHP in lib/high-precision/RealHP.hpp
+
+	def getDigitsHP(self,N):
+		ret = None
+		if (self.baseDigits in self.builtinHP) and (N <= len(self.builtinHP[self.baseDigits])):
+			ret = self.builtinHP[self.baseDigits][N-1]
+		else:
+			ret = self.baseDigits*N
+		self.assertEqual(ret,mne.digits10RealHP(N))
+		return ret
+
+	def adjustDigs0(self,N,HPn):
+		self.digs0     = self.getDigitsHP(N)
+		self.digs1     = self.digs0 + 1
+		mpmath.mp.dps  = self.digs0 + self.extraStrDigits
+		# tolerance = 1.001×10⁻ᵈ⁺¹, where ᵈ==self.digs0
+		# so basically we store one more decimal digit, and expect one less decimal digit. That amounts to ignoring one (two, if the extra one is counted) least significant digits.
+		self.tolerance = (mpmath.mpf(10)**(-self.digs0+1))*mpmath.mpf("1.001")
+
+	def runCheck(self,N0,func):
+		N      = N0+1                        # RealHP<1> == Real, the HP<…> counting starts from 1, while the python range(…) loop starts from 0.
+		nameHP = "HP" + str(N)               # the same as the line 'string    name = "HP" + boost::lexical_cast<std::string>(N);' in function registerInScope in ToFromPythonConverter.hpp
+		HPn    = getattr(mne,nameHP);        # the same as the line 'py::scope HPn  = boost::python::class_<ScopeHP<N>>(name.c_str());'   in ToFromPythonConverter.hpp
+		if(N==1):
+			self.adjustDigs0(N,mne)
+			if(self.digs0 == 33): return ######### FIXME - remove that. It's here because on devuan beowulf, with older compiler float128 is segfaulting
+			func(N,mne,"mne.")           # test global scope functions with RealHP<1>
+		print('RealHP<'+str(N)+'>', end=' ')
+		self.adjustDigs0(N,HPn)
+		if(self.digs0 == 33): return ######### FIXME - remove that. It's here because on devuan beowulf, with older compiler float128 is segfaulting
+		func(N,HPn,"mne."+nameHP+".")        # test scopes HP1, HP2, etc
+
 	def checkRelativeError(self,a,b):
 		if b!=0:
 			self.assertLessEqual(abs( (mpmath.mpf(a)-mpmath.mpf(b))/mpmath.mpf(b) ),self.tolerance)
@@ -28,6 +60,10 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.assertLessEqual(abs( (mpmath.mpc(a)-mpmath.mpc(b))/mpmath.mpc(b) ),self.tolerance)
 
 	def testMpmath(self):
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestMpmath)
+
+	def HPtestMpmath(self,N,HPn,prefix):
 		self.assertEqual(2    ,float(mpmath.mpf(2)))
 		self.assertEqual(2    ,float(mpmath.mpf("2")))
 		self.assertEqual(2**3 ,mpmath.mpf("2")**3)
@@ -47,8 +83,12 @@ class ExtendedMinieigenTests(unittest.TestCase):
 
 
 	def testVector2i(self):
-		a2i = mne.Vector2i(2,1)
-		b2i = mne.Vector2i(3,5)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector2i)
+
+	def HPtestVector2i(self,N,HPn,prefix):
+		a2i = HPn.Vector2i(2,1)
+		b2i = HPn.Vector2i(3,5)
 		c2i = a2i + b2i
 		#self.assertEqual(c2i.eigenFlags(),352)
 		#self.assertEqual(c2i.eigenStorageOrder(),0)
@@ -63,12 +103,15 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c2i[0] , mpmath.mpf("10") )
 		self.checkRelativeError( c2i[1] , mpmath.mpf("12") )
 
-		from yade.minieigenHP import Vector2i
-		self.assertEqual( c2i , eval(c2i.__str__()) )
+		self.assertEqual( c2i , eval(prefix+c2i.__str__()) )
 
 	def testVector2(self):
-		a2 = mne.Vector2(2,1)
-		b2 = mne.Vector2(3,5)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector2)
+
+	def HPtestVector2(self,N,HPn,prefix):
+		a2 = HPn.Vector2(2,1)
+		b2 = HPn.Vector2(3,5)
 		c2 = a2 + b2
 		#self.assertEqual(c2.eigenFlags(),352)
 		#self.assertEqual(c2.eigenStorageOrder(),0)
@@ -83,12 +126,15 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c2[0] , mpmath.mpf("10") )
 		self.checkRelativeError( c2[1] , mpmath.mpf("12") )
 
-		from yade.minieigenHP import Vector2
-		self.assertEqual( c2 , eval(c2.__str__()) )
+		self.assertEqual( c2 , eval(prefix+c2.__str__()) )
 
 	def testVector2c(self):
-		a2c = mne.Vector2c(2-10j,1)
-		b2c = mne.Vector2c(3,5)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector2c)
+
+	def HPtestVector2c(self,N,HPn,prefix):
+		a2c = HPn.Vector2c(2-10j,1)
+		b2c = HPn.Vector2c(3,5)
 		c2c = a2c + b2c
 		#self.assertEqual(c2c.eigenFlags(),352)
 		#self.assertEqual(c2c.eigenStorageOrder(),0)
@@ -103,12 +149,15 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeComplexError( c2c[0] , mpmath.mpc("10","-20") )
 		self.checkRelativeComplexError( c2c[1] , mpmath.mpf("12") )
 
-		from yade.minieigenHP import Vector2c
-		self.assertEqual( c2c , eval(c2c.__str__()) )
+		self.assertEqual( c2c , eval(prefix+c2c.__str__()) )
 
 	def testVector3i(self):
-		a3i = mne.Vector3i(2,1,4)
-		b3i = mne.Vector3i(3,5,5)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector3i)
+
+	def HPtestVector3i(self,N,HPn,prefix):
+		a3i = HPn.Vector3i(2,1,4)
+		b3i = HPn.Vector3i(3,5,5)
 		c3i = a3i + b3i
 		#self.assertEqual(c3i.eigenFlags(),352)
 		#self.assertEqual(c3i.eigenStorageOrder(),0)
@@ -125,12 +174,15 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c3i[1] , mpmath.mpf("18") )
 		self.checkRelativeError( c3i[2] , mpmath.mpf("27") )
 
-		from yade.minieigenHP import Vector3i
-		self.assertEqual( c3i , eval(c3i.__str__()) )
+		self.assertEqual( c3i , eval(prefix+c3i.__str__()) )
 
 	def testVector3(self):
-		a3r = mne.Vector3(2.1,1.1,4.3)
-		b3r = mne.Vector3(3.1,5.1,5.2)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector3)
+
+	def HPtestVector3(self,N,HPn,prefix):
+		a3r = HPn.Vector3(2.1,1.1,4.3)
+		b3r = HPn.Vector3(3.1,5.1,5.2)
 		c3r = a3r + b3r
 		#self.assertEqual(c3r.eigenFlags(),352)
 		#self.assertEqual(c3r.eigenStorageOrder(),0)
@@ -147,14 +199,17 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c3r[1] , mpmath.mpf("18.6") )
 		self.checkRelativeError( c3r[2] , mpmath.mpf("28.5") )
 
-		from yade.minieigenHP import Vector3
-		self.checkRelativeError( c3r[0] , eval(c3r.__str__())[0] )
-		self.checkRelativeError( c3r[1] , eval(c3r.__str__())[1] )
-		self.checkRelativeError( c3r[2] , eval(c3r.__str__())[2] )
+		self.checkRelativeError( c3r[0] , eval(prefix+c3r.__str__())[0] )
+		self.checkRelativeError( c3r[1] , eval(prefix+c3r.__str__())[1] )
+		self.checkRelativeError( c3r[2] , eval(prefix+c3r.__str__())[2] )
 
 	def testVector3c(self):
-		a3c = mne.Vector3c(2.1+1j,1.1+2.5j,4.3-1j)
-		b3c = mne.Vector3c(3.1,5.1,5.2)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector3c)
+
+	def HPtestVector3c(self,N,HPn,prefix):
+		a3c = HPn.Vector3c(2.1+1j,1.1+2.5j,4.3-1j)
+		b3c = HPn.Vector3c(3.1,5.1,5.2)
 		c3c = a3c + b3c
 		#self.assertEqual(c3c.eigenFlags(),352)
 		#self.assertEqual(c3c.eigenStorageOrder(),0)
@@ -171,15 +226,18 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeComplexError( c3c[1] , mpmath.mpc("18.6","7.5") )
 		self.checkRelativeComplexError( c3c[2] , mpmath.mpc("28.5","-3") )
 
-		from yade.minieigenHP import Vector3c
-		self.checkRelativeComplexError( c3c[0] , eval(c3c.__str__())[0] )
-		self.checkRelativeComplexError( c3c[1] , eval(c3c.__str__())[1] )
-		self.checkRelativeComplexError( c3c[2] , eval(c3c.__str__())[2] )
+		self.checkRelativeComplexError( c3c[0] , eval(prefix+c3c.__str__())[0] )
+		self.checkRelativeComplexError( c3c[1] , eval(prefix+c3c.__str__())[1] )
+		self.checkRelativeComplexError( c3c[2] , eval(prefix+c3c.__str__())[2] )
 
 	def testVector3na(self):
-		if((mne.vectorize == False) or (not hasattr(mne, 'Vector3na'))): return
-		a3a = mne.Vector3na(2.1,1.1,4.3)
-		b3a = mne.Vector3na(3.1,5.1,5.2)
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector3na)
+
+	def HPtestVector3na(self,N,HPn,prefix):
+		if((HPn.vectorize == False) or (not hasattr(HPn, 'Vector3na'))): return
+		a3a = HPn.Vector3na(2.1,1.1,4.3)
+		b3a = HPn.Vector3na(3.1,5.1,5.2)
 		c3a = a3a + b3a
 		#self.assertEqual(c3a.eigenFlags(),352)
 		#self.assertEqual(c3a.eigenStorageOrder(),0)
@@ -196,16 +254,19 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c3a[1] , mpmath.mpf("18.6") )
 		self.checkRelativeError( c3a[2] , mpmath.mpf("28.5") )
 
-		from yade.minieigenHP import Vector3na as Vector3
-		self.checkRelativeError( c3a[0] , eval(c3a.__str__())[0] )
-		self.checkRelativeError( c3a[1] , eval(c3a.__str__())[1] )
-		self.checkRelativeError( c3a[2] , eval(c3a.__str__())[2] )
+		self.checkRelativeError( c3a[0] , eval(prefix+c3a.__str__())[0] )
+		self.checkRelativeError( c3a[1] , eval(prefix+c3a.__str__())[1] )
+		self.checkRelativeError( c3a[2] , eval(prefix+c3a.__str__())[2] )
 
 	def testVector4(self):
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestVector4)
+
+	def HPtestVector4(self,N,HPn,prefix):
 		# The Vector4 bug was fixed only recently, don't test if there's nothing to test
-		if(not hasattr(mne, 'Vector4')): return
-		a4r = mne.Vector4(2.1,1.1,4.3,5.5)
-		b4r = mne.Vector4(3.1,5.1,5.2,-5.0)
+		if(not hasattr(HPn, 'Vector4')): return
+		a4r = HPn.Vector4(2.1,1.1,4.3,5.5)
+		b4r = HPn.Vector4(3.1,5.1,5.2,-5.0)
 		c4r = a4r + b4r
 		#self.assertEqual(c4r.eigenFlags(),352)
 		#self.assertEqual(c4r.eigenStorageOrder(),0)
@@ -224,14 +285,17 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		self.checkRelativeError( c4r[2] , mpmath.mpf("28.5") )
 		self.checkRelativeError( c4r[3] , mpmath.mpf("1.5") )
 
-		from yade.minieigenHP import Vector4
-		self.checkRelativeError( c4r[0] , eval(c4r.__str__())[0] )
-		self.checkRelativeError( c4r[1] , eval(c4r.__str__())[1] )
-		self.checkRelativeError( c4r[2] , eval(c4r.__str__())[2] )
-		self.checkRelativeError( c4r[3] , eval(c4r.__str__())[3] )
+		self.checkRelativeError( c4r[0] , eval(prefix+c4r.__str__())[0] )
+		self.checkRelativeError( c4r[1] , eval(prefix+c4r.__str__())[1] )
+		self.checkRelativeError( c4r[2] , eval(prefix+c4r.__str__())[2] )
+		self.checkRelativeError( c4r[3] , eval(prefix+c4r.__str__())[3] )
 
 	def testMatrix3Test(self):
-		a3m=mne.Matrix3(1,2,3,
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestMatrix3Test)
+
+	def HPtestMatrix3Test(self,N,HPn,prefix):
+		a3m=HPn.Matrix3(1,2,3,
 		                4,5,6,
 		                7,8,9)
 		#self.assertEqual(a3m.eigenFlags(),352)
@@ -256,14 +320,17 @@ class ExtendedMinieigenTests(unittest.TestCase):
 
 		self.checkRelativeError( a3m.maxAbsCoeff() , mpmath.mpf("9") )
 
-		from yade.minieigenHP import Matrix3
 		for i in range(3):
 			for j in range(3):
-				self.checkRelativeError( b3m[i][j] , eval(b3m.__str__())[i][j] )
+				self.checkRelativeError( b3m[i][j] , eval(prefix+b3m.__str__())[i][j] )
 		#print(b3m.__str__())
 
 	def testMatrix3cTest(self):
-		a3m=mne.Matrix3c(1+1j,2,3,
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestMatrix3cTest)
+
+	def HPtestMatrix3cTest(self,N,HPn,prefix):
+		a3m=HPn.Matrix3c(1+1j,2,3,
 		                4,5,6,
 		                7,8,9-9j)
 		#self.assertEqual(a3m.eigenFlags(),352)
@@ -288,15 +355,18 @@ class ExtendedMinieigenTests(unittest.TestCase):
 
 		self.checkRelativeComplexError( a3m.maxAbsCoeff() , abs(mpmath.mpc("9","-9")) )
 
-		from yade.minieigenHP import Matrix3c
 		for i in range(3):
 			for j in range(3):
-				self.checkRelativeComplexError( b3m[i][j] , eval(b3m.__str__())[i][j] )
+				self.checkRelativeComplexError( b3m[i][j] , eval(prefix+b3m.__str__())[i][j] )
 		#print(b3m.__str__())
 
 
 	def testQuaternion(self):
-		q1 = mne.Quaternion.Identity
+		for N in range(self.maxTestLevelHP):
+			self.runCheck(N , self.HPtestQuaternion)
+
+	def HPtestQuaternion(self,N,HPn,prefix):
+		q1 = HPn.Quaternion.Identity
 		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( q1[3] , mpmath.mpf("1") )
 		#self.assertEqual(q1.eigenFlags(),32)
@@ -304,10 +374,10 @@ class ExtendedMinieigenTests(unittest.TestCase):
 
 		q2 = q1.inverse()
 		self.checkRelativeError( q2[3] , mpmath.mpf("1") )
-		if(yade.config.highPrecisionMpmath):
-			q3=mne.Quaternion(axis=mne.Vector3(1,0,0),angle=mpmath.pi/2.0)
+		if(True):
+			q3=HPn.Quaternion(axis=HPn.Vector3(1,0,0),angle=mpmath.pi/2.0)
 		else:
-			q3=mne.Quaternion(axis=mne.Vector3(1,0,0),angle=1.570796326794896619231321691639)
+			q3=HPn.Quaternion(axis=HPn.Vector3(1,0,0),angle=1.570796326794896619231321691639)
 		m3q=q3.toRotationMatrix()
 		self.checkRelativeError( m3q[0][0] , mpmath.mpf("1") )
 		#print(m3q[1][2].__repr__())
@@ -315,16 +385,15 @@ class ExtendedMinieigenTests(unittest.TestCase):
 		#print(q3)
 		self.assertEqual(mpmath.mp.dps , self.digs1 )
 
-		q4 = mne.Quaternion.Identity
-		q4.setFromTwoVectors(mne.Vector3(1,2,3),mne.Vector3(2,3,4))
+		q4 = HPn.Quaternion.Identity
+		q4.setFromTwoVectors(HPn.Vector3(1,2,3),HPn.Vector3(2,3,4))
 		#print(q4.norm().__repr__())
 		self.assertEqual(mpmath.mp.dps , self.digs1 )
 		self.checkRelativeError( q4.norm() , mpmath.mpf("1") )
 
-		from yade.minieigenHP import Quaternion
-		self.checkRelativeError( q3[0] , eval(q3.__str__())[0] )
-		self.checkRelativeError( q3[1] , eval(q3.__str__())[1] )
-		self.checkRelativeError( q3[2] , eval(q3.__str__())[2] )
-		self.checkRelativeError( q3[3] , eval(q3.__str__())[3] )
+		self.checkRelativeError( q3[0] , eval(prefix+q3.__str__())[0] )
+		self.checkRelativeError( q3[1] , eval(prefix+q3.__str__())[1] )
+		self.checkRelativeError( q3[2] , eval(prefix+q3.__str__())[2] )
+		self.checkRelativeError( q3[3] , eval(prefix+q3.__str__())[3] )
 		#print(q3.__str__())
 
