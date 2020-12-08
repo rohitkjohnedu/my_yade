b55cb49b24c833df111fb3983e045d9a9dd71c2c
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2020-12-08 19:32:17 +0100

    improve error checking

--- scripts/checks-and-tests/checks/checkGravity.py
+++ scripts/checks-and-tests/checks/checkGravity.py
@@ -33,6 +33,8 @@ id_0=o.bodies.append(sphere((0.0,calcPos(0,O.dt/2),0),0.2,material=sphereMat))  
 id_down=o.bodies.append(sphere((1.0,calcPos(v_down,O.dt/2),0),0.2,material=sphereMat)) #The body has an initial vertical Velocity -5
 id_up=o.bodies.append(sphere((2.0,calcPos(v_up,O.dt/2),0),0.2,material=sphereMat))   #The body has an initial vertical Velocity +5
 
+# Instead of setting initial positions (above) to be declared at O.dt/2 one can also set initial velocities to be at -O.dt/2
+# see https://gitlab.com/yade-dev/trunk/-/merge_requests/555#note_462560944
 O.bodies[id_down].state.vel[1] = v_down
 O.bodies[id_up].state.vel[1] = v_up
 
@@ -53,19 +55,24 @@ o.engines=[
 ]
 
 def checkPos():
-  if ((O.bodies[id_0].state.pos[1] - getCurrentPos(0))/O.bodies[id_0].state.pos[1] > tolerance): 
-    warningMessagePos (0, O.bodies[id_0].state.pos[1], getCurrentPos(0))
-  if ((O.bodies[id_down].state.pos[1] - getCurrentPos(v_down))/O.bodies[id_down].state.pos[1] > tolerance): 
-    warningMessagePos (v_down, O.bodies[id_down].state.pos[1], getCurrentPos(0))
-  if ((O.bodies[id_up].state.pos[1] - getCurrentPos(v_up))/O.bodies[id_up].state.pos[1] > tolerance): 
-    warningMessagePos (v_up, O.bodies[id_up].state.pos[1], getCurrentPos(0))
-  if ((O.bodies[id_0].state.vel[1] - getCurrentVel(0))/O.bodies[id_0].state.vel[1] > tolerance): 
-    warningMessageVel (0, O.bodies[id_0].state.vel[1], getCurrentPos(0))
-  if ((O.bodies[id_down].state.vel[1] - getCurrentVel(v_down))/O.bodies[id_down].state.vel[1] > tolerance): 
-    warningMessageVel (v_down, O.bodies[id_down].state.vel[1], getCurrentPos(0))
-  if ((O.bodies[id_up].state.vel[1] - getCurrentVel(v_up))/O.bodies[id_up].state.vel[1] > tolerance): 
-    warningMessageVel (v_up, O.bodies[id_up].state.vel[1], getCurrentPos(0))
-  
+  if (abs((O.bodies[id_0   ].state.pos[1] - getCurrentPos(0     ))/O.bodies[id_0   ].state.pos[1]) > tolerance):
+    warningMessagePos (0     , O.bodies[id_0   ].state.pos[1], getCurrentPos(0))
+
+  if (abs((O.bodies[id_down].state.pos[1] - getCurrentPos(v_down))/O.bodies[id_down].state.pos[1]) > tolerance):
+    warningMessagePos (v_down, O.bodies[id_down].state.pos[1], getCurrentPos(v_down))
+
+  if (abs((O.bodies[id_up  ].state.pos[1] - getCurrentPos(v_up  ))/O.bodies[id_up  ].state.pos[1]) > tolerance):
+    warningMessagePos (v_up  , O.bodies[id_up  ].state.pos[1], getCurrentPos(v_up))
+
+  if (abs((O.bodies[id_0   ].state.vel[1] - getCurrentVel(0     ))/O.bodies[id_0   ].state.vel[1]) > tolerance):
+    warningMessageVel (0     , O.bodies[id_0   ].state.vel[1], getCurrentVel(0))
+
+  if (abs((O.bodies[id_down].state.vel[1] - getCurrentVel(v_down))/O.bodies[id_down].state.vel[1]) > tolerance):
+    warningMessageVel (v_down, O.bodies[id_down].state.vel[1], getCurrentVel(v_down))
+
+  if (abs((O.bodies[id_up  ].state.vel[1] - getCurrentVel(v_up  ))/O.bodies[id_up  ].state.vel[1]) > tolerance):
+    warningMessageVel (v_up  , O.bodies[id_up  ].state.vel[1], getCurrentVel(v_up))
+
 def getCurrentPos(inVel=0):
   t = O.time+O.dt*1.5
   return inVel*t + g*t*t/2
@@ -75,10 +82,10 @@ def getCurrentVel(inVel=0):
   return inVel + g*t
 
 def warningMessagePos(inVel, y_pos, y_pos_need):
-  raise YadeCheckError("The body with the initial velocity %.3f, has an y-position %.3f, but it should be %.3f" % (inVel, y_pos, y_pos_need))
+  raise YadeCheckError("The body with the initial velocity %.3f, has an y-position %.15f, but it should be %.15f. Iter=%i, time=%.15f, dt=%.15f" % (inVel, y_pos, y_pos_need, O.iter, O.time, O.dt))
 
-def warningMessageVel(inVel, y_vel, y_pos_vel):
-  raise YadeCheckError("The body with the initial velocity %.3f, has an y-velocity %.3f, but it should be %.3f" % (inVel, y_vel, y_pos_vel))
+def warningMessageVel(inVel, y_vel, y_vel_need):
+  raise YadeCheckError("The body with the initial velocity %.3f, has an y-velocity %.15f, but it should be %.15f. Iter=%i, time=%.15f, dt=%.15f" % (inVel, y_vel, y_vel_need, O.iter, O.time, O.dt))
 
 O.saveTmp('init');
 O.run(1000000)
