0b073022dada7b50485c1877009d8509d039ea20
bchareyre <bruno.chareyre@grenoble-inp.fr>
2020-12-14 21:41:16 +0100

    simpler and complete parallel version of DEM8 benchmark 1 (needs to re-fix units)

--- scripts/checks-and-tests/DEM8Benchmark/Case1_SiloFlow.py
+++ scripts/checks-and-tests/DEM8Benchmark/Case1_SiloFlow.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python
 # 2020 © Vasileios Angelidakis <v.angelidakis2@ncl.ac.uk>
 # 2020 © Bruno Chareyre <bruno.chareyre@grenoble-inp.fr> 
-#from builtins import range
-#import time
-
-numMPIThreads=1
-
-if numMPIThreads > 1:
-    from yade import mpy as mp
-    mp.initialize(numMPIThreads)
 
 # Benchmark of basic performance of open-source DEM simulation systems
 # Case 1: Silo flow
@@ -26,16 +18,18 @@ if numMPIThreads > 1:
 # -------------------------------------------------------------------- #
 # Input Data -> Define Material and Orifice size. Uncomment the prefered choice
 
-granularMaterial='M1'
-#granularMaterial='M2'
-
+#FIXME: most of what follows don't have to be guarded by rank==0
 fileName='SiloLargeOrifice'
 #fileName='SiloSmallOrifice'
+if fileName=='SiloLargeOrifice':
+    z=70	# This is the height of the lowest point of the funnel (at the orifice), measuring from the lowest cylindrical cross section of the silo
+elif fileName=='SiloSmallOrifice':
+    z=80
 
-# -------------------------------------------------------------------- #
-# End of input data. The rest of the script should be automated for all different scenarios. Bruno, Robert, let me know if this is not the case/I have missed something.
+particleRadius = 2 # 2mm in the benchmark, bigger for less particles
 
-
+granularMaterial='M1'
+#granularMaterial='M2'
 
 # -------------------------------------------------------------------- #
 # Materials
@@ -51,179 +45,172 @@ e_M2_M2=0.4;	f_M2_M2=0.4
 e_M2_St=0.4;	f_M2_St=0.2
 
 if granularMaterial=='M1':
-	M1=O.materials.append(FrictMat(young=1.0e-3,poisson=0.2,density=2500e-12,label='M1'))
-	e_gg=e_M1_M1	# Coefficient of restitution (e) between granular material (g) and granular material (g)
-	f_gg=f_M1_M1	# Coefficient of friction (f)...
+    M1=O.materials.append(FrictMat(young=1.0e-3,poisson=0.2,density=2500e-12,label='M1'))
+    e_gg=e_M1_M1	# Coefficient of restitution (e) between granular material (g) and granular material (g)
+    f_gg=f_M1_M1	# Coefficient of friction (f)...
 
-	e_gs=e_M1_St	# Coefficient of restitution (e) between granular material (g) and steel (s)
-	f_gs=f_M1_St	# Coefficient of friction (f)...
+    e_gs=e_M1_St	# Coefficient of restitution (e) between granular material (g) and steel (s)
+    f_gs=f_M1_St	# Coefficient of friction (f)...
 elif granularMaterial=='M2':
-	M2=O.materials.append(FrictMat(young=0.5e-3,poisson=0.2,density=2000e-12,label='M2'))
-	e_gg=e_M2_M2
-	f_gg=f_M2_M2
+    M2=O.materials.append(FrictMat(young=0.5e-3,poisson=0.2,density=2000e-12,label='M2'))
+    e_gg=e_M2_M2
+    f_gg=f_M2_M2
 
-	e_gs=e_M2_St
-	f_gs=f_M2_St
+    e_gs=e_M2_St
+    f_gs=f_M2_St
 
 F_gg=atan(f_gg) # Friction Angle between granular material (g) and granular material (g)
 F_gs=atan(f_gs) # Friction Angle between granular material (g) and steel (s)
 
 
-if fileName=='SiloLargeOrifice':
-	z=70	# This is the height of the lowest point of the funnel (at the orifice), measuring from the lowest cylindrical cross section of the silo
-elif fileName=='SiloSmallOrifice':
-	z=80
-
 # -------------------------------------------------------------------- #
 ## Engines 
 O.engines=[
-	ForceResetter(),
-	InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Facet_Aabb()],label="collider"),
-	InteractionLoop(
-		[Ig2_Sphere_Sphere_ScGeom(), Ig2_Facet_Sphere_ScGeom()],
-		[Ip2_FrictMat_FrictMat_MindlinPhys(
-			frictAngle = MatchMaker(matches=((1,1,F_gg),(0,1,F_gs))), # 0 being the id of Steel and
-			en         = MatchMaker(matches=((1,1,e_gg),(0,1,e_gs)))  # 1 being the id of granularMaterial
-		)],
-		[Law2_ScGeom_MindlinPhys_Mindlin()],
-	),
-	NewtonIntegrator(damping=0,gravity=[0,0,-9810],label="newton"),
-	#GlobalStiffnessTimeStepper(active=1,timestepSafetyCoefficient=0.8, timeStepUpdateInterval=100, parallelMode=False, label = "ts",defaultDt=PWaveTimeStep()), #FIXME Remember to reinstate parallelMode=True when we use MPI
-	#VTKRecorder(virtPeriod=0.04,fileName='/tmp/Silo-',recorders=['spheres','facets']),
+    ForceResetter(),
+    InsertionSortCollider([Bo1_Sphere_Aabb(),Bo1_Facet_Aabb()],label="collider"),
+    InteractionLoop(
+        [Ig2_Sphere_Sphere_ScGeom(), Ig2_Facet_Sphere_ScGeom()],
+        [Ip2_FrictMat_FrictMat_MindlinPhys(
+            frictAngle = MatchMaker(matches=((1,1,F_gg),(0,1,F_gs))), # 0 being the id of Steel and
+            en         = MatchMaker(matches=((1,1,e_gg),(0,1,e_gs)))  # 1 being the id of granularMaterial
+        )],
+        [Law2_ScGeom_MindlinPhys_Mindlin()],
+    ),
+    NewtonIntegrator(damping=0,gravity=[0,0,-9.810],label="newton"),
+    #GlobalStiffnessTimeStepper(active=1,timestepSafetyCoefficient=0.8, timeStepUpdateInterval=100, parallelMode=False, label = "ts",defaultDt=PWaveTimeStep()), #FIXME Remember to reinstate parallelMode=True when we use MPI
+    #VTKRecorder(virtPeriod=0.04,fileName='/tmp/Silo-',recorders=['spheres','facets']),
 ]
 
+numThreads=3
+from yade import mpy as mp
+if mp.rank==0:
 
+    # -------------------------------------------------------------------- #
+    # Generate initial packing. Choose among regularOrtho, regularHexa or randomDensePack (which I think is best).
 
-# -------------------------------------------------------------------- #
-# Generate initial packing. Choose among regularOrtho, regularHexa or randomDensePack (which I think is best).
+    ## Using regularOrtho
+    #sp=pack.regularOrtho(pack.inCylinder(Vector3(0,0,0),Vector3(0,0,305),radius),radius=2,gap=r*1/10.,material=granularMaterial)
 
-## Using regularOrtho
-#sp=pack.regularOrtho(pack.inCylinder(Vector3(0,0,0),Vector3(0,0,305),radius),radius=2,gap=r*1/10.,material=granularMaterial)
+    ## Using regularHexa
+    #sp=pack.regularHexa(pack.inCylinder(Vector3(0,0,0),Vector3(0,0,215),radius),radius=2,gap=r*1/10.,material=granularMaterial)  
 
-## Using regularHexa
-#sp=pack.regularHexa(pack.inCylinder(Vector3(0,0,0),Vector3(0,0,215),radius),radius=2,gap=r*1/10.,material=granularMaterial)  
+    # Using randomDensePack
+    sp=pack.randomDensePack(pack.inCylinder((0,0,0),(0,0,254),100),radius=particleRadius,spheresInCell=500,returnSpherePack=False,material=granularMaterial,seed=1) # FIXME: We can maybe increase spheresInCell to 5k or 10k. I used 500 to have a fast generation of the sample at this stage. If you prefer, we can load the packing from a db using memoizeDb, to achieve a faster sample generation.
 
-# Using randomDensePack
-sp=pack.randomDensePack(pack.inCylinder((0,0,0),(0,0,254),100),radius=4,spheresInCell=500,returnSpherePack=False,material=granularMaterial,seed=1) # FIXME: We can maybe increase spheresInCell to 5k or 10k. I used 500 to have a fast generation of the sample at this stage. If you prefer, we can load the packing from a db using memoizeDb, to achieve a faster sample generation.
+    # -------------------------------------------------------------------- #
+    # Sort packing in ascending Z coordinates and delete excess particles to achieve sample size of 122k
+    zValues=[]
+    for s in sp:
+        zValues.append(s.state.pos[2])
 
-# -------------------------------------------------------------------- #
-# Sort packing in ascending Z coordinates and delete excess particles to achieve sample size of 122k
-zValues=[]
-for s in sp:
-	zValues.append(s.state.pos[2])
+    from operator import itemgetter
+    indices, zValues_sorted = zip(*sorted(enumerate(zValues), key=itemgetter(1)))
+    list(zValues)
+    list(indices)
 
-from operator import itemgetter
-indices, zValues_sorted = zip(*sorted(enumerate(zValues), key=itemgetter(1)))
-list(zValues)
-list(indices)
+    sp_new=[]
+    for i in range(0,len(sp)):
+        sp_new.append(sp[indices[i]])
 
-sp_new=[]
-for i in range(0,len(sp)):
-	sp_new.append(sp[indices[i]])
+    Nspheres=122000
+    sp_new=sp_new[0:Nspheres]
 
-Nspheres=122000
-sp_new=sp_new[0:Nspheres]
-
-from yade import ymport
-facets = ymport.stl(fileName+'.stl',color=(0,1,0),material=Steel)
-fctIds = range(len(facets))
-
-NSTEPS = 1000
-
-if numMPIThreads > 1:
-    mp.mprint("appending bodies, rank", mp.rank)
-
-    if mp.rank==0:
-        O.bodies.append(facets)
-        mp.mprint("master has",len(O.bodies), "facets")
-    else:
-        import numpy as np
-        layers = np.array_split(sp_new,numMPIThreads-1)
-        mp.mprint("layers",[len(l) for l in layers])
-        layerNo = mp.rank-1 #rank zero is for facets
-        nextId = int(len(facets) + np.sum([len(x) for x in layers[:layerNo]]))
-        mp.mprint("s",s,"startId",nextId)
-        for s in layers[layerNo]:
-            s.subdomain = mp.rank
-            O.bodies.insertAtId(s,nextId)
-            nextId += 1
-        mp.mprint("s",s,"lastId",nextId-1)
-        
-        # tune mpi
-    mp.VERBOSE_OUTPUT=False
-    mp.DISTRIBUTED_INSERT=True
-    mp.REALLOCATE_FREQUENCY=4
-    mp.ACCUMULATE_FORCES=False
-    mp.MAX_RANK_OUTPUT=4
+    from yade import ymport
+    facets = ymport.stl(fileName+'.stl',color=(0,1,0),material=Steel)
+    fctIds = range(len(facets))
     
-
-else:
     O.bodies.append(facets)
     O.bodies.append(sp_new)
-    
+        
+    # -------------------------------------------------------------------- #
+    # Count the number of spherical particles to verify sample size. We can comment this out later on.
+    numSpheres=0
+    for b in O.bodies:
+        if isinstance(b.shape,Sphere):
+            numSpheres=numSpheres+1
+    print('The total number of spheres is: ',numSpheres)
+        
+    collider.verletDist = 0.2*particleRadius
+    O.dt=0.6 * PWaveTimeStep()
+    O.dynDt=False
 
 
-collider.verletDist = 0.5
-O.dt=1e-10
-O.dynDt=False
 
-if numMPIThreads>1:
-    mp.mpirun(1,numMPIThreads,False) #this is to eliminate initialization overhead in Cundall number and timings
-    mp.YADE_TIMING=True
-    t1=time.time()
-    mp.mpirun(NSTEPS,withMerge=False)
-    t2=time.time()
-    mp.mprint("num. bodies:",len([b for b in O.bodies])," ",len(O.bodies))
-    if mp.rank==0:
-        mp.mprint("CPU wall time for ",NSTEPS," iterations:",t2-t1,"; Cundall number = TODO")
-    #mp.mergeScene()
+if numThreads>1:
+    mp.DOMAIN_DECOMPOSITION =True
+    mp.ACCUMULATE_FORCES=False
+    mp.MERGE_W_INTERACTIONS=False
+    mp.REALLOCATE_MINIMAL = False # if true, intersections are minimized before reallocations, hence minimizing the number of reallocated bodies
+    mp.REALLOCATE_FREQUENCY=20
+    mp.USE_CPP_REALLOC=True
+    mp.MINIMAL_INTERSECTIONS = True
+    mp.MASTER_UPDATE_STATES = False
+    mp.mpirun(1,numThreads,False) #this is to eliminate initialization overhead in Cundall number and timings
+    #mp.MINIMAL_INTERSECTIONS = True
+    mp.YADE_TIMING=False
+
 
 else: 
     O.run(1,True)
+    O.timingEnabled=True
     t1=time.time()
-    O.run(NSTEPS,True)
-    t2=time.time()
-    print("num. bodies:",len([b for b in O.bodies])," ",len(O.bodies))
-    print("CPU wall time for ",NSTEPS," iterations:",t2-t1,"; Cundall number = TODO")
+    #O.run(NSTEPS,True)
+    #t2=time.time()
+    #print("num. bodies:",len([b for b in O.bodies])," ",len(O.bodies))
+    #print("CPU wall time for ",NSTEPS," iterations:",t2-t1,"; Cundall number = TODO")
 
-# -------------------------------------------------------------------- #
-# Record time-dependent number of retained particles
+#-------------------------------------------------------------------- #
+#Record time-dependent number of retained particles and vtk export
 
-#def recordRetained():
-#	retained=0
-#	retained=sum(1 for i in indSpheres if O.bodies[i].state.pos[2]>-z) # z=-70mm for the silo with LargeOrifice and z=-80 for the siilo with SmallOrifice
-##	print(retained)
-#	t=O.time
-#	return t,retained
+from yade import plot
+plot.plots={'time':('retained')}
 
-#from yade import plot
-#def addPlotData(): 
-##	try:
-	#retained=sum(1 for i in indSpheres if O.bodies[i].state.pos[2]>-z)  
-	#eraseEscapedParticles()
-	#plot.addData(retained=retained, time1=O.time)
-	#if retained==0:
-		#finish()
-##	except:
-##		pass
+numErased = 0
+def addPlotData(): 
+	plot.addData(retained=numSpheres-numErased, time=O.time)
 
+# Can't be used very easily with MPI split/merge in between, so I'm calling in a loop below (Bruno)
 #O.engines=O.engines+[PyRunner(iterPeriod=100,command='addPlotData()')] # FIXME: Remember to revisit the iterPeriod
 
-#plot.plots={'time1':'retained'}
-#plot.plot(noShow=False)
+from yade import export
+vtk = export.VTKExporter("spheres")
+
+# -------------------------------------------------------------------- #
+# Erase particles flowing out of the silo
+
+def eraseEscapedParticles():
+    global numErased
+    count=0
+    for b in O.bodies:
+        if isinstance(b.shape,Sphere) and b.state.pos[2]<-z-20: # I do not delete the particles right after they pass the orifice, to disturb the simulation as little as possible
+            #O.bodies.erase(b.id)
+            mp.bodyErase(b.id)
+            count+=1
+    numErased+=count
+    mp.mprint("erased",count)
 
 
-## -------------------------------------------------------------------- #
-## Erase particles flowing out of the silo
-#def eraseEscapedParticles():
-	#for i in indSpheres:
-		#if O.bodies[i].state.pos[2]<-z-20: # I do not delete the particles right after they pass the orifice, to disturb the simulation as little as possible
-			#indSpheres.remove(i)
-			#O.bodies.erase(i)
+# -------------------------------------------------------------------- #
+# Run iterations 
 
-#def finish():
-	#plot.saveDataTxt(fileName+'_'+granularMaterial+'.txt',vars=('time1','retained'))
-	#O.pause()
+t1=time.time()
+
+#while numSpheres-numErased>0: # <----- to comment in, in final release
+for k in range(2):
+    mp.mpirun(500,numThreads,True)
+    if mp.rank==0:
+        addPlotData()
+        vtk.exportSpheres(what=dict(particleVelocity='b.state.vel',color='b.shape.color'))
+    if mp.rank>0: mp.mprint("has",len(O.subD.ids),"particles")
+    eraseEscapedParticles()
+
+t2=time.time()
+mp.mprint("CPU time:",t2-t1)
+
+if mp.rank==0:
+    print("Cundall number:", 0.5*numSpheres*O.iter/(t2-t1)) # computed with N*0.5 since we go from N to zero
+    plot.plot(noShow=True).savefig(fileName+'_'+granularMaterial+'.png')
+    plot.saveDataTxt(fileName+'_'+granularMaterial+'.txt')
 
 ## -------------------------------------------------------------------- #
 ## GUI
@@ -240,6 +227,8 @@ else:
 	##rndr.shape=False
 	##rndr.bound=True
 
-##O.saveTmp()
-##O.run()
-
+## To play interactively after mpi execution:
+## mp.mpirun(100,numThreads,True) #'True' so we see merged scene after the run
+## eraseEscapedParticles()
+## mp.mpirun(100,numThreads,True)
+## etc.
