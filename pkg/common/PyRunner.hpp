044d1f2166df92f8b3fc7fbb800f1f932b655d6d
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2021-01-03 01:53:30 +0100

    PyRunner.hpp : add optional flag updateGlobals==true by default.

--- pkg/common/PyRunner.hpp
+++ pkg/common/PyRunner.hpp
@@ -13,13 +13,16 @@ public:
 	virtual void action()
 	{
 		if (command.size() > 0)
-			pyRunString(command, ignoreErrors);
+			pyRunString(command, ignoreErrors, updateGlobals);
 	}
 	// clang-format off
 	YADE_CLASS_BASE_DOC_ATTRS(PyRunner,PeriodicEngine,
 		"Execute a python command periodically, with defined (and adjustable) periodicity. See :yref:`PeriodicEngine` documentation for details.",
 		((string,command,"",,"Command to be run by python interpreter. Not run if empty."))
 		((bool,ignoreErrors,false,,"Debug only: set this value to true to tell PyRunner to ignore any errors encountered during command execution."))
+		((bool,updateGlobals,true,,"Whether to workaround `ipython not recognizing local variables <https://github.com/ipython/ipython/issues/136>`__ "
+		"by calling globals().update(locals()). When true PyRunner is able to call functions declared later locally in a running (live) yade session. "
+		"The call is a bit slower to update globals() with recently declared python functions."))
 	);
 	// clang-format on
 };
