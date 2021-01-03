7a8b3f6e390a62b281d28981c6744f1e7d99b9da
Janek Kozicki <jkozicki-yade@pg.edu.pl>
2021-01-03 02:11:39 +0100

    remove spurious '1' which got here 9 years ago

--- py/runtime.py
+++ py/runtime.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-1# this module is populated at initialization from the c++ part of PythonUI
+# this module is populated at initialization from the c++ part of PythonUI
 """Runtime variables, populated at yade startup."""
 # default value
 hasDisplay=False 
